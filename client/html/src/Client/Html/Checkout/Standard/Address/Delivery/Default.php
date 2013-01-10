c9b7bdbd20cbae5fbe3bdbb1e4009ab4baf68733
Norbert Sendetzky <norbert@linuxnetworks>
2013-01-10 20:33:47 +0100

    Adds address part of standard checkout client

---
+++ client/html/src/Client/Html/Checkout/Standard/Address/Delivery/Default.php
@@ -0,0 +1,200 @@
+<?php
+
+/**
+ * @copyright Copyright (c) Metaways Infosystems GmbH, 2013
+ * @license LGPLv3, http://www.arcavias.com/en/license
+ * @package Client
+ * @subpackage Html
+ */
+
+
+/**
+ * Default implementation of checkout billing address HTML client.
+ *
+ * @package Client
+ * @subpackage Html
+ */
+class Client_Html_Checkout_Standard_Address_Delivery_Default
+	extends Client_Html_Abstract
+	implements Client_Html_Interface
+{
+	private $_cache;
+	private $_subPartPath = 'client/html/checkout/standard/address/delivery/default/subparts';
+	private $_subPartNames = array();
+
+
+	/**
+	 * Returns the HTML code for insertion into the body.
+	 *
+	 * @param string|null $name Template name
+	 * @return string HTML code
+	 */
+	public function getBody( $name = null )
+	{
+		$view = $this->_process( $this->getView() );
+
+		$html = '';
+		foreach( $this->_getSubClients( $this->_subPartPath, $this->_subPartNames ) as $subclient ) {
+			$html .= $subclient->setView( $view )->getBody();
+		}
+		$view->deliveryBody = $html;
+
+		$tplconf = 'client/html/checkout/standard/address/delivery/default/template-body';
+		$default = 'checkout/standard/address-delivery-body-default.html';
+
+		return $view->render( $this->_getTemplate( $tplconf, $default ) );
+	}
+
+
+	/**
+	 * Returns the HTML string for insertion into the header.
+	 *
+	 * @param string|null $name Template name
+	 * @return string String including HTML tags for the header
+	 */
+	public function getHeader( $name = null )
+	{
+		$view = $this->_process( $this->getView() );
+
+		$html = '';
+		foreach( $this->_getSubClients( $this->_subPartPath, $this->_subPartNames ) as $subclient ) {
+			$html .= $subclient->setView( $view )->getHeader();
+		}
+		$view->deliveryHeader = $html;
+
+		$tplconf = 'client/html/checkout/standard/address/delivery/default/template-header';
+		$default = 'checkout/standard/address-delivery-header-default.html';
+
+		return $view->render( $this->_getTemplate( $tplconf, $default ) );
+	}
+
+
+	/**
+	 * Returns the sub-client given by its name.
+	 *
+	 * @param string $type Name of the client type
+	 * @param string|null $name Name of the sub-client (Default if null)
+	 * @return Client_Html_Interface Sub-client object
+	 */
+	public function getSubClient( $type, $name = null )
+	{
+		return $this->_createSubClient( 'checkout/standard/address/delivery/' . $type, $name );
+	}
+
+
+	/**
+	 * Tests if the output of is cachable.
+	 *
+	 * @param integer $what Header or body constant from Client_HTML_Abstract
+	 * @return boolean True if the output can be cached, false if not
+	 */
+	public function isCachable( $what )
+	{
+		return false;
+	}
+
+
+	/**
+	 * Stores the given or fetched billing address in the basket.
+	 */
+	public function process()
+	{
+		$view = $this->getView();
+
+		// only start if there's something to do
+		if( $view->param( 'ca-delivery-option', null ) === null ) {
+			return;
+		}
+
+		$basketCtrl = Controller_Frontend_Basket_Factory::createController( $this->_getContext() );
+		$basket = $basketCtrl->get();
+
+		try
+		{
+			$type = MShop_Order_Item_Base_Address_Abstract::TYPE_DELIVERY;
+
+			if( ( $option = $view->param( 'ca-delivery-option', '' ) ) == '' ) // new address
+			{
+				$param = $view->param( 'ca-delivery', array() );
+
+				$missing = array();
+				$default = array(
+					'order.base.address.salutation',
+					'order.base.address.firstname',
+					'order.base.address.lastname',
+					'order.base.address.address1',
+					'order.base.address.postal',
+					'order.base.address.city',
+					'order.base.address.langid'
+				);
+
+				foreach( $view->config( 'checkout/address/delivery/mandatory', $default ) as $mandatory )
+				{
+					if( !isset( $param[$mandatory] ) ) {
+						$missing[$mandatory] = sprintf( 'Delivery adddress part "%1$s" is missing', $mandatory );
+					}
+				}
+
+				if( count( $missing ) > 0 )
+				{
+					$view->deliveryError = $missing;
+					throw new Client_Html_Exception( 'At least one delivery address part is missing' );
+				}
+
+				$basketCtrl->setAddress( $type, $param );
+			}
+			else // existing address
+			{
+				/** @todo check that only addresses for the logged in customer can be added */
+				$customerManager = MShop_Customer_Manager_Factory::createManager( $this->_getContext() );
+				$customerAddressManager = $customerManager->getSubManager( 'address' );
+				$basketCtrl->setAddress( $type, $customerAddressManager->getItem( $option ) );
+			}
+
+			$view->deliveryAddress = $basket->getAddress( $type );
+		}
+		catch( Controller_Frontend_Exception $e )
+		{
+			$view->deliveryError = $e->getErrorList();
+			throw $e;
+		}
+	}
+
+
+	/**
+	 * Sets the necessary parameter values in the view.
+	 *
+	 * @param MW_View_Interface $view The view object which generates the HTML output
+	 * @return MW_View_Interface Modified view object
+	 */
+	protected function _process( MW_View_Interface $view )
+	{
+		if( !isset( $this->_cache ) )
+		{
+			$default = array(
+				'order.base.address.salutation',
+				'order.base.address.firstname',
+				'order.base.address.lastname',
+				'order.base.address.address1',
+				'order.base.address.postal',
+				'order.base.address.city',
+				'order.base.address.langid',
+			);
+
+			$view->deliveryMandatory = $view->config( 'checkout/address/delivery/mandatory', $default );
+
+			$default = array(
+				'order.base.address.company',
+				'order.base.address.address2',
+				'order.base.address.countryid',
+			);
+
+			$view->deliveryOptional = $view->config( 'checkout/address/delivery/optional', $default );
+
+
+			$this->_cache = $view;
+		}
+
+		return $this->_cache;
+	}
+}
---
+++ client/html/tests/Client/Html/Checkout/Standard/Address/Delivery/DefaultTest.php
@@ -0,0 +1,218 @@
+<?php
+
+/**
+ * @copyright Copyright (c) Metaways Infosystems GmbH, 2013
+ * @license LGPLv3, http://www.arcavias.com/en/license
+ */
+
+class Client_Html_Checkout_Standard_Address_Delivery_DefaultTest extends MW_Unittest_Testcase
+{
+	protected $_object;
+	protected $_context;
+
+
+	/**
+	 * Runs the test methods of this class.
+	 *
+	 * @access public
+	 * @static
+	 */
+	public static function main()
+	{
+		require_once 'PHPUnit/TextUI/TestRunner.php';
+
+		$suite = new PHPUnit_Framework_TestSuite('Client_Html_Checkout_Standard_Address_DeliveryDefaultTest');
+		$result = PHPUnit_TextUI_TestRunner::run($suite);
+	}
+
+
+	/**
+	 * Sets up the fixture, for example, opens a network connection.
+	 * This method is called before a test is executed.
+	 *
+	 * @access protected
+	 */
+	protected function setUp()
+	{
+		$this->_context = TestHelper::getContext();
+
+		$paths = TestHelper::getHtmlTemplatePaths();
+		$this->_object = new Client_Html_Checkout_Standard_Address_Delivery_Default( $this->_context, $paths );
+		$this->_object->setView( TestHelper::getView() );
+	}
+
+
+	/**
+	 * Tears down the fixture, for example, closes a network connection.
+	 * This method is called after a test is executed.
+	 *
+	 * @access protected
+	 */
+	protected function tearDown()
+	{
+		unset( $this->_object );
+	}
+
+
+	public function testGetHeader()
+	{
+		$output = $this->_object->getHeader();
+		$this->assertStringStartsWith( '<style type="text/css">', $output );
+	}
+
+
+	public function testGetBody()
+	{
+		$view = TestHelper::getView();
+		$this->_object->setView( $view );
+
+		$output = $this->_object->getBody();
+		$this->assertStringStartsWith( '<div class="address-delivery">', $output );
+
+		$this->assertGreaterThan( 0, count( $view->deliveryMandatory ) );
+		$this->assertGreaterThan( 0, count( $view->deliveryOptional ) );
+	}
+
+
+	public function testGetSubClientInvalid()
+	{
+		$this->setExpectedException( 'Client_Html_Exception' );
+		$this->_object->getSubClient( 'invalid', 'invalid' );
+	}
+
+
+	public function testGetSubClientInvalidName()
+	{
+		$this->setExpectedException( 'Client_Html_Exception' );
+		$this->_object->getSubClient( '$$$', '$$$' );
+	}
+
+
+	public function testIsCachable()
+	{
+		$this->assertEquals( false, $this->_object->isCachable( Client_HTML_Abstract::CACHE_BODY ) );
+		$this->assertEquals( false, $this->_object->isCachable( Client_HTML_Abstract::CACHE_HEADER ) );
+	}
+
+
+	public function testProcess()
+	{
+		$view = TestHelper::getView();
+		$this->_object->setView( $view );
+
+		$this->_object->process();
+	}
+
+
+	public function testProcessNewAddress()
+	{
+		$view = TestHelper::getView();
+
+		$param = array(
+			'ca-delivery-option' => '',
+			'ca-delivery' => array(
+				'order.base.address.salutation' => 'mr',
+				'order.base.address.firstname' => 'test',
+				'order.base.address.lastname' => 'user',
+				'order.base.address.address1' => 'mystreet 1',
+				'order.base.address.postal' => '20000',
+				'order.base.address.city' => 'hamburg',
+				'order.base.address.langid' => 'en',
+			),
+		);
+		$helper = new MW_View_Helper_Parameter_Default( $view, $param );
+		$view->addHelper( 'param', $helper );
+
+		$this->_object->setView( $view );
+
+		$this->_object->process();
+
+		$basket = Controller_Frontend_Basket_Factory::createController( $this->_context )->get();
+		$this->assertEquals( 'hamburg', $basket->getAddress( 'delivery' )->getCity() );
+	}
+
+
+	public function testProcessNewAddressMissing()
+	{
+		$view = TestHelper::getView();
+
+		$param = array(
+			'ca-delivery-option' => '',
+			'ca-delivery' => array(
+				'order.base.address.firstname' => 'test',
+				'order.base.address.lastname' => 'user',
+				'order.base.address.address1' => 'mystreet 1',
+				'order.base.address.postal' => '20000',
+				'order.base.address.city' => 'hamburg',
+			),
+		);
+		$helper = new MW_View_Helper_Parameter_Default( $view, $param );
+		$view->addHelper( 'param', $helper );
+
+		$this->_object->setView( $view );
+
+		try
+		{
+			$this->_object->process();
+		}
+		catch( Client_Html_Exception $e )
+		{
+			$this->assertEquals( 2, count( $view->deliveryError ) );
+			$this->assertArrayHasKey( 'order.base.address.salutation', $view->deliveryError );
+			$this->assertArrayHasKey( 'order.base.address.langid', $view->deliveryError );
+			return;
+		}
+
+		$this->fail( 'Expected exception not thrown' );
+	}
+
+
+	public function testProcessExistingAddress()
+	{
+		$customerManager = MShop_Customer_Manager_Factory::createManager( $this->_context );
+		$search = $customerManager->createSearch();
+		$search->setConditions( $search->compare( '==', 'customer.code', 'UTC001' ) );
+		$result = $customerManager->searchItems( $search );
+
+		if( ( $customer = reset( $result ) ) === false ) {
+			throw new Exception( 'Customer item not found' );
+		}
+
+		$customerAddressManager = $customerManager->getSubManager( 'address' );
+		$search = $customerAddressManager->createSearch();
+		$search->setConditions( $search->compare( '==', 'customer.address.refid', $customer->getId() ) );
+		$result = $customerAddressManager->searchItems( $search );
+
+		if( ( $address = reset( $result ) ) === false ) {
+			throw new Exception( 'Customer address item not found' );
+		}
+
+		$view = TestHelper::getView();
+
+		$param = array( 'ca-delivery-option' => $address->getId() );
+		$helper = new MW_View_Helper_Parameter_Default( $view, $param );
+		$view->addHelper( 'param', $helper );
+
+		$this->_object->setView( $view );
+
+		$this->_object->process();
+
+		$basket = Controller_Frontend_Basket_Factory::createController( $this->_context )->get();
+		$this->assertEquals( 'Metaways', $basket->getAddress( 'payment' )->getCompany() );
+	}
+
+
+	public function testProcessInvalidId()
+	{
+		$view = TestHelper::getView();
+
+		$param = array( 'ca-delivery-option' => -1 );
+		$helper = new MW_View_Helper_Parameter_Default( $view, $param );
+		$view->addHelper( 'param', $helper );
+
+		$this->_object->setView( $view );
+
+		$this->setExpectedException( 'MShop_Exception' );
+		$this->_object->process();
+	}
+}
