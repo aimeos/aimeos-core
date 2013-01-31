35595945e759f664957b2c0262755310aff87479
cmann2012 <c.mann@metaways.de>
2013-01-31 09:41:53 +0100

    add get/set methods to the item interfaces

--- lib/mshoplib/config/common/mshop/order/manager/base/service/attribute/default.php
+++ lib/mshoplib/config/common/mshop/order/manager/base/service/attribute/default.php
@@ -1,10 +1,10 @@
 <?php
 
 /**
-* @copyright Copyright (c) Metaways Infosystems GmbH, 2011
-* @license LGPLv3, http://www.arcavias.com/en/license
-* @version $Id: default.php 14246 2011-12-09 12:25:12Z nsendetzky $
-*/
+ * @copyright Copyright (c) Metaways Infosystems GmbH, 2011
+ * @license LGPLv3, http://www.arcavias.com/en/license
+ * @version $Id: default.php 14246 2011-12-09 12:25:12Z nsendetzky $
+ */
 
 return array(
 	'item' => array(
