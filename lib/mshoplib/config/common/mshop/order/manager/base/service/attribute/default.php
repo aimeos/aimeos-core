298c230d2b2dcd17a0c03cbc48ff7362dc993f7a
cmann2012 <c.mann@metaways.de>
2013-01-31 08:55:14 +0100

    adapt the indention of the lines

--- lib/mshoplib/config/common/mshop/order/manager/base/service/attribute/default.php
+++ lib/mshoplib/config/common/mshop/order/manager/base/service/attribute/default.php
@@ -7,39 +7,39 @@
 */
 
 return array(
-'item' => array(
-	'delete' => '
-		DELETE FROM "mshop_order_base_service_attr"
-		WHERE "id" = ?
-	',
-	'insert' => '			
-		INSERT INTO "mshop_order_base_service_attr" ( "siteid", "ordservid", "type", "code", "value", "name",
-			"mtime", "editor", "ctime" )
-		VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ? )
-	',
-	'update' => '
-		UPDATE "mshop_order_base_service_attr"
-		SET "siteid" = ?, "ordservid" = ?, "type" = ?, "code" = ?, "value" = ?, "name" = ?, "mtime" = ?, "editor" = ?
-		WHERE "id" = ?
-	',		
-	'search' => '
-		SELECT mordbaseat."id", mordbaseat."siteid", mordbaseat."ordservid", mordbaseat."type", mordbaseat."code", 
-		mordbaseat."value", mordbaseat."name", mordbaseat."mtime", mordbaseat."ctime", mordbaseat."editor"
-		FROM "mshop_order_base_service_attr" AS mordbaseat
-		:joins
-		WHERE :cond
-		/*-orderby*/ ORDER BY :order /*orderby-*/
-		LIMIT :size OFFSET :start
-	',
-	'count' => '
-		SELECT COUNT(*) AS "count"
-		FROM(
-			SELECT DISTINCT mordbaseat."id"
+	'item' => array(
+		'delete' => '
+			DELETE FROM "mshop_order_base_service_attr"
+			WHERE "id" = ?
+		',
+		'insert' => '
+			INSERT INTO "mshop_order_base_service_attr" ( "siteid", "ordservid", "type", "code", "value", "name",
+				"mtime", "editor", "ctime" )
+			VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ? )
+		',
+		'update' => '
+			UPDATE "mshop_order_base_service_attr"
+			SET "siteid" = ?, "ordservid" = ?, "type" = ?, "code" = ?, "value" = ?, "name" = ?, "mtime" = ?, "editor" = ?
+			WHERE "id" = ?
+		',
+		'search' => '
+			SELECT mordbaseat."id", mordbaseat."siteid", mordbaseat."ordservid", mordbaseat."type", mordbaseat."code", 
+			mordbaseat."value", mordbaseat."name", mordbaseat."mtime", mordbaseat."ctime", mordbaseat."editor"
 			FROM "mshop_order_base_service_attr" AS mordbaseat
 			:joins
 			WHERE :cond
-			LIMIT 10000 OFFSET 0
-		) AS list
-	',
+			/*-orderby*/ ORDER BY :order /*orderby-*/
+			LIMIT :size OFFSET :start
+		',
+		'count' => '
+			SELECT COUNT(*) AS "count"
+			FROM(
+				SELECT DISTINCT mordbaseat."id"
+				FROM "mshop_order_base_service_attr" AS mordbaseat
+				:joins
+				WHERE :cond
+				LIMIT 10000 OFFSET 0
+			) AS list
+		',
 	),
 );
