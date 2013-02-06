b090f177b80037f8aedb0814e626378d59729a8e
doleiynyk <doleiynyk@dev.hh.metaways.de>
2013-02-06 13:55:04 +0100

    Adds breadcrumb controller.

---
+++ client/html/tests/Client/Html/Catalog/List/Breadcrumb/DefaultTest.php
@@ -0,0 +1,97 @@
+<?php
+
+/**
+ * @copyright Copyright (c) Metaways Infosystems GmbH, 2012
+ * @license LGPLv3, http://www.arcavias.com/en/license
+ */
+
+class Client_Html_Catalog_List_Breadcrumb_DefaultTest extends MW_Unittest_Testcase
+{
+	protected $_object;
+
+
+	/**
+	 * Runs the test methods of this class.
+	 *
+	 * @access public
+	 * @static
+	 */
+	public static function main()
+	{
+		require_once 'PHPUnit/TextUI/TestRunner.php';
+
+		$suite = new PHPUnit_Framework_TestSuite('Client_Html_Catalog_List_Breadcrumb_DefaultTest');
+		$result = PHPUnit_TextUI_TestRunner::run($suite);
+	}
+
+
+	/**
+	 * Sets up the fixture, for example, opens a network connection.
+	 * This method is called before a test is executed.
+	 *
+	 * @access protected
+	 */
+	protected function setUp()
+	{
+		$context = TestHelper::getContext();
+		$paths = TestHelper::getHtmlTemplatePaths();
+		$this->_object = new Client_Html_Catalog_List_Breadcrumb_Default( $context, $paths );
+
+		$catalogManager = MShop_Catalog_Manager_Factory::createManager( $context );
+		$search = $catalogManager->createSearch();
+		$search->setConditions( $search->compare( '==', 'catalog.code', 'cafe' ) );
+		$catItems = $catalogManager->searchItems( $search );
+
+		if( ( $catItem = reset( $catItems ) ) === false ) {
+			throw new Exception( 'No catalog item found' );
+		}
+
+		$productManager = MShop_Product_Manager_Factory::createManager( $context );
+		$search = $productManager->createSearch();
+		$search->setConditions( $search->compare( '==', 'product.code', array( 'CNC', 'CNE' ) ) );
+		$total = 0;
+
+		$view = TestHelper::getView();
+
+		$view->listProductItems = $productManager->searchItems( $search, array( 'media', 'price', 'text' ), $total );
+		$view->listProductTotal = $total;
+		$view->listPageSize = 100;
+		$view->listPageCurr = 1;
+		$view->listParams = array();
+		$view->listCatPath = array( $catalogManager->createItem(), $catItem );
+
+		$this->_object->setView( $view );
+	}
+
+
+	/**
+	 * Tears down the fixture, for example, closes a network connection.
+	 * This method is called after a test is executed.
+	 *
+	 * @access protected
+	 */
+	protected function tearDown()
+	{
+		unset( $this->_object );
+	}
+
+
+	public function testGetHeader()
+	{
+		$output = $this->_object->getHeader();
+		$this->assertEquals( '', $output );
+	}
+
+
+	public function testGetBody()
+	{
+		$output = $this->_object->getBody();
+		$this->assertStringStartsWith( '<div class="catalog-list-breadcrumb">', $output );
+	}
+
+	public function testGetSubClient()
+	{
+		$this->setExpectedException( 'Client_Html_Exception' );
+		$this->_object->getSubClient( 'invalid', 'invalid' );
+	}
+}
