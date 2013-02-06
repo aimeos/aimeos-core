40d3c512d07dfd1a0ab9d7d0fe7c6be7d4aa188b
doleiynyk <doleiynyk@dev.hh.metaways.de>
2013-02-06 18:38:15 +0100

    Adds search and simple client files.

---
+++ client/html/src/Client/Html/Catalog/List/Simple.php
@@ -0,0 +1,177 @@
+<?php
+
+/**
+ * @copyright Copyright (c) Metaways Infosystems GmbH, 2013
+ * @license LGPLv3, http://www.arcavias.com/en/license
+ * @package Client
+ * @subpackage Html
+ */
+
+
+/**
+ * Simple implementation of catalog list section HTML clients.
+ *
+ * @package Client
+ * @subpackage Html
+ */
+class Client_Html_Catalog_List_Simple
+	extends Client_Html_Abstract
+	implements Client_Html_Interface
+{
+	private $_cache;
+	private $_subPartPath = 'client/html/catalog/list/simple/subparts';
+	private $_subPartNames = array( 'header', 'pagination', 'items', 'pagination' );
+
+
+	/**
+	 * Returns the HTML code for insertion into the body.
+	 *
+	 * @return string HTML code
+	 */
+	public function getBody()
+	{
+		$view = $this->_setViewParams( $this->getView() );
+
+		$html = '';
+		foreach( $this->_getSubClients( $this->_subPartPath, $this->_subPartNames ) as $subclient ) {
+			$html .= $subclient->setView( $view )->getBody();
+		}
+		$view->simpleListBody = $html;
+
+		$tplconf = 'client/html/catalog/list/simple/template-body';
+		$default = 'catalog/list/body-simple.html';
+
+		return $view->render( $this->_getTemplate( $tplconf, $default ) );
+	}
+
+
+	/**
+	 * Returns the HTML string for insertion into the header.
+	 *
+	 * @return string String including HTML tags for the header
+	 */
+	public function getHeader()
+	{
+		$view = $this->_setViewParams( $this->getView() );
+
+		$html = '';
+		foreach( $this->_getSubClients( $this->_subPartPath, $this->_subPartNames ) as $subclient ) {
+			$html .= $subclient->setView( $view )->getHeader();
+		}
+		$view->simpleListHeader = $html;
+
+		$tplconf = 'client/html/catalog/list/simple/template-header';
+		$default = 'catalog/list/header-simple.html';
+
+		return $view->render( $this->_getTemplate( $tplconf, $default ) );
+	}
+
+
+	/**
+	 * Returns the sub-client given by its name.
+	 *
+	 * @param string $type Name of the client type
+	 * @param string|null $name Name of the sub-client (Default if null)
+	 * @return Client_Html_Interface Sub-client object
+	 */
+	public function getSubClient( $type, $name = null )
+	{
+		return $this->_createSubClient( 'catalog/list/' . $type, $name );
+	}
+
+
+	/**
+	 * Tests if the output of is cachable.
+	 *
+	 * @param integer $what Header or body constant from Client_HTML_Abstract
+	 * @return boolean True if the output can be cached, false if not
+	 */
+	public function isCachable( $what )
+	{
+		return false;
+	}
+
+
+	/**
+	 * Processes the input, e.g. store given values.
+	 * A view must be available and this method doesn't generate any output
+	 * besides setting view variables.
+	 */
+	public function process()
+	{
+		$this->_process( $this->_subPartPath, $this->_subPartNames );
+	}
+
+
+	/**
+	 * Sets the necessary parameter values in the view.
+	 *
+	 * @param MW_View_Interface $view The view object which generates the HTML output
+	 * @return MW_View_Interface Modified view object
+	 */
+	protected function _setViewParams( MW_View_Interface $view )
+	{
+		if( !isset( $this->_cache ) )
+		{
+			$context = $this->_getContext();
+			$config = $context->getConfig();
+
+
+			$params = array();
+			foreach( $view->param() as $key => $value )
+			{
+				if( strncmp( 'f-', $key, 2 ) === 0 || strncmp( 'l-', $key, 2 ) === 0 ) {
+					$params[$key] = $value;
+				}
+			}
+
+
+			$defaultPageSize = $config->get( 'client/html/catalog/list/simple/size', 48 );
+			$domains = $config->get( 'client/html/catalog/list/simple/domains', array( 'media', 'price', 'text' ) );
+
+
+			$page = (int) $view->param( 'l-page', 1 );
+			$size = (int) $view->param( 'l-size', $defaultPageSize );
+			$sortation = (string) $view->param( 'l-sort', 'position' );
+			$text = (string) $view->param( 'f-search-text' );
+			$catid = $view->param( 'f-catalog-id' );
+
+			$page = ( $page < 1 ? 1 : $page );
+			$size = ( $size < 1 || $size > 100 ? $defaultPageSize : $size );
+			$sortation = ( strlen( $sortation ) === 0 ? $sortation = 'position' : $sortation );
+
+
+			$sortdir = ( $sortation[0] === '-' ? '-' : '+' );
+			$sort = ltrim( $sortation, '-' );
+			$total = 0;
+
+
+			$controller = Controller_Frontend_Catalog_Factory::createController( $context );
+			$catalogManager = MShop_Catalog_Manager_Factory::createManager( $context );
+
+			if( !empty( $catid ) )
+			{
+				$filter = $controller->createProductFilterByCategory( $catid, $sort, $sortdir, ($page-1) * $size, $size );
+				$view->listCatPath = $catalogManager->getPath( $catid, array( 'text', 'media', 'attribute' ) );
+			}
+			else
+			{
+				$filter = $controller->createProductFilterByText( $text, $sort, $sortdir, ($page-1) * $size, $size );
+			}
+
+			$items = $controller->getProductList( $filter, $total, $domains );
+
+
+			$view->listProductItems = $items;
+			$view->listProductTotal = $total;
+			$view->listProductSort = $sortation;
+			$view->listPageCurr = $page;
+			$view->listPageSize = $size;
+			$view->listParams = $params;
+
+			$this->_cache = $view;
+		}
+
+		return $this->_cache;
+	}
+}
