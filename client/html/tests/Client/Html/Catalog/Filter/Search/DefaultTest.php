49dc9bf8ad62f39e9d5ba598c97d69c458af754f
Norbert <norbert@linuxnetworks.de>
2013-02-18 18:03:45 +0100

    Adapts test of header output

--- client/html/tests/Client/Html/Catalog/Filter/Search/DefaultTest.php
+++ client/html/tests/Client/Html/Catalog/Filter/Search/DefaultTest.php
@@ -54,7 +54,7 @@ class Client_Html_Catalog_Filter_Search_DefaultTest extends MW_Unittest_Testcase
 	public function testGetHeader()
 	{
 		$output = $this->_object->getHeader();
-		$this->assertStringStartsWith( '<script type="text/javascript">', $output );
+		$this->assertStringStartsWith( '<link href="css/search.css"', $output );
 	}
 
 
