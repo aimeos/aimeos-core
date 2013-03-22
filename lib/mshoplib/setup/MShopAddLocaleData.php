bbec47530e30eede15d624ed09c152695c0e99cc
Norbert <norbert@linuxnetworks.de>
2013-03-22 10:11:21 +0100

    Fixes pre-dependency to TablesCreateMShop

--- lib/mshoplib/setup/MShopAddLocaleData.php
+++ lib/mshoplib/setup/MShopAddLocaleData.php
@@ -19,7 +19,7 @@ class MW_Setup_Task_MShopAddLocaleData extends MW_Setup_Task_Abstract
 	 */
 	public function getPreDependencies()
 	{
-		return array();
+		return array( 'TablesCreateMShop' );
 	}
 
 
