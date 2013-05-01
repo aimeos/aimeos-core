27ec9e686bd95f8771ff099d95382e3c2d2d5c94
Norbert <norbert@linuxnetworks.de>
2013-05-01 03:10:35 +0200

    Fixes property visibility

--- lib/mshoplib/src/MShop/Plugin/Provider/Order/AddressesAvailable.php
+++ lib/mshoplib/src/MShop/Plugin/Provider/Order/AddressesAvailable.php
@@ -18,8 +18,8 @@
 class MShop_Plugin_Provider_Order_AddressesAvailable implements MShop_Plugin_Provider_Interface
 {
 
-	protected $_item;
-	protected $_context;
+	private $_item;
+	private $_context;
 
 
 	/**
