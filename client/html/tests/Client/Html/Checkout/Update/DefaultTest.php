527b7cb8ad4fb8836588453cf8c99be96fef2d99
Norbert <n.sendetzky@metaways.de>
2013-05-21 14:17:28 +0200

    Adds html client for processing order payment update notifications

---
+++ client/html/tests/Client/Html/Checkout/Update/DefaultTest.php
@@ -0,0 +1,85 @@
+<?php
+
+/**
+ * @copyright Copyright (c) Metaways Infosystems GmbH, 2013
+ * @license LGPLv3, http://www.arcavias.com/en/license
+ */
+
+class Client_Html_Checkout_Update_DefaultTest extends MW_Unittest_Testcase
+{
+	private $_object;
+	private $_context;
+
+
+	/**
+	 * Runs the test methods of this class.
+	 *
+	 * @access public
+	 * @static
+	 */
+	public static function main()
+	{
+		require_once 'PHPUnit/TextUI/TestRunner.php';
+
+		$suite = new PHPUnit_Framework_TestSuite('Client_Html_Checkout_Update_DefaultTest');
+		$result = PHPUnit_TextUI_TestRunner::run($suite);
+	}
+
+
+	/**
+	 * Sets up the fixture, for example, opens a network connection.
+	 * This method is called before a test is executed.
+	 *
+	 * @access protected
+	 */
+	protected function setUp()
+	{
+		$paths = TestHelper::getHtmlTemplatePaths();
+		$this->_object = new Client_Html_Checkout_Update_Default( TestHelper::getContext(), $paths );
+		$this->_object->setView( TestHelper::getView() );
+	}
+
+
+	/**
+	 * Tears down the fixture, for example, closes a network connection.
+	 * This method is called after a test is executed.
+	 *
+	 * @access protected
+	 */
+	protected function tearDown()
+	{
+		unset( $this->_object );
+	}
+
+
+	public function testGetHeader()
+	{
+		$this->assertEquals( '', $this->_object->getHeader() );
+	}
+
+
+	public function testGetBody()
+	{
+		$this->assertEquals( '', $this->_object->getBody() );
+	}
+
+
+	public function testGetSubClientInvalid()
+	{
+		$this->setExpectedException( 'Client_Html_Exception' );
+		$this->_object->getSubClient( 'invalid', 'invalid' );
+	}
+
+
+	public function testGetSubClientInvalidName()
+	{
+		$this->setExpectedException( 'Client_Html_Exception' );
+		$this->_object->getSubClient( '$$$', '$$$' );
+	}
+
+
+	public function testProcess()
+	{
+		$this->_object->process();
+	}
+}
