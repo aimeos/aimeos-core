d6a0dfe58ae1079ae55665bff6cc0b5decf4102f
Norbert <n.sendetzky@metaways.de>
2013-07-06 13:11:34 +0200

    Removes constructor from interface to be able to create managers directly from the common address manager

--- lib/mshoplib/src/MShop/Common/Manager/Address/Interface.php
+++ lib/mshoplib/src/MShop/Common/Manager/Address/Interface.php
@@ -16,12 +16,4 @@
 interface MShop_Common_Manager_Address_Interface
 	extends MShop_Common_Manager_Interface
 {
-	/**
-	 * Initializes a new common address manager object using the given context object.
-	 *
-	 * @param MShop_Context_Interface $_context Context object with required objects
-	 */
-	public function __construct( MShop_Context_Item_Interface $context,
-		array $config = array(), array $searchConfig = array() );
-
 }
