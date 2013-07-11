6004ee4df6934a639f2987c0caec9aeb6f2db66d
Norbert <n.sendetzky@metaways.de>
2013-07-11 11:44:43 +0200

    Returns always a string for the customer ID

--- client/html/src/Client/Html/Checkout/Standard/Order/Default.php
+++ client/html/src/Client/Html/Checkout/Standard/Order/Default.php
@@ -132,7 +132,7 @@ class Client_Html_Checkout_Standard_Order_Default
 			$orderBaseManager = $orderManager->getSubManager( 'base' );
 
 			$basket = $orderBaseManager->getSession();
-			$basket->setCustomerId( (string) $context->getUserId() );
+			$basket->setCustomerId( $context->getUserId() );
 			$basket->finish();
 
 			$orderBaseManager->store( $basket );
@@ -142,8 +142,10 @@ class Client_Html_Checkout_Standard_Order_Default
 			$orderItem->setType( MShop_Order_Item_Abstract::TYPE_WEB );
 			$orderManager->saveItem( $orderItem );
 
-			$view->orderItem = $orderItem;
 			$context->getSession()->set( 'arcavias/orderid', $orderItem->getId() );
+
+			$view->orderItem = $orderItem;
+			$view->orderBasket = $basket;
 
 			$this->_process( $this->_subPartPath, $this->_subPartNames );
 
