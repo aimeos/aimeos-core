68faaee0addb1abaecf1527e286c8ac8abfcbf29
Norbert <n.sendetzky@metaways.de>
2013-07-15 17:17:08 +0200

    Fixes dependencies in setup tasks

--- lib/mshoplib/setup/unittest/PriceAddTestData.php
+++ lib/mshoplib/setup/unittest/PriceAddTestData.php
@@ -29,7 +29,7 @@ class MW_Setup_Task_PriceAddTestData extends MW_Setup_Task_Abstract
 	 */
 	public function getPostDependencies()
 	{
-		return array();
+		return array( 'CatalogRebuildTestIndex' );
 	}
 
 
