41e629838c1beb0c2d89aa8458e60338f1dbf21a
Norbert <n.sendetzky@metaways.de>
2013-09-24 17:50:34 +0200

    Implements empty url view helper

---
+++ lib/mwlib/tests/MW/View/Helper/Url/NoneTest.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * @copyright Copyright (c) Metaways Infosystems GmbH, 2013
+ * @license LGPLv3, http://www.gnu.org/licenses/lgpl.html
+ */
+
+
+/**
+ * Test class for MW_View_Helper_Url_None.
+ */
+class MW_View_Helper_Url_NoneTest extends MW_Unittest_Testcase
+{
+	private $_object;
+
+
+	/**
+	 * Runs the test methods of this class.
+	 *
+	 * @access public
+	 * @static
+	 */
+	public static function main()
+	{
+		$suite  = new PHPUnit_Framework_TestSuite('MW_View_Helper_Url_None');
+		$result = PHPUnit_TextUI_TestRunner::run($suite);
+	}
+
+
+	/**
+	 * Sets up the fixture, for example, opens a network connection.
+	 * This method is called before a test is executed.
+	 *
+	 * @access protected
+	 */
+	protected function setUp()
+	{
+		$view = new MW_View_Default();
+		$this->_object = new MW_View_Helper_Url_None( $view );
+	}
+
+
+	/**
+	 * Tears down the fixture, for example, closes a network connection.
+	 * This method is called after a test is executed.
+	 *
+	 * @access protected
+	 */
+	protected function tearDown()
+	{
+		$this->_object = null;
+	}
+
+
+	public function testTransform()
+	{
+		$params = array( 'plain' => 1, 'multi' => array( 'sub' => true ) );
+		$trailing = array( 'some', 'nice', 'text' );
+
+		$this->assertEquals( '', $this->_object->transform( 'module', 'test', 'index', $params, $trailing ) );
+	}
+
+}
