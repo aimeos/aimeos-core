3466c99198b399ed95e6114b114ce0a8b70ac95f
doleiynyk <doleiynyk@metaways.de>
2013-11-12 20:36:10 +0100

    Renamed files

---
+++ controller/extjs/src/Controller/ExtJS/Product/Export/Text/Excel.php
@@ -0,0 +1,360 @@
+<?php
+
+/**
+ * @copyright Copyright (c) Metaways Infosystems GmbH, 2011
+ * @license LGPLv3, http://www.arcavias.com/en/license
+ * @package Controller
+ * @subpackage ExtJS
+ */
+
+
+
+/**
+ * ExtJS product text export controller for admin interfaces.
+ *
+ * @package Controller
+ * @subpackage ExtJS
+ */
+class Controller_ExtJS_Product_Export_Text_Excel
+	extends Controller_ExtJS_Common_Load_Text_Abstract
+	implements Controller_ExtJS_Common_Load_Text_Interface
+{
+	private $_sheetLine = 1;
+
+
+	/**
+	 * Initializes the controller.
+	 *
+	 * @param MShop_Context_Item_Interface $context MShop context object
+	 */
+	public function __construct( MShop_Context_Item_Interface $context )
+	{
+		parent::__construct( $context, 'Product_Export_Text' );
+	}
+
+
+	/**
+	 * Creates a XLS file with all attribute texts and outputs it directly.
+	 *
+	 * @param stdClass $params Object containing the properties, e.g. the list of product IDs
+	 */
+	public function createHttpOutput( stdClass $params )
+	{
+		$this->_checkParams( $params, array( 'site', 'items' ) );
+		$this->_setLocale( $params->site );
+
+		$items = ( !is_array( $params->items ) ? array( $params->items ) : $params->items );
+		$lang = ( property_exists( $params, 'lang' ) && is_array( $params->lang ) ? $params->lang : array() );
+
+		$this->_getContext()->getLogger()->log( sprintf( 'Create export for product IDs: %1$s', implode( ',', $items ) ), MW_Logger_Abstract::DEBUG );
+
+		@header('Content-Type: application/vnd.ms-excel');
+		@header('Content-Disposition: attachment; filename=arcavias-product-texts.xls');
+		@header('Cache-Control: max-age=0');
+
+		$phpExcel = $this->_createDocument( $items, $lang );
+		$objWriter = PHPExcel_IOFactory::createWriter($phpExcel, 'Excel5');
+		$objWriter->save('php://output');
+	}
+
+
+	/**
+	 * Creates a new job to export an excel file.
+	 *
+	 * @param stdClass $params Object containing the properties, e.g. the list of product IDs
+	 */
+	public function createJob( stdClass $params )
+	{
+		$this->_checkParams( $params, array( 'site', 'items' ) );
+		$this->_setLocale( $params->site );
+
+		$config = $this->_getContext()->getConfig();
+		$dir = $config->get( 'controller/extjs/product/export/text/default/exportdir', 'uploads' );
+
+		$items = (array) $params->items;
+		$lang = ( property_exists( $params, 'lang' ) ) ? (array) $params->lang : array();
+
+		$languages = ( count( $lang ) > 0 ) ? implode( $lang, '-' ) : 'all';
+
+		$result = (object) array(
+			'site' => $params->site,
+			'items' => array(
+				(object) array(
+					'job.label' => 'Product text export: '. $languages,
+					'job.method' => 'Product_Export_Text.exportFile',
+					'job.parameter' => array(
+						'site' => $params->site,
+						'items' => $items,
+						'lang' => $params->lang,
+					),
+					'job.status' => 1,
+				),
+			),
+		);
+
+		$jobController = Controller_ExtJS_Admin_Job_Factory::createController( $this->_getContext() );
+		$jobController->saveItems( $result );
+
+		return array(
+			'items' => $items,
+			'success' => true,
+		);
+	}
+
+
+	/**
+	 * Create an excel file in the filesystem.
+	 *
+	 * @param stdClass $params Object containing the properties, e.g. the list of product IDs
+	 */
+	public function exportFile( stdClass $params )
+	{
+		$this->_checkParams( $params, array( 'site', 'items' ) );
+		$this->_setLocale( $params->site );
+
+		$items = (array) $params->items;
+		$lang = ( property_exists( $params, 'lang' ) ) ? (array) $params->lang : array();
+
+		$config = $this->_getContext()->getConfig();
+		$dir = $config->get( 'controller/extjs/product/export/text/default/exportdir', 'uploads' );
+		$perms = $config->get( 'controller/extjs/product/export/text/default/dirperms', 0775 );
+
+		if( is_dir( $dir ) === false && mkdir( $dir, $perms, true ) === false ) {
+			throw new Controller_ExtJS_Exception( sprintf( 'Couldn\'t create directory "%1$s" with permissions "%2$o"', $dir, $perms ) );
+		}
+
+		$filename = 'product-text-export_' . date('Y-m-d') . '_' . md5( time() . getmypid() ) .'.xls';
+
+		$this->_getContext()->getLogger()->log( sprintf( 'Create export file for product IDs: %1$s', implode( ',', $items ) ), MW_Logger_Abstract::DEBUG );
+
+		$phpExcel = $this->_createDocument( $items, $lang );
+		$objWriter = PHPExcel_IOFactory::createWriter($phpExcel, 'Excel5');
+		$objWriter->save( $dir . DIRECTORY_SEPARATOR . $filename );
+
+		$downloadFile = $config->get( 'controller/extjs/product/export/text/default/downloaddir', 'uploads' ) . DIRECTORY_SEPARATOR . $filename;
+
+		return array(
+			'file' => '<a href="'.$downloadFile.'">Download</a>',
+		);
+	}
+
+
+	/**
+	 * Returns the service description of the class.
+	 * It describes the class methods and its parameters including their types
+	 *
+	 * @return array Associative list of class/method names, their parameters and types
+	 */
+	public function getServiceDescription()
+	{
+		return array(
+			'Product_Export_Text.createHttpOutput' => array(
+				"parameters" => array(
+					array( "type" => "string","name" => "site","optional" => false ),
+					array( "type" => "array","name" => "items","optional" => false ),
+					array( "type" => "array","name" => "lang","optional" => true ),
+				),
+				"returns" => "",
+			),
+		);
+	}
+
+
+	/**
+	 * Creates a new PHPExcel document object.
+	 *
+	 * @param array $ids List of item IDs that should be part of the document
+	 * @param array $lang List of languages to export (empty array for all)
+	 * @return PHPExcel Document object
+	 */
+	protected function _createDocument( array $ids, array $lang )
+	{
+		$phpExcel = new PHPExcel();
+		$phpExcel->removeSheetByIndex( 0 );
+
+		$sheet = $phpExcel->createSheet();
+
+
+		$actualLangid = $this->_getContext()->getLocale()->getLanguageId();
+
+		$phpExcel->getProperties()
+			->setCreator( 'Arcavias' )
+			->setLastModifiedBy( 'Arcavias export' )
+			->setTitle( 'Arcavias product text export' )
+			->setSubject( 'Arcavias product text export' )
+			->setDescription( 'Export file for all product texts' )
+			->setKeywords( 'export product text translation' );
+
+
+		$manager = MShop_Locale_Manager_Factory::createManager( $this->_getContext() );
+		$globalLanguageManager = $manager->getSubManager( 'language' );
+
+		$search = $globalLanguageManager->createSearch();
+		$search->setSortations( array( $search->sort( '+', 'locale.language.id') ) );
+
+		if( !empty( $lang ) ) {
+			$search->setConditions( $search->compare( '==', 'locale.language.id', $lang ) );
+		}
+
+		$start = $temp = $total = 0;
+		$items = array();
+
+		do
+		{
+			$result = $globalLanguageManager->searchItems( $search, array(), $temp );
+			if( $temp ) { $total = $temp; $temp = null; }
+
+			foreach ( $result as $item )
+			{
+				$this->_getContext()->getLocale()->setLanguageId( $item->getId() );
+				$this->_addLanguage( $phpExcel, $item, $ids, $sheet );
+			}
+
+			$start += count( $result );
+			$search->setSlice( $start );
+		}
+		while( $start < $total );
+
+		$this->_getContext()->getLocale()->setLanguageId( $actualLangid );
+
+		return $phpExcel;
+	}
+
+
+	/**
+	 * Adds a new sheet for the given language to the document.
+	 *
+	 * @param PHPExel $phpExcel PHPExcel object
+	 * @param MShop_Locale_Item_Language_Interface $langItem Language item object
+	 * @param array $items List of of item ids whose texts should be added
+	 */
+	protected function _addLanguage( PHPExcel $phpExcel, MShop_Locale_Item_Language_Interface $langItem, array $ids, $sheet )
+	{
+		$sheet = $this->_createSheet( $sheet, $phpExcel, $langItem->getId() );
+
+		$manager = MShop_Product_Manager_Factory::createManager( $this->_getContext() );
+		$search = $manager->createSearch();
+
+		if( count( $ids ) > 0 ) {
+			$search->setConditions( $search->compare( '==', 'product.id', $ids ) );
+		}
+
+		$sort = array( $search->sort( '+', 'product.type.code' ), $search->sort( '+', 'product.code' ) );
+		$search->setSortations( $sort );
+
+		$start = $temp = $total = 0;
+		$items = array();
+
+		do
+		{
+			$result = $manager->searchItems( $search, array('text'), $temp );
+			if( $temp ) { $total = $temp; $temp = null; }
+
+			foreach( $result as $item ) {
+				$this->_addItem( $sheet, $langItem, $item );
+			}
+
+			$start += count( $result );
+			$search->setSlice( $start );
+		}
+		while( $start < $total );
+	}
+
+
+	/**
+	 * Adds all texts belonging to an product item to the given sheet.
+	 *
+	 * @param PHPExcel_Worksheet $sheet Worksheet where the texts will be added
+	 * @param MShop_Locale_Item_Language_Interface $langItem Language item object
+	 * @param MShop_Product_Item_Interface $item product item object
+	 * @param array $textMap Associative list of text type codes as key and list of text items
+	 */
+	protected function _addItem( PHPExcel_Worksheet $sheet, MShop_Locale_Item_Language_Interface $langItem,
+		MShop_Product_Item_Interface $item )
+	{
+		$listTypes = array();
+
+		foreach( $item->getListItems( 'text' ) as $listItem ) {
+			$listTypes[ $listItem->getRefId() ] = $listItem->getType();
+		}
+
+		foreach( $this->_getTextTypes( 'product' ) as $textTypeItem )
+		{
+			$textItems = $item->getRefItems( 'text', $textTypeItem->getCode() );
+
+			if( count( $textItems ) > 0 )
+			{
+				foreach( $textItems as $textItem )
+				{
+					$listType = ( isset( $listTypes[ $textItem->getId() ] ) ? $listTypes[ $textItem->getId() ] : '' );
+
+					$sheet->setCellValueByColumnAndRow( 0, $this->_sheetLine, $langItem->getId() );
+					$sheet->setCellValueByColumnAndRow( 1, $this->_sheetLine, $item->getType() );
+					$sheet->setCellValueByColumnAndRow( 2, $this->_sheetLine, $item->getCode() );
+					$sheet->setCellValueByColumnAndRow( 3, $this->_sheetLine, $listType );
+					$sheet->setCellValueByColumnAndRow( 4, $this->_sheetLine, $textTypeItem->getCode() );
+
+					// use language of the text item because it may be null
+					if( ( $textItem->getLanguageId() == $langItem->getId() || is_null( $textItem->getLanguageId() ) )
+						&& $textItem->getTypeId() == $textTypeItem->getId() )
+					{
+						$sheet->setCellValueByColumnAndRow( 0, $this->_sheetLine, $textItem->getLanguageId() );
+						$sheet->setCellValueByColumnAndRow( 5, $this->_sheetLine, $textItem->getId() );
+						$sheet->setCellValueByColumnAndRow( 6, $this->_sheetLine, $textItem->getContent() );
+					}
+
+					$this->_sheetLine++;
+				}
+			}
+			else
+			{
+				$sheet->setCellValueByColumnAndRow( 0, $this->_sheetLine, $langItem->getId() );
+				$sheet->setCellValueByColumnAndRow( 1, $this->_sheetLine, $item->getType() );
+				$sheet->setCellValueByColumnAndRow( 2, $this->_sheetLine, $item->getCode() );
+				$sheet->setCellValueByColumnAndRow( 3, $this->_sheetLine, 'default' );
+				$sheet->setCellValueByColumnAndRow( 4, $this->_sheetLine, $textTypeItem->getCode() );
+
+				$this->_sheetLine++;
+			}
+		}
+	}
+
+
+	/**
+	 * Creates a new worksheet that will be attached to the given document.
+	 *
+	 * @param PHPExcel $phpExcel Document object
+	 * @param string $title Title of the sheet
+	 * @return PHPExcel_Worksheet New worksheet attached to the document
+	 */
+	protected function _createSheet( $sheet, PHPExcel $phpExcel, $title )
+	{
+		$sheet->setTitle( $title );
+
+		$style = $sheet->getDefaultStyle();
+		$style->getAlignment()->setWrapText(true);
+		$style->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_LEFT);
+
+		$sheet->getStyle('A'.$this->_sheetLine.':'.'G'.$this->_sheetLine)->getFont()->setBold(true);
+
+		$sheet->getColumnDimension('A')->setAutoSize(true);
+		$sheet->getColumnDimension('B')->setAutoSize(true);
+		$sheet->getColumnDimension('C')->setAutoSize(true);
+		$sheet->getColumnDimension('D')->setAutoSize(true);
+		$sheet->getColumnDimension('E')->setAutoSize(true);
+		$sheet->getColumnDimension('F')->setAutoSize(true);
+		$sheet->getColumnDimension('G')->setWidth(60);
+
+		$sheet->setCellValueByColumnAndRow( 0, $this->_sheetLine, 'Language ID' );
+		$sheet->setCellValueByColumnAndRow( 1, $this->_sheetLine, 'Product type' );
+		$sheet->setCellValueByColumnAndRow( 2, $this->_sheetLine, 'Product code');
+		$sheet->setCellValueByColumnAndRow( 3, $this->_sheetLine, 'List type');
+		$sheet->setCellValueByColumnAndRow( 4, $this->_sheetLine, 'Text type');
+		$sheet->setCellValueByColumnAndRow( 5, $this->_sheetLine, 'Text ID');
+		$sheet->setCellValueByColumnAndRow( 6, $this->_sheetLine, 'Text');
+
+		$this->_sheetLine++;
+
+		return $sheet;
+	}
+}
---
+++ controller/extjs/src/Controller/ExtJS/Product/Import/Text/Excel.php
@@ -0,0 +1,170 @@
+<?php
+
+/**
+ * @copyright Copyright (c) Metaways Infosystems GmbH, 2011
+ * @license LGPLv3, http://www.arcavias.com/en/license
+ * @package Controller
+ * @subpackage ExtJS
+ */
+
+
+
+/**
+ * ExtJS product text import controller for admin interfaces.
+ *
+ * @package Controller
+ * @subpackage ExtJS
+ */
+class Controller_ExtJS_Product_Import_Text_Excel
+	extends Controller_ExtJS_Common_Load_Text_Abstract
+	implements Controller_ExtJS_Common_Load_Text_Interface
+{
+	/**
+	 * Initializes the controller.
+	 *
+	 * @param MShop_Context_Item_Interface $context MShop context object
+	 */
+	public function __construct( MShop_Context_Item_Interface $context )
+	{
+		parent::__construct( $context, 'Product_Import_Text' );
+	}
+
+
+	/**
+	 * Uploads a XLS file with all product texts.
+	 *
+	 * @param stdClass $params Object containing the properties
+	 */
+	public function uploadFile( stdClass $params )
+	{
+		$this->_checkParams( $params, array( 'site' ) );
+		$this->_setLocale( $params->site );
+
+		if( ( $fileinfo = reset( $_FILES ) ) === false ) {
+			throw new Controller_ExtJS_Exception( 'No file was uploaded' );
+		}
+
+		$config = $this->_getContext()->getConfig();
+		$dir = $config->get( 'controller/extjs/product/import/text/default/uploaddir', 'uploads' );
+
+		if( $config->get( 'controller/extjs/product/import/text/default/enablecheck', true ) ) {
+			$this->_checkFileUpload( $fileinfo['tmp_name'], $fileinfo['error'] );
+		}
+
+		$fileext = pathinfo( $fileinfo['name'], PATHINFO_EXTENSION );
+		$dest = $dir . DIRECTORY_SEPARATOR . md5( $fileinfo['name'] . time() . getmypid() ) . '.' . $fileext;
+
+		if( rename( $fileinfo['tmp_name'], $dest ) !== true )
+		{
+			$msg = sprintf( 'Uploaded file could not be moved to upload directory "%1$s"', $dir );
+			throw new Controller_ExtJS_Exception( $msg );
+		}
+
+		$perms = $config->get( 'controller/extjs/product/import/text/default/fileperms', 0660 );
+		if( chmod( $dest, $perms ) !== true )
+		{
+			$msg = sprintf( 'Could not set permissions "%1$s" for file "%2$s"', $perms, $dest );
+			throw new Controller_ExtJS_Exception( $msg );
+		}
+
+		$result = (object) array(
+			'site' => $params->site,
+			'items' => array(
+				(object) array(
+					'job.label' => 'Product text import: ' . $fileinfo['name'],
+					'job.method' => 'Product_Import_Text.importFile',
+					'job.parameter' => array(
+						'site' => $params->site,
+						'items' => $dest,
+					),
+					'job.status' => 1,
+				),
+			),
+		);
+
+		$jobController = Controller_ExtJS_Admin_Job_Factory::createController( $this->_getContext() );
+		$jobController->saveItems( $result );
+
+		return array(
+			'items' => $dest,
+			'success' => true,
+		);
+	}
+
+
+	/**
+	 * Imports a XLS file with all product texts.
+	 *
+	 * @param stdClass $params Object containing the properties
+	 */
+	public function importFile( stdClass $params )
+	{
+		$this->_checkParams( $params, array( 'site', 'items' ) );
+		$this->_setLocale( $params->site );
+
+		$items = ( !is_array( $params->items ) ? array( $params->items ) : $params->items );
+
+		foreach( $items as $entry )
+		{
+			$this->_importFile( $entry );
+			unlink( $entry );
+		}
+
+		return array(
+			'success' => true,
+		);
+	}
+
+
+	/**
+	 * Returns the service description of the class.
+	 * It describes the class methods and its parameters including their types
+	 *
+	 * @return array Associative list of class/method names, their parameters and types
+	 */
+	public function getServiceDescription()
+	{
+		return array(
+			'Product_Import_Text.uploadFile' => array(
+				"parameters" => array(
+					array( "type" => "string","name" => "site","optional" => false ),
+				),
+				"returns" => "",
+			),
+			'Product_Import_Text.importFile' => array(
+				"parameters" => array(
+					array( "type" => "string","name" => "site","optional" => false ),
+					array( "type" => "array","name" => "items","optional" => false ),
+				),
+				"returns" => "",
+			),
+		);
+	}
+
+
+	/**
+	 * Imports a file that can be understood by PHPExcel.
+	 *
+	 * @param string $path Path to file for importing
+	 */
+	protected function _importFile( $path )
+	{
+		$type = PHPExcel_IOFactory::identify( $path );
+		$reader = PHPExcel_IOFactory::createReader( $type );
+		$reader->setReadDataOnly( true );
+		$phpExcel = $reader->load( $path );
+
+		$textTypeMap = array();
+		foreach( $this->_getTextTypes( 'product' ) as $item ) {
+			$textTypeMap[ $item->getCode() ] = $item->getId();
+		}
+
+		$manager = MShop_Product_Manager_Factory::createManager( $this->_getContext() );
+
+		foreach( $phpExcel->getWorksheetIterator() as $sheet )
+		{
+			$itemTextMap = $this->_importTextsFromXLS( $sheet, $textTypeMap, 'product' );
+			$this->_importReferences( $manager, $itemTextMap, 'product' );
+		}
+	}
+}
