e320b2bd4e9f810d2cf4da5dde241c267c520155
Norbert <n.sendetzky@metaways.de>
2013-12-01 12:46:30 +0100

    Allows more than one config directory

--- setup.php
+++ setup.php
@@ -85,7 +85,6 @@ try
 		usage();
 	}
 
-
 	spl_autoload_register( 'setup_autoload' );
 
 	require 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
@@ -106,7 +105,7 @@ try
 
 	$confPaths = $arcavias->getConfigPaths( 'mysql' );
 	if( isset( $options['config'] ) ) {
-		$confPaths[] = $options['config'];
+		$confPaths = array_merge( $confPaths, (array) $options['config'] );
 	}
 
 	$conf = new MW_Config_Array( array(), $confPaths );
