bf9c264930562d4cd7b32347f9f4c0aa0832432d
Norbert <n.sendetzky@metaways.de>
2013-12-19 18:43:47 +0100

    Implements new job for sending e-mails for payment status changes

---
+++ controller/jobs/src/Controller/Jobs/Order/Email/Payment/Factory.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * @copyright Copyright (c) Metaways Infosystems GmbH, 2013
+ * @license LGPLv3, http://www.arcavias.com/en/license
+ * @package Controller
+ * @subpackage Order
+ */
+
+
+/**
+ * Order payment e-mail controller factory.
+ *
+ * @package Controller
+ * @subpackage Order
+ */
+class Controller_Jobs_Order_Email_Payment_Factory
+	extends Controller_Jobs_Common_Factory_Abstract
+	implements Controller_Jobs_Common_Factory_Interface
+{
+	/**
+	 * Creates a new controller specified by the given name.
+	 *
+	 * @param MShop_Context_Item_Interface $context Context object required by controllers
+	 * @param Arcavias $arcavias Arcavias object
+	 * @param string|null $name Name of the controller or "Default" if null
+	 * @return Controller_Jobs_Interface New controller object
+	 */
+	public static function createController( MShop_Context_Item_Interface $context, Arcavias $arcavias, $name = null )
+	{
+		if ( $name === null ) {
+			$name = $context->getConfig()->get('classes/controller/jobs/order/email/payment/name', 'Default');
+		}
+
+		if ( ctype_alnum($name) === false )
+		{
+			$classname = is_string($name) ? 'Controller_Jobs_Order_Email_Payment_' . $name : '<not a string>';
+			throw new Controller_Jobs_Exception( sprintf( 'Invalid characters in class name "%1$s"', $classname ) );
+		}
+
+		$iface = 'Controller_Jobs_Interface';
+		$classname = 'Controller_Jobs_Order_Email_Payment_' . $name;
+
+		$controller = self::_createController( $context, $arcavias, $classname, $iface );
+		return self::_addControllerDecorators( $context, $arcavias, $controller, 'order/email/payment' );
+	}
+}
