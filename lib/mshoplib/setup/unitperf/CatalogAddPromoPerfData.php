b49a3742619eaef1aebd48fcf1844f21a59201f4
Norbert <n.sendetzky@metaways.de>
2014-01-03 15:41:29 +0100

    Generates promotion and suggestion data for performance test site

---
+++ lib/mshoplib/setup/unitperf/CatalogAddPromoPerfData.php
@@ -0,0 +1,116 @@
+<?php
+
+/**
+ * @copyright Copyright (c) Metaways Infosystems GmbH, 2014
+ * @license LGPLv3, http://www.arcavias.com/en/license
+ */
+
+
+/**
+ * Adds product promotion performance records to catalog list table.
+ */
+class MW_Setup_Task_CatalogAddPromoPerfData extends MW_Setup_Task_ProductAddBasePerfData
+{
+	/**
+	 * Returns the list of task names which this task depends on.
+	 *
+	 * @return array List of task names
+	 */
+	public function getPreDependencies()
+	{
+		return array( 'CatalogAddProductPerfData' );
+	}
+
+
+	/**
+	 * Returns the list of task names which depends on this task.
+	 *
+	 * @return array List of task names
+	 */
+	public function getPostDependencies()
+	{
+		return array( 'CatalogRebuildPerfIndex' );
+	}
+
+
+	/**
+	 * Inserts catalog promotion products.
+	 */
+	protected function _process()
+	{
+		$this->_msg( 'Adding catalog promotion performance data', 0 );
+
+
+		$context =  $this->_getContext();
+
+		$catalogManager = MShop_Catalog_Manager_Factory::createManager( $context );
+		$catalogListManager = $catalogManager->getSubManager( 'list' );
+		$catalogListTypeManager = $catalogListManager->getSubManager( 'type' );
+
+
+		$search = $catalogListTypeManager->createSearch();
+		$expr = array(
+			$search->compare( '==', 'catalog.list.type.domain', 'product' ),
+			$search->compare( '==', 'catalog.list.type.code', 'promotion' ),
+		);
+		$search->setConditions( $search->combine( '&&', $expr ) );
+		$types = $catalogListTypeManager->searchItems( $search );
+
+		if ( ( $typeItem = reset( $types ) ) === false ) {
+			throw new Exception( 'Catalog list type item not found' );
+		}
+
+
+		$search = $catalogManager->createSearch();
+		$search->setSortations( array( $search->sort( '+', 'catalog.level' ), $search->sort( '+', 'catalog.left' ) ) );
+
+
+		$listItem = $catalogListManager->createItem();
+		$listItem->setTypeId( $typeItem->getId() );
+		$listItem->setDomain( 'product' );
+
+
+		$start = 0;
+
+		do
+		{
+			$this->_txBegin();
+
+			$result = $catalogManager->searchItems( $search );
+
+			foreach( $result as $catId => $catItem )
+			{
+				$pos = 0;
+
+				$search = $catalogListManager->createSearch();
+				$expr = array(
+					$search->compare( '==', 'catalog.list.parentid', $catId ),
+					$search->compare( '==', 'catalog.list.position', array( 20, 40, 60, 80 ) ),
+					$search->compare( '==', 'catalog.list.domain', 'product' ),
+					$search->compare( '==', 'catalog.list.type.code', 'default' ),
+				);
+				$search->setConditions( $search->combine( '&&', $expr ) );
+
+				foreach( $catalogListManager->searchItems( $search ) as $item )
+				{
+					$listItem->setId( null );
+					$listItem->setParentId( $item->getParentId() );
+					$listItem->setRefId( $item->getRefId() );
+					$listItem->setPosition( $pos++ );
+
+					$catalogListManager->saveItem( $listItem, false );
+				}
+			}
+
+			$count = count( $result );
+			$start += $count;
+			$search->setSlice( $start );
+
+			$this->_txCommit();
+		}
+		while( $count == $search->getSliceSize() );
+
+
+		$this->_status( 'done' );
+	}
+}
---
+++ lib/mshoplib/setup/unitperf/ProductAddSuggestPerfData.php
@@ -0,0 +1,158 @@
+<?php
+
+/**
+ * @copyright Copyright (c) Metaways Infosystems GmbH, 2014
+ * @license LGPLv3, http://www.arcavias.com/en/license
+ */
+
+
+/**
+ * Adds suggestion performance records to products.
+ */
+class MW_Setup_Task_ProductAddSuggestPerfData extends MW_Setup_Task_Abstract
+{
+	public function __construct( MW_Setup_DBSchema_Interface $schema, MW_DB_Connection_Interface $conn, $additional = null )
+	{
+		$iface = 'MShop_Context_Item_Interface';
+		if( !( $additional instanceof $iface ) ) {
+			throw new MW_Setup_Exception( sprintf( 'Additionally provided object is not of type "%1$s"', $iface ) );
+		}
+
+		parent::__construct( $schema, $conn, $additional );
+	}
+
+
+	/**
+	 * Returns the list of task names which this task depends on.
+	 *
+	 * @return array List of task names
+	 */
+	public function getPreDependencies()
+	{
+		return array( 'ProductAddBasePerfData', 'ProductAddSelectPerfData' );
+	}
+
+
+	/**
+	 * Returns the list of task names which depends on this task.
+	 *
+	 * @return array List of task names
+	 */
+	public function getPostDependencies()
+	{
+		return array( 'CatalogRebuildPerfIndex' );
+	}
+
+
+	/**
+	 * Executes the task for MySQL databases.
+	 */
+	protected function _mysql()
+	{
+		$this->_process();
+	}
+
+
+	/**
+	 * Insert product data.
+	 */
+	protected function _process()
+	{
+		$this->_msg( 'Adding product suggestion performance data', 0 );
+
+
+		$productManager = MShop_Product_Manager_Factory::createManager( $this->_getContext() );
+		$productListManager = $productManager->getSubManager( 'list' );
+		$productListTypeManager = $productListManager->getSubManager( 'type' );
+
+		$expr = array();
+		$search = $productListTypeManager->createSearch();
+		$expr[] = $search->compare('==', 'product.list.type.code', 'suggestion');
+		$expr[] = $search->compare('==', 'product.list.type.domain', 'product');
+		$search->setConditions( $search->combine( '&&', $expr ) );
+		$types = $productListTypeManager->searchItems($search);
+
+		if ( ($listTypeItem = reset($types)) === false) {
+			throw new Exception('Product list type item not found');
+		}
+
+		$search = $productManager->createSearch();
+		$search->setSortations( array( $search->sort( '+', 'product.id' ) ) );
+
+		$refsearch = $productManager->createSearch();
+		$refsearch->setSortations( array( $refsearch->sort( '+', 'product.id' ) ) );
+		$refsearch->setSlice( 1 );
+
+		$listItem = $productListManager->createItem();
+		$listItem->setTypeId( $listTypeItem->getId() );
+		$listItem->setDomain( 'product' );
+
+
+		$this->_txBegin();
+
+		$start = 0;
+
+		do
+		{
+			$num = 0;
+
+			$result = $productManager->searchItems( $search );
+			$refresult = $productManager->searchItems( $refsearch );
+
+			foreach( $result as $id => $product )
+			{
+				$pos = 0;
+				$length = ( $num % 4 ) + 3;
+
+				foreach( array_slice( $refresult, $num, $length, true ) as $refid => $refproduct )
+				{
+					$listItem->setId( null );
+					$listItem->setParentId( $id );
+					$listItem->setRefId( $refid );
+					$listItem->setPosition( $pos++ );
+
+					$productListManager->saveItem( $listItem, false );
+				}
+
+				$num++;
+			}
+
+			$count = count( $result );
+			$start += $count;
+			$search->setSlice( $start );
+			$refsearch->setSlice( $start + 1 );
+		}
+		while( $count == $search->getSliceSize() );
+
+		$this->_txCommit();
+
+
+		$this->_status( 'done' );
+	}
+
+
+	protected function _getContext()
+	{
+		return $this->_additional;
+	}
+
+
+	protected function _txBegin()
+	{
+		$dbm = $this->_additional->getDatabaseManager();
+
+		$conn = $dbm->acquire();
+		$conn->begin();
+		$dbm->release( $conn );
+	}
+
+
+	protected function _txCommit()
+	{
+		$dbm = $this->_additional->getDatabaseManager();
+
+		$conn = $dbm->acquire();
+		$conn->commit();
+		$dbm->release( $conn );
+	}
+}
