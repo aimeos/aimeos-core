8d53e3b47abae1be498c51722a4f865cebd2a71e
Norbert <n.sendetzky@metaways.de>
2014-01-05 14:34:20 +0100

    Revert "Merge pull request #360 from doleiynyk/2013-12-03_Saving_items_after_exception_fails_fix"
    This reverts commit 1e4aa820ea2342a822613c4f80e07ac9d954c09b, reversing
    changes made to 77e4b99f32220a4017e438a053b2c3bf7977e06b.

--- client/extjs/src/panel/AbstractItemUi.js
+++ client/extjs/src/panel/AbstractItemUi.js
@@ -225,7 +225,6 @@ MShop.panel.AbstractItemUi = Ext.extend(Ext.Window, {
 	onStoreException: function(proxy, type, action, options, response) {
 		if (/*itwasus &&*/ this.isSaveing) {
 			this.isSaveing = false;
-			this.store.removeAt(this.store.getCount()-1);
 			this.saveMask.hide();
 		}
 	},
