c4262d367924eaeb9898e31e3bf65b6b50e30f2f
Michael Spahn <m.spahn@metaways.de>
2014-01-15 13:49:59 +0100

    Don't manipulate the phantom property
    Converts tabs to spaces as well according to style guide

--- client/extjs/src/panel/ListItemListUi.js
+++ client/extjs/src/panel/ListItemListUi.js
@@ -7,232 +7,231 @@
 Ext.ns('MShop.panel');
 
 MShop.panel.ListItemListUi = Ext.extend(MShop.panel.AbstractListUi, {
-	/**
-	 * @cfg {String} domain
-	 */
-	domain: null,
+    /**
+     * @cfg {String} domain
+     */
+    domain: null,
 
-	/**
-	 * @cfg {Function} getAdditionalColumns
-	 */
-	getAdditionalColumns: Ext.emptyFn,
+    /**
+     * @cfg {Function} getAdditionalColumns
+     */
+    getAdditionalColumns: Ext.emptyFn,
 
-	/**
-	 * @property MShop.panel.AbstractItemUi itemUi
-	 * parent itemUi this listpanel is child of
-	 */
-	itemUi: null,
+    /**
+     * @property MShop.panel.AbstractItemUi itemUi
+     * parent itemUi this listpanel is child of
+     */
+    itemUi: null,
 
-	/**
-	 * @property MShop.panel.AbstractListItemPickerUi listItemPickerUi
-	 * parent listItemPickerUi this listpanel is agregated in
-	 */
-	listItemPickerUi: null,
+    /**
+     * @property MShop.panel.AbstractListItemPickerUi listItemPickerUi
+     * parent listItemPickerUi this listpanel is agregated in
+     */
+    listItemPickerUi: null,
 
-	itemUiXType: 'MShop.panel.listitemitemui',
+    itemUiXType: 'MShop.panel.listitemitemui',
 
-	initComponent: function() {
-		// remove filter + paging
-		this.gridConfig = this.gridConfig || {};
-		this.gridConfig.tbar = null;
-		this.gridConfig.bbar = null;
+    initComponent: function() {
+        // remove filter + paging
+        this.gridConfig = this.gridConfig || {};
+        this.gridConfig.tbar = null;
+        this.gridConfig.bbar = null;
 
-		this.autoExpandColumn = 'refcontent';
+        this.autoExpandColumn = 'refcontent';
 
-		// fetch ListItemPickerUi
-		this.listItemPickerUi = this.findParentBy(function(c){
-			return c.isXType(MShop.panel.AbstractListItemPickerUi, false);
-		});
+        // fetch ListItemPickerUi
+        this.listItemPickerUi = this.findParentBy(function(c){
+            return c.isXType(MShop.panel.AbstractListItemPickerUi, false);
+        });
 
-		// fetch ItemUI
-		this.itemUi = this.findParentBy(function(c){
-			return c.isXType(MShop.panel.AbstractItemUi, false);
-		});
-		this.itemUi.on('save', this.onItemUiSave, this);
+        // fetch ItemUI
+        this.itemUi = this.findParentBy(function(c){
+            return c.isXType(MShop.panel.AbstractItemUi, false);
+        });
+        this.itemUi.on('save', this.onItemUiSave, this);
 
-		MShop.panel.ListItemListUi.superclass.initComponent.call(this);
-		
-		this.grid.getView().getRowClass = function(record, rowIndex, rowParams, store) { 
-			if( ( status = record.get( this.listItemPickerUi.itemConfig.listNamePrefix + 'status' ) ) <= 0 ) {
-				return  'statustext-' + Number( status );
-			}
-			return '';
-		}.createDelegate(this);
-	},
+        MShop.panel.ListItemListUi.superclass.initComponent.call(this);
 
-	initStore: function() {
-		this.storeConfig = this.storeConfig || {};
-		this.storeConfig.remoteSort = false;
-		this.storeConfig.autoSave = false;
+        this.grid.getView().getRowClass = function(record, rowIndex, rowParams, store) {
+            if( ( status = record.get( this.listItemPickerUi.itemConfig.listNamePrefix + 'status' ) ) <= 0 ) {
+                return  'statustext-' + Number( status );
+            }
+            return '';
+        }.createDelegate(this);
+    },
 
-		MShop.panel.ListItemListUi.superclass.initStore.call(this);
+    initStore: function() {
+        this.storeConfig = this.storeConfig || {};
+        this.storeConfig.remoteSort = false;
+        this.storeConfig.autoSave = false;
 
-		this.store.on('load', this.onStoreLoad, this);
-		this.store.on('beforeload', this.setFilters, this);
-		this.store.on('write', this.onStoreWrite, this);
-		//this.store.on('exception', this.onStoreException, this);
+        MShop.panel.ListItemListUi.superclass.initStore.call(this);
 
-	},
+        this.store.on('load', this.onStoreLoad, this);
+        this.store.on('beforeload', this.setFilters, this);
+        this.store.on('write', this.onStoreWrite, this);
+        //this.store.on('exception', this.onStoreException, this);
+    },
 
-	onDestroy: function() {
-		this.store.un('load', this.onStoreLoad, this);
-		this.store.un('beforeload', this.setFilters, this);
-		this.store.un('write', this.onStoreWrite, this);
-		//this.store.un('exception', this.onStoreException, this);
+    onDestroy: function() {
+        this.store.un('load', this.onStoreLoad, this);
+        this.store.un('beforeload', this.setFilters, this);
+        this.store.un('write', this.onStoreWrite, this);
+        //this.store.un('exception', this.onStoreException, this);
 
-		MShop.panel.ListItemListUi.superclass.onDestroy.apply(this, arguments);
-	},
+        MShop.panel.ListItemListUi.superclass.onDestroy.apply(this, arguments);
+    },
 
-	onOpenEditWindow: function(action) {
-		if (action === 'add') {
-			return Ext.Msg.alert(_('Select Item'), _('Please select an item on the right side and add it via drag and drop to this list.'));
-		}
+    onOpenEditWindow: function(action) {
+        if (action === 'add') {
+            return Ext.Msg.alert(_('Select Item'), _('Please select an item on the right side and add it via drag and drop to this list.'));
+        }
 
-		return MShop.panel.ListItemListUi.superclass.onOpenEditWindow.apply(this, arguments);
-	},
+        return MShop.panel.ListItemListUi.superclass.onOpenEditWindow.apply(this, arguments);
+    },
 
-	onStoreLoad: function(store) {
-		this.store.sort(this.listItemPickerUi.itemConfig.listNamePrefix + 'position', 'ASC');
-	},
+    onStoreLoad: function(store) {
+        this.store.sort(this.listItemPickerUi.itemConfig.listNamePrefix + 'position', 'ASC');
+    },
 
-	onStoreWrite: function() {
-		this.returnTicket();
-	},
+    onStoreWrite: function() {
+        this.returnTicket();
+    },
 
-	onItemUiSave: function(itemUi, record, ticketFn) {
-		// make sure all parentid are set
-		this.store.each(function(r) {
-			// Remove list id if the reference should be copied
-			if (this.itemUi.action == 'copy') {
-				r.id = null;
-			}
-			r.set(this.listItemPickerUi.itemConfig.listNamePrefix + 'parentid', record.id);
-		}, this);
+    onItemUiSave: function(itemUi, record, ticketFn) {
+        // make sure all parentid are set
+        this.store.each(function(r) {
+            // Remove list id if the reference should be copied
+            if (this.itemUi.action == 'copy') {
+                r.id = null;
+            }
+            r.set(this.listItemPickerUi.itemConfig.listNamePrefix + 'parentid', record.id);
+        }, this);
 
-		if (this.store.save() !== -1) {
-			this.returnTicket = ticketFn();
-		}
-	},
+        if (this.store.save() !== -1) {
+            this.returnTicket = ticketFn();
+        }
+    },
 
-	setFilters: function(store, options) {
-		if (this.itemUi.record.phantom) {
-			// nothing to load
-			return false;
-		}
+    setFilters: function(store, options) {
+        if (this.itemUi.record.phantom && this.itemUi.action != 'copy') {
+            // nothing to load
+            return false;
+        }
 
-		// filter for refid
-		//var parentIdProp = this.listItemPickerUi.listNamePrefix + ''
-		var parentIdCriteria = {};
-		parentIdCriteria[this.listItemPickerUi.itemConfig.listNamePrefix + 'parentid'] = this.itemUi.record.id;
-		var domainCriteria = {};
-		domainCriteria[this.listItemPickerUi.itemConfig.listNamePrefix + 'domain'] = this.domain;
+        // filter for refid
+        //var parentIdProp = this.listItemPickerUi.listNamePrefix + ''
+        var parentIdCriteria = {};
+        parentIdCriteria[this.listItemPickerUi.itemConfig.listNamePrefix + 'parentid'] = this.itemUi.record.id;
+        var domainCriteria = {};
+        domainCriteria[this.listItemPickerUi.itemConfig.listNamePrefix + 'domain'] = this.domain;
 
-		options.params = options.params || {};
-		options.params.condition = {'&&': [
-			{'==': parentIdCriteria},
-			{'==': domainCriteria}
-		]};
+        options.params = options.params || {};
+        options.params.condition = {'&&': [
+            {'==': parentIdCriteria},
+            {'==': domainCriteria}
+        ]};
 
-		return true;
-	},
+        return true;
+    },
 
-	getColumns : function() {
-		var expr = this.listTypeCondition;
-		var storeConfig = {
-			baseParams: {
-				site: MShop.config.site["locale.site.code"],
-				condition: expr
-			}
-		};
-		this.itemTypeStore = MShop.GlobalStoreMgr.get(this.listTypeControllerName, this.listTypeKey, storeConfig);
+    getColumns : function() {
+        var expr = this.listTypeCondition;
+        var storeConfig = {
+            baseParams: {
+                site: MShop.config.site["locale.site.code"],
+                condition: expr
+            }
+        };
+        this.itemTypeStore = MShop.GlobalStoreMgr.get(this.listTypeControllerName, this.listTypeKey, storeConfig);
 
-		return [
-			{
-				xtype : 'gridcolumn',
-				dataIndex : this.listItemPickerUi.itemConfig.listNamePrefix + 'id',
-				header : _('Id'),
-				width : 50,
-				hidden : true
-			},
-			{
-				xtype : 'gridcolumn',
-				dataIndex : this.listItemPickerUi.itemConfig.listNamePrefix + 'refid',
-				header : _('Ref-Id'),
-				width : 50,
-				hidden : true
-			},
-			{
-				xtype : 'gridcolumn',
-				dataIndex : this.listItemPickerUi.itemConfig.listNamePrefix + 'status',
-				header : _('List Status'),
-				width : 50,
-				hidden : true,
-				align: 'center',
-				renderer : this.statusColumnRenderer.createDelegate(this)
-			},
-			{
-				xtype : 'gridcolumn',
-				dataIndex : this.listItemPickerUi.itemConfig.listNamePrefix + 'position',
-				header : _('Position'),
-				width : 50,
-				hidden : true
-			},
-			{
-				xtype : 'datecolumn',
-				dataIndex : this.listItemPickerUi.itemConfig.listNamePrefix + 'datestart',
-				header : _('Start date'),
-				width : 120,
-				format : 'Y-m-d H:i:s',
-				hidden : true
-			},
-			{
-				xtype : 'datecolumn',
-				dataIndex : this.listItemPickerUi.itemConfig.listNamePrefix + 'dateend',
-				header : _('End date'),
-				width : 120,
-				format : 'Y-m-d H:i:s',
-				hidden : true
-			},
-			{
-				xtype : 'gridcolumn',
-				dataIndex : this.listItemPickerUi.itemConfig.listNamePrefix + 'config',
-				header : _('Configuration'),
-				width : 200,
-				editable : false,
-				hidden : true,
-				renderer: function (value) {
-					var s = "";
-					Ext.iterate(value, function (key, value, object) {
-						s = s + String.format('<div>{0}: {1}</div>', key, value);
-					}, this);
-					return s;
-				}
-			},
-			{
-				xtype : 'datecolumn',
-				dataIndex : this.listItemPickerUi.itemConfig.listNamePrefix + 'ctime',
-				header : _('Created'),
-				width : 120,
-				format : 'Y-m-d H:i:s',
-				hidden : true
-			},
-			{
-				xtype : 'datecolumn',
-				dataIndex : this.listItemPickerUi.itemConfig.listNamePrefix + 'mtime',
-				header : _('Last modified'),
-				width : 120,
-				format : 'Y-m-d H:i:s',
-				hidden : true
-			},
-			{
-				xtype : 'gridcolumn',
-				dataIndex : this.listItemPickerUi.itemConfig.listNamePrefix + 'editor',
-				header : _('Editor'),
-				width : 50,
-				hidden : true
-			}
-		].concat(this.getAdditionalColumns() || []);
-	}
+        return [
+            {
+                xtype : 'gridcolumn',
+                dataIndex : this.listItemPickerUi.itemConfig.listNamePrefix + 'id',
+                header : _('Id'),
+                width : 50,
+                hidden : true
+            },
+            {
+                xtype : 'gridcolumn',
+                dataIndex : this.listItemPickerUi.itemConfig.listNamePrefix + 'refid',
+                header : _('Ref-Id'),
+                width : 50,
+                hidden : true
+            },
+            {
+                xtype : 'gridcolumn',
+                dataIndex : this.listItemPickerUi.itemConfig.listNamePrefix + 'status',
+                header : _('List Status'),
+                width : 50,
+                hidden : true,
+                align: 'center',
+                renderer : this.statusColumnRenderer.createDelegate(this)
+            },
+            {
+                xtype : 'gridcolumn',
+                dataIndex : this.listItemPickerUi.itemConfig.listNamePrefix + 'position',
+                header : _('Position'),
+                width : 50,
+                hidden : true
+            },
+            {
+                xtype : 'datecolumn',
+                dataIndex : this.listItemPickerUi.itemConfig.listNamePrefix + 'datestart',
+                header : _('Start date'),
+                width : 120,
+                format : 'Y-m-d H:i:s',
+                hidden : true
+            },
+            {
+                xtype : 'datecolumn',
+                dataIndex : this.listItemPickerUi.itemConfig.listNamePrefix + 'dateend',
+                header : _('End date'),
+                width : 120,
+                format : 'Y-m-d H:i:s',
+                hidden : true
+            },
+            {
+                xtype : 'gridcolumn',
+                dataIndex : this.listItemPickerUi.itemConfig.listNamePrefix + 'config',
+                header : _('Configuration'),
+                width : 200,
+                editable : false,
+                hidden : true,
+                renderer: function (value) {
+                    var s = "";
+                    Ext.iterate(value, function (key, value, object) {
+                        s = s + String.format('<div>{0}: {1}</div>', key, value);
+                    }, this);
+                    return s;
+                }
+            },
+            {
+                xtype : 'datecolumn',
+                dataIndex : this.listItemPickerUi.itemConfig.listNamePrefix + 'ctime',
+                header : _('Created'),
+                width : 120,
+                format : 'Y-m-d H:i:s',
+                hidden : true
+            },
+            {
+                xtype : 'datecolumn',
+                dataIndex : this.listItemPickerUi.itemConfig.listNamePrefix + 'mtime',
+                header : _('Last modified'),
+                width : 120,
+                format : 'Y-m-d H:i:s',
+                hidden : true
+            },
+            {
+                xtype : 'gridcolumn',
+                dataIndex : this.listItemPickerUi.itemConfig.listNamePrefix + 'editor',
+                header : _('Editor'),
+                width : 50,
+                hidden : true
+            }
+        ].concat(this.getAdditionalColumns() || []);
+    }
 });
 
 
