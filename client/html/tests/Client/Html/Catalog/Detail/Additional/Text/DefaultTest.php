462a86d3e3aeb80059ce5fc6cde85df3d9977fce
Norbert <n.sendetzky@metaways.de>
2014-02-05 17:05:12 +0100

    Implements additional attribute section for catalog detail view

---
+++ client/html/tests/Client/Html/Catalog/Detail/Additional/Text/DefaultTest.php
@@ -0,0 +1,94 @@
+<?php
+
+/**
+ * @copyright Copyright (c) Metaways Infosystems GmbH, 2014
+ * @license LGPLv3, http://www.arcavias.com/en/license
+ */
+
+class Client_Html_Catalog_Detail_Additional_Text_DefaultTest extends MW_Unittest_Testcase
+{
+	private $_object;
+
+
+	/**
+	 * Runs the test methods of this class.
+	 *
+	 * @access public
+	 * @static
+	 */
+	public static function main()
+	{
+		require_once 'PHPUnit/TextUI/TestRunner.php';
+
+		$suite = new PHPUnit_Framework_TestSuite('Client_Html_Catalog_Detail_Additional_Text_DefaultTest');
+		$result = PHPUnit_TextUI_TestRunner::run($suite);
+	}
+
+
+	/**
+	 * Sets up the fixture, for example, opens a network connection.
+	 * This method is called before a test is executed.
+	 *
+	 * @access protected
+	 */
+	protected function setUp()
+	{
+		$paths = TestHelper::getHtmlTemplatePaths();
+		$this->_object = new Client_Html_Catalog_Detail_Additional_Text_Default( TestHelper::getContext(), $paths );
+		$this->_object->setView( TestHelper::getView() );
+	}
+
+
+	/**
+	 * Tears down the fixture, for example, closes a network connection.
+	 * This method is called after a test is executed.
+	 *
+	 * @access protected
+	 */
+	protected function tearDown()
+	{
+		unset( $this->_object );
+		Controller_Frontend_Factory::clear();
+		MShop_Factory::clear();
+	}
+
+
+	public function testGetHeader()
+	{
+		$view = $this->_object->getView();
+		$view->detailProductItem = $this->_getProductItem();
+
+		$output = $this->_object->getHeader();
+	}
+
+
+	public function testGetBody()
+	{
+		$view = $this->_object->getView();
+		$view->detailProductItem = $this->_getProductItem();
+
+		$output = $this->_object->getBody();
+		$this->assertContains( '<h2 class="header description">', $output );
+	}
+
+	public function testGetSubClient()
+	{
+		$this->setExpectedException( 'Client_Html_Exception' );
+		$this->_object->getSubClient( 'invalid', 'invalid' );
+	}
+
+
+	protected function _getProductItem()
+	{
+		$manager = MShop_Product_Manager_Factory::createManager( TestHelper::getContext() );
+		$search = $manager->createSearch();
+		$search->setConditions( $search->compare( '==', 'product.code', 'CNC' ) );
+		$items = $manager->searchItems( $search, array( 'text' ) );
+
+		if( ( $item = reset( $items ) ) === false ) {
+			throw new Exception( 'No product item with code "CNC" found' );
+		}
+
+		return $item;
+	}
+}
