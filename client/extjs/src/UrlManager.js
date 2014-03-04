bc05596dd4c537f32cb3848c8e41df977cde43b1
Michael Spahn <m.spahn@metaways.de>
2014-03-04 12:43:47 +0100

    Remove debug output and format code

--- client/extjs/src/UrlManager.js
+++ client/extjs/src/UrlManager.js
@@ -3,53 +3,49 @@
  * LGPLv3, http://www.arcavias.com/en/license
  */
 
-
 Ext.ns('MShop');
 
-MShop.UrlManager = function( href ) {
-		this.href = href || null;
-		this.tmpl = new Ext.Template( MShop.config.urlTemplate );
-		this.data = {
-			site: MShop.config.site['locale.site.code'],
-			locale: MShop.i18n.locale,
-			tab : MShop.config.activeTab
-		};
+MShop.UrlManager = function(href) {
+    this.href = href || null;
+    this.tmpl = new Ext.Template(MShop.config.urlTemplate);
+    this.data = {
+        site : MShop.config.site['locale.site.code'],
+        locale : MShop.i18n.locale,
+        tab : MShop.config.activeTab
+    };
 };
 
 MShop.UrlManager.prototype = {
-		redirect: function( config )
-		{
-			if( typeof config == 'object' )
-			{
-				if( config.hasOwnProperty( 'site' ) ) {
-					this.setSiteCode( config.site );
-				}
+    redirect : function(config) {
+        if ( typeof config == 'object') {
+            if (config.hasOwnProperty('site')) {
+                this.setSiteCode(config.site);
+            }
 
-				if( MShop.i18n.hasOwnProperty( 'locale' ) ) {
-                    this.setLanguageCode( config.locale );
-                }
+            if (MShop.i18n.hasOwnProperty('locale')) {
+                this.setLanguageCode(config.locale);
+            }
 
-				if( config.hasOwnProperty( 'tab' ) ) {
-					this.setActiveTab( config.tab );
-				}
-			}
-			window.location.href = this.tmpl.apply( this.data );
-		},
-
-		setActiveTab: function( value ) {
-			this.data.tab = parseInt( value, 10 );
-		},
-
-		getActiveTab: function() {
-			return this.data.tab;
-		},
-
-		setSiteCode: function( siteCode ) {
-			this.data.site = siteCode;
-		},
-
-		setLanguageCode: function( languageCode ) {
-            this.data.locale = languageCode;
-            console.log(languageCode);
+            if (config.hasOwnProperty('tab')) {
+                this.setActiveTab(config.tab);
+            }
         }
+        window.location.href = this.tmpl.apply(this.data);
+    },
+
+    setActiveTab : function(value) {
+        this.data.tab = parseInt(value, 10);
+    },
+
+    getActiveTab : function() {
+        return this.data.tab;
+    },
+
+    setSiteCode : function(siteCode) {
+        this.data.site = siteCode;
+    },
+
+    setLanguageCode : function(languageCode) {
+        this.data.locale = languageCode;
+    }
 };
--- client/extjs/src/elements/siteLanguage.js
+++ client/extjs/src/elements/siteLanguage.js
@@ -25,20 +25,20 @@ MShop.elements.siteLanguage.ComboBox = function(config) {
 
 /**
  * @static
- * 
+ *
  * @param {String} langId
  * @return {String} label
  */
 MShop.elements.siteLanguage.renderer = function(langId, metaData, record, rowIndex, colIndex, store) {
     var language = MShop.elements.siteLanguage.getStore().getById(langId);
 
-    if( language ) {
-        metaData.css = 'statustext-' + Number( language.get( 'locale.language.status' ) );
-        return language.get( 'locale.language.label' );
+    if (language) {
+        metaData.css = 'statustext-' + Number(language.get('locale.language.status'));
+        return language.get('locale.language.label');
     }
 
     metaData.css = 'statustext-1';
-    return langId || MShop.I18n.dt( 'client/extjs', 'All' );
+    return langId || MShop.I18n.dt('client/extjs', 'All');
 };
 
 Ext.extend(MShop.elements.siteLanguage.ComboBox, Ext.form.ComboBox, {
@@ -50,17 +50,12 @@ Ext.extend(MShop.elements.siteLanguage.ComboBox, Ext.form.ComboBox, {
     },
 
     onSiteLanguageSelect : function(ComboBox, language) {
-        var mainTabPanel = Ext.getCmp('MShop.MainTabPanel'),
-            activeTabPanel = mainTabPanel.getActiveTab(),
-            domainTabIdx = mainTabPanel.items.indexOf(activeTabPanel),
-            languageCode = language ? language.get('locale.language.code') : 'en';
+        var mainTabPanel = Ext.getCmp('MShop.MainTabPanel'), activeTabPanel = mainTabPanel.getActiveTab(), domainTabIdx = mainTabPanel.items.indexOf(activeTabPanel), languageCode = language ? language.get('locale.language.code') : 'en';
 
         new Ext.LoadMask(Ext.getBody(), {
             msg : MShop.I18n.dt('client/extjs', 'Switching site language ...')
         }).show();
-        
-        console.log(languageCode);
-        
+
         MShop.urlManager.redirect({
             locale : languageCode
         });
