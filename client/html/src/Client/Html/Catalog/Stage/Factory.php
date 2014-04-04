b0e33e3e06c2d65ada0083d023e835d26b657ff0
Norbert <n.sendetzky@metaways.de>
2014-04-04 19:30:10 +0200

    Adds documentation for HTML client configuration options

--- client/html/src/Client/Html/Catalog/Stage/Factory.php
+++ client/html/src/Client/Html/Catalog/Stage/Factory.php
@@ -29,6 +29,39 @@ class Client_Html_Catalog_Stage_Factory
 	 */
 	public static function createClient( MShop_Context_Item_Interface $context, array $templatePaths, $name = null )
 	{
+		/** classes/client/html/catalog/stage/name
+		 * Class name of the used catalog stage client implementation
+		 *
+		 * Each default HTML client can be replace by an alternative imlementation.
+		 * To use this implementation, you have to set the last part of the class
+		 * name as configuration value so the client factory knows which class it
+		 * has to instantiate.
+		 *
+		 * For example, if the name of the default class is
+		 *
+		 *  Client_Html_Catalog_Stage_Default
+		 *
+		 * and you want to replace it with your own version named
+		 *
+		 *  Client_Html_Catalog_Stage_Mystage
+		 *
+		 * then you have to set the this configuration option:
+		 *
+		 *  classes/client/html/catalog/stage/name = Mystage
+		 *
+		 * The value is the last part of your own class name and it's case sensitive,
+		 * so take care that the configuration value is exactly named like the last
+		 * part of the class name.
+		 *
+		 * The allowed characters of the class name are A-Z, a-z and 0-9. No other
+		 * characters are possible! You should always start the last part of the class
+		 * name with an upper case character and continue only with lower case characters
+		 * or numbers. Avoid chamel case names like "MyStage"!
+		 *
+		 * @param string Last part of the class name
+		 * @since 2014.03
+		 * @category Developer
+		 */
 		if( $name === null ) {
 			$name = $context->getConfig()->get( 'classes/client/html/catalog/stage/name', 'Default' );
 		}
--- client/html/src/Client/Html/Checkout/Confirm/Factory.php
+++ client/html/src/Client/Html/Checkout/Confirm/Factory.php
@@ -29,6 +29,39 @@ class Client_Html_Checkout_Confirm_Factory
 	 */
 	public static function createClient( MShop_Context_Item_Interface $context, array $templatePaths, $name = null )
 	{
+		/** classes/client/html/checkout/confirm/name
+		 * Class name of the used checkout confirm client implementation
+		 *
+		 * Each default HTML client can be replace by an alternative imlementation.
+		 * To use this implementation, you have to set the last part of the class
+		 * name as configuration value so the client factory knows which class it
+		 * has to instantiate.
+		 *
+		 * For example, if the name of the default class is
+		 *
+		 *  Client_Html_Checkout_Confirm_Default
+		 *
+		 * and you want to replace it with your own version named
+		 *
+		 *  Client_Html_Checkout_Confirm_Myconfirm
+		 *
+		 * then you have to set the this configuration option:
+		 *
+		 *  classes/client/html/checkout/confirm/name = Myconfirm
+		 *
+		 * The value is the last part of your own class name and it's case sensitive,
+		 * so take care that the configuration value is exactly named like the last
+		 * part of the class name.
+		 *
+		 * The allowed characters of the class name are A-Z, a-z and 0-9. No other
+		 * characters are possible! You should always start the last part of the class
+		 * name with an upper case character and continue only with lower case characters
+		 * or numbers. Avoid chamel case names like "MyConfirm"!
+		 *
+		 * @param string Last part of the class name
+		 * @since 2014.03
+		 * @category Developer
+		 */
 		if( $name === null ) {
 			$name = $context->getConfig()->get( 'classes/client/html/checkout/confirm/name', 'Default' );
 		}
