14a54371d5c423d70614951c69d687da6945a17f
Norbert <n.sendetzky@metaways.de>
2014-04-29 11:19:17 +0200

    Implements decorator for required products and improves BasketValues implementation

--- lib/mshoplib/src/MShop/Coupon/Provider/Decorator/BasketValues.php
+++ lib/mshoplib/src/MShop/Coupon/Provider/Decorator/BasketValues.php
@@ -21,28 +21,26 @@ class MShop_Coupon_Provider_Decorator_BasketValues
 	 * Checks for the min/max order value.
 	 *
 	 * @param MShop_Order_Item_Base_Interface $base Basic order of the customer
-	 * @param array $config Associative array with config content
 	 * @return boolean True if the basket matches the constraints, false if not
 	 */
 	public function isAvailable( MShop_Order_Item_Base_Interface $base )
 	{
-		$config = $this->_getItem()->getConfig();
-
-		$currency = $base->getPrice()->getCurrencyId();
 		$price = $base->getPrice();
+		$currency = $price->getCurrencyId();
 		$value = $price->getValue() + $price->getRebate();
 
-		if ( isset( $config['basketvalues.total-value-min'][ $currency ] ) &&
-			$config['basketvalues.total-value-min'][ $currency ] > $value ) {
+		$minvalue = $this->_getConfigValue( 'basketvalues.total-value-min', array() );
+
+		if( isset( $minvalue[$currency] ) && $minvalue[$currency] > $value ) {
 			return false;
 		}
 
-		if ( isset( $config['basketvalues.total-value-max'][ $currency ] ) &&
-			$config['basketvalues.total-value-max'][ $currency ] < $value ) {
+		$maxvalue = $this->_getConfigValue( 'basketvalues.total-value-max', array() );
+
+		if( isset( $maxvalue[$currency] ) && $maxvalue[$currency] < $value ) {
 			return false;
 		}
 
 		return parent::isAvailable( $base );
 	}
-
 }
---
+++ lib/mshoplib/src/MShop/Coupon/Provider/Decorator/Required.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * @copyright Copyright (c) Metaways Infosystems GmbH, 2014
+ * @license LGPLv3, http://www.arcavias.com/en/license
+ * @package MShop
+ * @subpackage Coupon
+ */
+
+
+/**
+ * Required decorator for coupon provider.
+ *
+ * @package MShop
+ * @subpackage Coupon
+ */
+class MShop_Coupon_Provider_Decorator_Required
+	extends MShop_Coupon_Provider_Decorator_Abstract
+{
+	/**
+	 * Checks for requirements.
+	 *
+	 * @param MShop_Order_Item_Base_Interface $base Basic order of the customer
+	 * @return boolean True if the requirements are met, false if not
+	 */
+	public function isAvailable( MShop_Order_Item_Base_Interface $base )
+	{
+		if( isset( $config['required.productcode'] ) )
+		{
+			foreach( $base->getProducts() as $product )
+			{
+				if( $product->getProductCode() == $config['required.productcode'] ) {
+					return parent::isAvailable( $base );
+				}
+			}
+
+			return false;
+		}
+
+		return true;
+	}
+}
