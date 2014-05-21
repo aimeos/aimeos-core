59ad048cb5afd9d4a7c7c831b17d2f9a252e05ed
Norbert <n.sendetzky@metaways.de>
2014-05-21 14:49:23 +0200

    Adapts performance test and demo data to always use a warehouse for stock levels

--- lib/mshoplib/setup/unitperf/ProductAddStockPerfData.php
+++ lib/mshoplib/setup/unitperf/ProductAddStockPerfData.php
@@ -43,14 +43,26 @@ class MW_Setup_Task_ProductAddStockPerfData extends MW_Setup_Task_ProductAddBase
 
 		$context =  $this->_getContext();
 
-		$productManager = MShop_Product_Manager_Factory::createManager( $context );
+		$productManager = MShop_Factory::createManager( $context, 'product' );
 		$productStockManager = $productManager->getSubManager( 'stock' );
+		$productWarehouseManager = $productStockManager->getSubManager( 'warehouse' );
+
+
+		$search = $productWarehouseManager->createSearch();
+		$search->setConditions( $search->compare( '==', 'product.stock.warehouse.code', 'default' ) );
+		$result = $productWarehouseManager->searchItems( $search );
+
+		if( ( $whItem = reset( $result ) ) === false ) {
+			throw new Exception( 'No warehouse with code "default" found' );
+		}
+
 
 		$item = $productStockManager->createItem();
 		$search = $productManager->createSearch();
 		$search->setSortations( array( $search->sort( '+', 'product.id' ) ) );
 
 		$start = 0;
+		$warehouseid = $whItem->getId();
 		$stocklevels = array( null, 100, 80, 60, 40, 20, 10, 5, 2, 0 );
 
 
@@ -64,6 +76,7 @@ class MW_Setup_Task_ProductAddStockPerfData extends MW_Setup_Task_ProductAddBase
 			{
 				$item->setId( null );
 				$item->setProductId( $id );
+				$item->setWarehouseId( $warehouseid );
 				$item->setStockLevel( current( $stocklevels ) );
 				$productStockManager->saveItem( $item );
 
