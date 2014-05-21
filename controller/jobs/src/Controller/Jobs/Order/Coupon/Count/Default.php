328873529c79f25addcd402e6b07877694c1ca81
Norbert <n.sendetzky@metaways.de>
2014-05-21 16:51:24 +0200

    Implements a job controller for handling unfinished orders
    The job controllers for decreasing the stock levels and coupon counts are needless now as blocking the resources is done when creating the order

--- controller/jobs/src/Controller/Jobs/Order/Coupon/Count/Default.php
+++ controller/jobs/src/Controller/Jobs/Order/Coupon/Count/Default.php
@@ -13,9 +13,10 @@
  *
  * @package Controller
  * @subpackage Jobs
+ * @deprecated Use Controller_Jobs_Order_Cleanup_Unfinished_Default instead
  */
 class Controller_Jobs_Order_Coupon_Count_Default
-	extends Controller_Jobs_Abstract
+	extends Controller_Jobs_Order_Cleanup_Unfinished_Default
 	implements Controller_Jobs_Interface
 {
 	/**
@@ -25,7 +26,7 @@ class Controller_Jobs_Order_Coupon_Count_Default
 	 */
 	public function getName()
 	{
-		return $this->_getContext()->getI18n()->dt( 'controller/jobs', 'Order coupon counts' );
+		return $this->_getContext()->getI18n()->dt( 'controller/jobs', 'Deprecated: Use "Remove unfinised orders"' );
 	}
 
 
@@ -37,81 +38,5 @@ class Controller_Jobs_Order_Coupon_Count_Default
 	public function getDescription()
 	{
 		return $this->_getContext()->getI18n()->dt( 'controller/jobs', 'Decreases the counts of successfully redeemed coupons' );
-	}
-
-
-	/**
-	 * Executes the job.
-	 *
-	 * @throws Controller_Jobs_Exception If an error occurs
-	 */
-	public function run()
-	{
-		$context = $this->_getContext();
-
-		$orderManager = MShop_Order_Manager_Factory::createManager( $context );
-		$orderStatusManager = $orderManager->getSubManager( 'status' );
-		$orderCouponManager = $orderManager->getSubManager( 'base' )->getSubManager( 'coupon' );
-		$codeManager = MShop_Coupon_Manager_Factory::createManager( $context )->getSubManager( 'code' );
-
-
-		$statusItem = $orderStatusManager->createItem();
-		$statusItem->setType( MShop_Order_Item_Status_Abstract::COUPON_UPDATE );
-		$statusItem->setValue( 1 );
-
-
-		$search = $orderCouponManager->createSearch();
-		$search->setSlice( 0, 0x7fffffff );
-
-		$criteria = $orderManager->createSearch();
-
-		$params = array( MShop_Order_Item_Status_Abstract::COUPON_UPDATE, 1 );
-		$cmpfunc = $criteria->createFunction( 'order.containsStatus', $params );
-
-		$expr = array(
-			$criteria->compare( '>=', 'order.statuspayment', MShop_Order_Item_Abstract::PAY_AUTHORIZED ),
-			$criteria->compare( '==', $cmpfunc, 0 ),
-		);
-		$criteria->setConditions( $criteria->combine( '&&', $expr ) );
-
-
-		$start = 0;
-
-		do
-		{
-			$baseIds = array();
-			$items = $orderManager->searchItems( $criteria );
-
-			foreach( $items as $id => $item ) {
-				$baseIds[ $item->getBaseId() ][] = $id;
-			}
-
-			$search->setConditions( $search->compare( '==', 'order.base.coupon.baseid', array_keys( $baseIds ) ) );
-
-			foreach( $orderCouponManager->searchItems( $search ) as $couponItem )
-			{
-				try
-				{
-					foreach( (array) $baseIds[ $couponItem->getBaseId() ] as $orderId )
-					{
-						$codeManager->decrease( $couponItem->getCode(), 1 );
-
-						$statusItem->setId( null );
-						$statusItem->setParentId( $orderId );
-						$orderStatusManager->saveItem( $statusItem );
-					}
-				}
-				catch( Exception $e )
-				{
-					$str = 'Error while updating coupons for order ID "%1$s": %2$s';
-					$context->getLogger()->log( sprintf( $str, $id, $e->getMessage() ) );
-				}
-			}
-
-			$count = count( $items );
-			$start += $count;
-			$criteria->setSlice( $start );
-		}
-		while( $count >= $criteria->getSliceSize() );
 	}
 }
---
+++ controller/jobs/tests/Controller/Jobs/Order/Cleanup/Unfinished/DefaultTest.php
@@ -0,0 +1,105 @@
+<?php
+
+/**
+ * @copyright Copyright (c) Metaways Infosystems GmbH, 2014
+ * @license LGPLv3, http://www.arcavias.com/en/license
+ */
+
+
+class Controller_Jobs_Order_Cleanup_Unfinished_DefaultTest
+	extends MW_Unittest_Testcase
+{
+	private $_object;
+
+
+	/**
+	 * Sets up the fixture, for example, opens a network connection.
+	 * This method is called before a test is executed.
+	 *
+	 * @access protected
+	 */
+	protected function setUp()
+	{
+		$context = TestHelper::getContext();
+		$arcavias = TestHelper::getArcavias();
+
+		$this->_object = new Controller_Jobs_Order_Cleanup_Unfinished_Default( $context, $arcavias );
+	}
+
+
+	/**
+	 * Tears down the fixture, for example, closes a network connection.
+	 * This method is called after a test is executed.
+	 *
+	 * @access protected
+	 */
+	protected function tearDown()
+	{
+		$this->_object = null;
+	}
+
+
+	public function testGetName()
+	{
+		$this->assertEquals( 'Removes unfinished orders', $this->_object->getName() );
+	}
+
+
+	public function testGetDescription()
+	{
+		$text = 'Deletes unfinished orders an makes their products and coupon codes available again';
+		$this->assertEquals( $text, $this->_object->getDescription() );
+	}
+
+
+	public function testRun()
+	{
+		$context = TestHelper::getContext();
+		$arcavias = TestHelper::getArcavias();
+
+
+		$name = 'ControllerJobsOrderCleanupUnfinishedDefaultRun';
+		$context->getConfig()->set( 'classes/order/manager/name', $name );
+		$context->getConfig()->set( 'classes/controller/common/order/name', $name );
+
+
+		$orderManagerStub = $this->getMockBuilder( 'MShop_Order_Manager_Default' )
+			->setMethods( array( 'searchItems', 'getSubManager' ) )
+			->setConstructorArgs( array( $context ) )
+			->getMock();
+
+		$orderBaseManagerStub = $this->getMockBuilder( 'MShop_Order_Manager_Base_Default' )
+			->setMethods( array( 'deleteItems' ) )
+			->setConstructorArgs( array( $context ) )
+			->getMock();
+
+		$orderCntlStub = $this->getMockBuilder( 'Controller_Common_Order_Default' )
+			->setMethods( array( 'unblock' ) )
+			->setConstructorArgs( array( $context ) )
+			->getMock();
+
+
+		MShop_Order_Manager_Factory::injectManager( 'MShop_Order_Manager_' . $name, $orderManagerStub );
+		Controller_Common_Order_Factory::injectController( 'Controller_Common_Order_' . $name, $orderCntlStub );
+
+
+		$orderItem = $orderManagerStub->createItem();
+		$orderItem->setBaseId( 1 );
+		$orderItem->setId( 2 );
+
+
+		$orderManagerStub->expects( $this->once() )->method( 'getSubManager' )
+			->will( $this->returnValue( $orderBaseManagerStub ) );
+
+		$orderManagerStub->expects( $this->once() )->method( 'searchItems' )
+			->will( $this->returnValue( array( $orderItem->getId() => $orderItem ) ) );
+
+		$orderBaseManagerStub->expects( $this->once() )->method( 'deleteItems' );
+
+		$orderCntlStub->expects( $this->once() )->method( 'unblock' );
+
+
+		$object = new Controller_Jobs_Order_Cleanup_Unfinished_Default( $context, $arcavias );
+		$object->run();
+	}
+}
