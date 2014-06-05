cd3bd1e3a8a2fc508b6198cdcfe9565fefd5ef64
Norbert <n.sendetzky@metaways.de>
2014-06-05 15:59:45 +0200

    Implements decorator for hiding delivery/payment options based on the country code

---
+++ lib/mshoplib/src/MShop/Service/Provider/Decorator/Country.php
@@ -0,0 +1,159 @@
+<?php
+
+/**
+ * @copyright Copyright (c) Metaways Infosystems GmbH, 2014
+ * @license LGPLv3, http://www.arcavias.com/en/license
+ * @package MShop
+ * @subpackage Service
+ */
+
+
+/**
+ * Country-limiting decorator for service providers.
+ *
+ * @package MShop
+ * @subpackage Service
+ */
+class MShop_Service_Provider_Decorator_Country
+	extends MShop_Service_Provider_Decorator_Abstract
+{
+	private $_beConfig = array(
+		'country.billing-include' => array(
+			'code' => 'country.billing-include',
+			'internalcode'=> 'country.billing-include',
+			'label'=> 'List of countries allowed for the billing address',
+			'type'=> 'string',
+			'internaltype'=> 'string',
+			'default'=> '',
+			'required'=> false,
+		),
+		'country.billing-exclude' => array(
+			'code' => 'country.billing-exclude',
+			'internalcode'=> 'country.billing-exclude',
+			'label'=> 'List of countries not allowed for the billing address',
+			'type'=> 'string',
+			'internaltype'=> 'string',
+			'default'=> '',
+			'required'=> false,
+		),
+		'country.delivery-include' => array(
+			'code' => 'country.delivery-include',
+			'internalcode'=> 'country.delivery-include',
+			'label'=> 'List of countries allowed for the delivery address',
+			'type'=> 'string',
+			'internaltype'=> 'string',
+			'default'=> '',
+			'required'=> false,
+		),
+		'country.delivery-exclude' => array(
+			'code' => 'country.delivery-exclude',
+			'internalcode'=> 'country.delivery-exclude',
+			'label'=> 'List of countries not allowed for the delivery address',
+			'type'=> 'string',
+			'internaltype'=> 'string',
+			'default'=> '',
+			'required'=> false,
+		),
+	);
+
+
+	/**
+	 * Checks the backend configuration attributes for validity.
+	 *
+	 * @param array $attributes Attributes added by the shop owner in the administraton interface
+	 * @return array An array with the attribute keys as key and an error message as values for all attributes that are
+	 * 	known by the provider but aren't valid
+	 */
+	public function checkConfigBE( array $attributes )
+	{
+		$error = $this->_getProvider()->checkConfigBE( $attributes );
+		$error += $this->_checkConfig( $this->_beConfig, $attributes );
+
+		return $error;
+	}
+
+
+	/**
+	 * Returns the configuration attribute definitions of the provider to generate a list of available fields and
+	 * rules for the value of each field in the administration interface.
+	 *
+	 * @return array List of attribute definitions implementing MW_Common_Critera_Attribute_Interface
+	 */
+	public function getConfigBE()
+	{
+		$list = $this->_getProvider()->getConfigBE();
+
+		foreach( $this->_beConfig as $key => $config ) {
+			$list[ $key ] = new MW_Common_Criteria_Attribute_Default( $config );
+		}
+
+		return $list;
+	}
+
+
+	/**
+	 * Checks if the country code is allowed for the service provider.
+	 *
+	 * @param MShop_Order_Item_Base_Interface $basket Basket object
+	 * @return boolean True if payment provider can be used, false if not
+	 */
+	public function isAvailable( MShop_Order_Item_Base_Interface $basket )
+	{
+		$addresses = $basket->getAddresses();
+		$config = $this->getServiceItem()->getConfig();
+
+		$paymentType = MShop_Order_Item_Base_Address_Abstract::TYPE_PAYMENT;
+		$deliveryType = MShop_Order_Item_Base_Address_Abstract::TYPE_DELIVERY;
+
+
+		if( isset( $addresses[$deliveryType] ) )
+		{
+			$code = strtoupper( $addresses[$deliveryType]->getCountryId() );
+
+			if( $this->_checkCountryCode( $code, 'country.delivery-include' ) === false
+				|| $this->_checkCountryCode( $code, 'country.delivery-exclude' ) === true
+			) {
+				return false;
+			}
+		}
+		else if( isset( $addresses[$paymentType] ) ) // use billing address if no delivery address is available
+		{
+			$code = strtoupper( $addresses[$paymentType]->getCountryId() );
+
+			if( $this->_checkCountryCode( $code, 'country.delivery-include' ) === false
+				|| $this->_checkCountryCode( $code, 'country.delivery-exclude' ) === true
+			) {
+				return false;
+			}
+		}
+
+		if( isset( $addresses[$paymentType] ) )
+		{
+			$code = strtoupper( $addresses[$paymentType]->getCountryId() );
+
+			if( $this->_checkCountryCode( $code, 'country.billing-include' ) === false
+				|| $this->_checkCountryCode( $code, 'country.billing-exclude' ) === true
+			) {
+				return false;
+			}
+		}
+
+		return $this->_getProvider()->isAvailable( $basket );
+	}
+
+
+	/**
+	 * Checks if the country code is in the list of codes specified by the given key
+	 *
+	 * @param string $code Two letter ISO country code in upper case
+	 * @param string $key Configuration key referring to the country code configuration
+	 */
+	protected function _checkCountryCode( $code, $key )
+	{
+		if( ( $str = $this->_getConfigValue( array( $key ) ) ) === null ) {
+			return null;
+		}
+
+		return in_array( $code, explode( ',', str_replace( ' ', '', strtoupper( $str ) ) ) );
+	}
+}
---
+++ lib/mshoplib/tests/MShop/Service/Provider/Decorator/CountryTest.php
@@ -0,0 +1,372 @@
+<?php
+
+/**
+ * @copyright Copyright (c) Metaways Infosystems GmbH, 2014
+ * @license LGPLv3, http://www.arcavias.com/en/license
+ */
+
+
+/**
+ * Test class for MShop_Service_Provider_Decorator_Country.
+ */
+class MShop_Service_Provider_Decorator_CountryTest extends MW_Unittest_Testcase
+{
+	private $_object;
+	private $_basket;
+	private $_context;
+	private $_servItem;
+	private $_mockProvider;
+
+
+	protected function setUp()
+	{
+		$this->_context = TestHelper::getContext();
+
+		$servManager = MShop_Factory::createManager( $this->_context, 'service' );
+		$this->_servItem = $servManager->createItem();
+
+		$this->_mockProvider = $this->getMockBuilder( 'MShop_Service_Provider_Decorator_Country' )
+			->disableOriginalConstructor()->getMock();
+
+		$this->_basket = MShop_Order_Manager_Factory::createManager( $this->_context )
+			->getSubManager( 'base' )->createItem();
+
+		$this->_object = new MShop_Service_Provider_Decorator_Country( $this->_context, $this->_servItem, $this->_mockProvider );
+	}
+
+
+	protected function tearDown()
+	{
+		unset( $this->_object, $this->_basket, $this->_mockProvider, $this->_servItem, $this->_context );
+	}
+
+
+	public function testGetConfigBE()
+	{
+		$this->_mockProvider->expects( $this->once() )
+			->method( 'getConfigBE' )
+			->will( $this->returnValue( array() ) );
+
+		$result = $this->_object->getConfigBE();
+
+		$this->assertArrayHasKey( 'country.billing-include', $result );
+		$this->assertArrayHasKey( 'country.billing-exclude', $result );
+		$this->assertArrayHasKey( 'country.delivery-include', $result );
+		$this->assertArrayHasKey( 'country.delivery-exclude', $result );
+	}
+
+
+	public function testCheckConfigBE()
+	{
+		$this->_mockProvider->expects( $this->once() )
+			->method( 'checkConfigBE' )
+			->will( $this->returnValue( array() ) );
+
+		$attributes = array(
+			'country.billing-include' => ' DE, AT, CH ',
+			'country.billing-exclude' => ' DE ,AT ,CH ',
+			'country.delivery-include' => 'DE, AT, CH',
+			'country.delivery-exclude' => 'DE ,AT ,CH',
+		);
+		$result = $this->_object->checkConfigBE( $attributes );
+
+		$this->assertEquals( 4, count( $result ) );
+		$this->assertInternalType( 'null', $result['country.billing-include'] );
+		$this->assertInternalType( 'null', $result['country.billing-exclude'] );
+		$this->assertInternalType( 'null', $result['country.delivery-include'] );
+		$this->assertInternalType( 'null', $result['country.delivery-exclude'] );
+	}
+
+
+	public function testCheckConfigBENoConfig()
+	{
+		$this->_mockProvider->expects( $this->once() )
+			->method( 'checkConfigBE' )
+			->will( $this->returnValue( array() ) );
+
+		$result = $this->_object->checkConfigBE( array() );
+
+		$this->assertEquals( 4, count( $result ) );
+		$this->assertInternalType( 'null', $result['country.billing-include'] );
+		$this->assertInternalType( 'null', $result['country.billing-exclude'] );
+		$this->assertInternalType( 'null', $result['country.delivery-include'] );
+		$this->assertInternalType( 'null', $result['country.delivery-exclude'] );
+	}
+
+
+	public function testCheckConfigBEFailure()
+	{
+		$this->_mockProvider->expects( $this->once() )
+			->method( 'checkConfigBE' )
+			->will( $this->returnValue( array() ) );
+
+		$attributes = array(
+			'country.billing-include' => array(),
+			'country.billing-exclude' => 1.5,
+			'country.delivery-include' => array(),
+			'country.delivery-exclude' => 1.5,
+		);
+		$result = $this->_object->checkConfigBE( $attributes );
+
+		$this->assertEquals( 4, count( $result ) );
+		$this->assertInternalType( 'string', $result['country.billing-include'] );
+		$this->assertInternalType( 'string', $result['country.billing-exclude'] );
+		$this->assertInternalType( 'string', $result['country.delivery-include'] );
+		$this->assertInternalType( 'string', $result['country.delivery-exclude'] );
+	}
+
+
+	public function testIsAvailableNoAddresses()
+	{
+		$config = array(
+			'country.billing-include' => '',
+			'country.delivery-include' => '',
+		);
+		$this->_servItem->setConfig( $config );
+
+		$this->_mockProvider->expects( $this->once() )
+			->method( 'isAvailable' )
+			->will( $this->returnValue( true ) );
+
+		$this->assertTrue( $this->_object->isAvailable( $this->_basket ) );
+	}
+
+
+	public function testIsAvailableNoConfig()
+	{
+		$address = MShop_Factory::createManager( $this->_context, 'order/base/address' )->createItem();
+		$address->setCountryId( 'DE' );
+
+		$this->_basket->setAddress( $address, MShop_Order_Item_Base_Address_Abstract::TYPE_PAYMENT );
+		$this->_basket->setAddress( $address, MShop_Order_Item_Base_Address_Abstract::TYPE_DELIVERY );
+		$this->_servItem->setConfig( array() );
+
+		$this->_mockProvider->expects( $this->once() )
+			->method( 'isAvailable' )
+			->will( $this->returnValue( true ) );
+
+		$this->assertTrue( $this->_object->isAvailable( $this->_basket ) );
+	}
+
+
+	public function testIsAvailableNoIncludeBilling()
+	{
+		$address = MShop_Factory::createManager( $this->_context, 'order/base/address' )->createItem();
+		$address->setCountryId( 'DE' );
+
+		$this->_basket->setAddress( $address, MShop_Order_Item_Base_Address_Abstract::TYPE_PAYMENT );
+		$this->_servItem->setConfig( array( 'country.billing-include' => '' ) );
+
+		$this->_mockProvider->expects( $this->never() )->method( 'isAvailable' );
+
+		$this->assertFalse( $this->_object->isAvailable( $this->_basket ) );
+	}
+
+
+	public function testIsAvailableNoIncludeDelivery()
+	{
+		$address = MShop_Factory::createManager( $this->_context, 'order/base/address' )->createItem();
+		$address->setCountryId( 'DE' );
+
+		$this->_basket->setAddress( $address, MShop_Order_Item_Base_Address_Abstract::TYPE_DELIVERY );
+		$this->_servItem->setConfig( array( 'country.delivery-include' => '' ) );
+
+		$this->_mockProvider->expects( $this->never() )->method( 'isAvailable' );
+
+		$this->assertFalse( $this->_object->isAvailable( $this->_basket ) );
+	}
+
+
+	public function testIsAvailableNoIncludeDeliveryFallback()
+	{
+		$address = MShop_Factory::createManager( $this->_context, 'order/base/address' )->createItem();
+		$address->setCountryId( 'DE' );
+
+		$this->_basket->setAddress( $address, MShop_Order_Item_Base_Address_Abstract::TYPE_PAYMENT );
+		$this->_servItem->setConfig( array( 'country.delivery-include' => '' ) );
+
+		$this->_mockProvider->expects( $this->never() )->method( 'isAvailable' );
+
+		$this->assertFalse( $this->_object->isAvailable( $this->_basket ) );
+	}
+
+
+	public function testIsAvailableNoExcludeBilling()
+	{
+		$address = MShop_Factory::createManager( $this->_context, 'order/base/address' )->createItem();
+		$address->setCountryId( 'DE' );
+
+		$this->_basket->setAddress( $address, MShop_Order_Item_Base_Address_Abstract::TYPE_PAYMENT );
+		$this->_servItem->setConfig( array( 'country.billing-exclude' => '' ) );
+
+		$this->_mockProvider->expects( $this->once() )
+			->method( 'isAvailable' )
+			->will( $this->returnValue( true ) );
+
+		$this->assertTrue( $this->_object->isAvailable( $this->_basket ) );
+	}
+
+
+	public function testIsAvailableNoExcludeDelivery()
+	{
+		$address = MShop_Factory::createManager( $this->_context, 'order/base/address' )->createItem();
+		$address->setCountryId( 'DE' );
+
+		$this->_basket->setAddress( $address, MShop_Order_Item_Base_Address_Abstract::TYPE_DELIVERY );
+		$this->_servItem->setConfig( array( 'country.delivery-exclude' => '' ) );
+
+		$this->_mockProvider->expects( $this->once() )
+			->method( 'isAvailable' )
+			->will( $this->returnValue( true ) );
+
+		$this->assertTrue( $this->_object->isAvailable( $this->_basket ) );
+	}
+
+
+	public function testIsAvailableNoExcludeDeliveryFallback()
+	{
+		$address = MShop_Factory::createManager( $this->_context, 'order/base/address' )->createItem();
+		$address->setCountryId( 'DE' );
+
+		$this->_basket->setAddress( $address, MShop_Order_Item_Base_Address_Abstract::TYPE_PAYMENT );
+		$this->_servItem->setConfig( array( 'country.delivery-exclude' => '' ) );
+
+		$this->_mockProvider->expects( $this->once() )
+			->method( 'isAvailable' )
+			->will( $this->returnValue( true ) );
+
+		$this->assertTrue( $this->_object->isAvailable( $this->_basket ) );
+	}
+
+
+	public function testIsAvailableExcludeBilling()
+	{
+		$address = MShop_Factory::createManager( $this->_context, 'order/base/address' )->createItem();
+		$address->setCountryId( 'DE' );
+
+		$this->_basket->setAddress( $address, MShop_Order_Item_Base_Address_Abstract::TYPE_PAYMENT );
+		$this->_servItem->setConfig( array( 'country.billing-exclude' => 'de' ) );
+
+		$this->_mockProvider->expects( $this->never() )->method( 'isAvailable' );
+
+		$this->assertFalse( $this->_object->isAvailable( $this->_basket ) );
+	}
+
+
+	public function testIsAvailableExcludeDelivery()
+	{
+		$address = MShop_Factory::createManager( $this->_context, 'order/base/address' )->createItem();
+		$address->setCountryId( 'DE' );
+
+		$this->_basket->setAddress( $address, MShop_Order_Item_Base_Address_Abstract::TYPE_DELIVERY );
+		$this->_servItem->setConfig( array( 'country.delivery-exclude' => 'de' ) );
+
+		$this->_mockProvider->expects( $this->never() )->method( 'isAvailable' );
+
+		$this->assertFalse( $this->_object->isAvailable( $this->_basket ) );
+	}
+
+
+	public function testIsAvailableExcludeDeliveryFallback()
+	{
+		$address = MShop_Factory::createManager( $this->_context, 'order/base/address' )->createItem();
+		$address->setCountryId( 'DE' );
+
+		$this->_basket->setAddress( $address, MShop_Order_Item_Base_Address_Abstract::TYPE_PAYMENT );
+		$this->_servItem->setConfig( array( 'country.delivery-exclude' => 'de' ) );
+
+		$this->_mockProvider->expects( $this->never() )->method( 'isAvailable' );
+
+		$this->assertFalse( $this->_object->isAvailable( $this->_basket ) );
+	}
+
+
+	public function testIsAvailableIncludeBilling()
+	{
+		$address = MShop_Factory::createManager( $this->_context, 'order/base/address' )->createItem();
+		$address->setCountryId( 'DE' );
+
+		$this->_basket->setAddress( $address, MShop_Order_Item_Base_Address_Abstract::TYPE_PAYMENT );
+		$this->_servItem->setConfig( array( 'country.billing-include' => 'de' ) );
+
+		$this->_mockProvider->expects( $this->once() )
+			->method( 'isAvailable' )
+			->will( $this->returnValue( true ) );
+
+		$this->assertTrue( $this->_object->isAvailable( $this->_basket ) );
+	}
+
+
+	public function testIsAvailableIncludeDelivery()
+	{
+		$address = MShop_Factory::createManager( $this->_context, 'order/base/address' )->createItem();
+		$address->setCountryId( 'DE' );
+
+		$this->_basket->setAddress( $address, MShop_Order_Item_Base_Address_Abstract::TYPE_DELIVERY );
+		$this->_servItem->setConfig( array( 'country.delivery-include' => 'de' ) );
+
+		$this->_mockProvider->expects( $this->once() )
+			->method( 'isAvailable' )
+			->will( $this->returnValue( true ) );
+
+		$this->assertTrue( $this->_object->isAvailable( $this->_basket ) );
+	}
+
+
+	public function testIsAvailableIncludeDeliveryFallback()
+	{
+		$address = MShop_Factory::createManager( $this->_context, 'order/base/address' )->createItem();
+		$address->setCountryId( 'DE' );
+
+		$this->_basket->setAddress( $address, MShop_Order_Item_Base_Address_Abstract::TYPE_PAYMENT );
+		$this->_servItem->setConfig( array( 'country.delivery-include' => 'de' ) );
+
+		$this->_mockProvider->expects( $this->once() )
+			->method( 'isAvailable' )
+			->will( $this->returnValue( true ) );
+
+		$this->assertTrue( $this->_object->isAvailable( $this->_basket ) );
+	}
+
+
+	public function testIsAvailableIncludeBillingFailure()
+	{
+		$address = MShop_Factory::createManager( $this->_context, 'order/base/address' )->createItem();
+		$address->setCountryId( 'DE' );
+
+		$this->_basket->setAddress( $address, MShop_Order_Item_Base_Address_Abstract::TYPE_PAYMENT );
+		$this->_servItem->setConfig( array( 'country.billing-include' => 'ch' ) );
+
+		$this->_mockProvider->expects( $this->never() )->method( 'isAvailable' );
+
+		$this->assertFalse( $this->_object->isAvailable( $this->_basket ) );
+	}
+
+
+	public function testIsAvailableIncludeDeliveryFailure()
+	{
+		$address = MShop_Factory::createManager( $this->_context, 'order/base/address' )->createItem();
+		$address->setCountryId( 'DE' );
+
+		$this->_basket->setAddress( $address, MShop_Order_Item_Base_Address_Abstract::TYPE_DELIVERY );
+		$this->_servItem->setConfig( array( 'country.delivery-include' => 'ch' ) );
+
+		$this->_mockProvider->expects( $this->never() )->method( 'isAvailable' );
+
+		$this->assertFalse( $this->_object->isAvailable( $this->_basket ) );
+	}
+
+
+	public function testIsAvailableIncludeDeliveryFailureFallback()
+	{
+		$address = MShop_Factory::createManager( $this->_context, 'order/base/address' )->createItem();
+		$address->setCountryId( 'DE' );
+
+		$this->_basket->setAddress( $address, MShop_Order_Item_Base_Address_Abstract::TYPE_PAYMENT );
+		$this->_servItem->setConfig( array( 'country.delivery-include' => 'ch' ) );
+
+		$this->_mockProvider->expects( $this->never() )->method( 'isAvailable' );
+
+		$this->assertFalse( $this->_object->isAvailable( $this->_basket ) );
+	}
+}
