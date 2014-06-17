61a74bcaa03e487abc444f0a1d59373ddc029e0e
Norbert <n.sendetzky@metaways.de>
2014-06-17 15:58:18 +0200

    Implements job controller for removing expired cache entries

---
+++ controller/jobs/src/Controller/Jobs/Admin/Cache/Default.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * @copyright Copyright (c) Metaways Infosystems GmbH, 2014
+ * @license LGPLv3, http://www.arcavias.com/en/license
+ * @package Controller
+ * @subpackage Jobs
+ */
+
+
+/**
+ * Jobs admin job controller for admin interfaces.
+ *
+ * @package Controller
+ * @subpackage Jobs
+ */
+class Controller_Jobs_Admin_Cache_Default
+	extends Controller_Jobs_Abstract
+	implements Controller_Jobs_Interface
+{
+	/**
+	 * Returns the localized name of the job.
+	 *
+	 * @return string Name of the job
+	 */
+	public function getName()
+	{
+		return $this->_getContext()->getI18n()->dt( 'controller/jobs', 'Cache cleanup' );
+	}
+
+
+	/**
+	 * Returns the localized description of the job.
+	 *
+	 * @return string Description of the job
+	 */
+	public function getDescription()
+	{
+		return $this->_getContext()->getI18n()->dt( 'controller/jobs', 'Removes the expired entries from the cache' );
+	}
+
+
+	/**
+	 * Executes the job.
+	 *
+	 * @throws Controller_Jobs_Exception If an error occurs
+	 */
+	public function run()
+	{
+		MAdmin_Cache_Manager_Factory::createManager( $this->_getContext() )->getCache()->cleanup();
+	}
+}
