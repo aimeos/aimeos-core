b51b6473b37fb4b25db2d736148f527639c7b2af
Norbert <n.sendetzky@metaways.de>
2014-07-14 10:24:04 +0200

    Adds watched product to unit test data

--- lib/mshoplib/setup/unittest/data/customer-list.php
+++ lib/mshoplib/setup/unittest/data/customer-list.php
@@ -17,5 +17,6 @@ return array (
 		array( 'parentid' => 'customer/UTC003', 'typeid' => 'text/default', 'domain' => 'text', 'refid' => 'text/customer/notify', 'start' => '2010-01-01 00:00:00', 'end' => '2022-01-01 00:00:00', 'config' => array(), 'pos' => 1, 'status' => 1 ),
 		array( 'parentid' => 'customer/UTC003', 'typeid' => 'text/default', 'domain' => 'text', 'refid' => 'text/customer/newsletter', 'start' => '2010-01-01 00:00:00', 'end' => '2022-01-01 00:00:00', 'config' => array(), 'pos' => 2, 'status' => 1 ),
 		array( 'parentid' => 'customer/UTC001', 'typeid' => 'text/default', 'domain' => 'text', 'refid' => 'text/customer/information', 'start' => '2010-01-01 00:00:00', 'end' => '2022-01-01 00:00:00', 'config' => array(), 'pos' => 2, 'status' => 1 ),
+		array( 'parentid' => 'customer/UTC001', 'typeid' => 'product/watch', 'domain' => 'product', 'refid' => 'product/CNE', 'start' => null, 'end' => '2100-01-01 00:00:00', 'config' => array( 'stock' => 1 ), 'pos' => 1, 'status' => 1 ),
 	),
 );
