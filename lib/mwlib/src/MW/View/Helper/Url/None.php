1827206c8b0769c0da36abdee9fd97859c6f4373
Norbert <n.sendetzky@metaways.de>
2014-07-21 19:20:46 +0200

    Adds URL config array as last parameter as used by the other URL view helpers

--- lib/mwlib/src/MW/View/Helper/Url/None.php
+++ lib/mwlib/src/MW/View/Helper/Url/None.php
@@ -26,9 +26,10 @@ class MW_View_Helper_Url_None
 	 * @param string|null $action Name of the action which should be part of the link (if any)
 	 * @param array $params Associative list of parameters that should be part of the URL
 	 * @param array $trailing Trailing URL parts that are not relevant to identify the resource (for pretty URLs)
+	 * @param array $config Additional configuration parameter per URL
 	 * @return string Complete URL that can be used in the template
 	 */
-	public function transform( $target = null, $controller = null, $action = null, array $params = array(), array $trailing = array() )
+	public function transform( $target = null, $controller = null, $action = null, array $params = array(), array $trailing = array(), array $trailing = array() )
 	{
 		return '';
 	}
