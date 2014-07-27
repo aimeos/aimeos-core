b642462baaaee2751cfed3ff8d1310ea38f1b959
Norbert <n.sendetzky@metaways.de>
2014-07-27 12:31:21 +0200

    Updates compressed JS file for admin interface

--- client/extjs/deploy/js/arcavias-core.js
+++ client/extjs/deploy/js/arcavias-core.js
@@ -3276,8 +3276,10 @@ MShop.panel.AbstractListUi = Ext.extend(Ext.Panel, {
                 emptyText: MShop.I18n.dt( 'client/extjs', 'No items' ),
                 getRowClass: function( record, index ) {
 
-                    siteid = MShop.config.site['locale.site.id'];
-                    if( record.phantom === false && record.get( this.siteidProperty ) != siteid ) {
+                    var siteid = MShop.config.site['locale.site.id'];
+                    var recSiteid = record.get( this.siteidProperty );
+                    
+                    if( record.phantom === false && recSiteid != null && recSiteid != siteid ) {
                         return this.rowCssClass;
                     }
                     return '';
@@ -11668,6 +11670,8 @@ MShop.panel.coupon.code.ListUi = Ext.extend(MShop.panel.AbstractListUi, {
 				}
 			} ]
 		};
+		
+		return true;
 	},
 
 	
@@ -12448,6 +12452,10 @@ MShop.panel.order.base.address.DeliveryItemUi = Ext.extend(Ext.FormPanel, {
 				xtype: 'displayfield',
 				fieldLabel: MShop.I18n.dt( 'client/extjs', 'Company' ),
 				name: 'order.base.address.company'
+			}, {
+				xtype: 'displayfield',
+				fieldLabel: MShop.I18n.dt( 'client/extjs', 'Vat ID' ),
+				name: 'order.base.address.vatid'
 			}, {
 				xtype: 'displayfield',
 				fieldLabel: MShop.I18n.dt( 'client/extjs', 'Salutation' ),
@@ -14444,6 +14452,44 @@ MShop.panel.stock.warehouse.ItemUi = Ext.extend( MShop.panel.AbstractItemUi, {
 });
 
 Ext.reg( 'MShop.panel.stock.warehouse.itemui', MShop.panel.stock.warehouse.ItemUi );/*!
+ * Copyright (c) Metaways Infosystems GmbH, 2014
+ * LGPLv3, http://www.arcavias.com/en/license
+ */
+
+
+Ext.ns('MShop.panel.locale');
+
+/**
+ * Nest all locale related panels in the locale tab
+ * 
+ * @class MShop.panel.locale.TabUi
+ * @extends Ext.Panel
+ */
+MShop.panel.locale.TabUi = Ext.extend(Ext.Panel, {
+
+	maximized : true,
+	layout : 'fit',
+	modal : true,
+
+	initComponent : function() {
+
+		this.title = MShop.I18n.dt( 'client/extjs', 'Locale' );
+
+		this.items = [{
+			xtype : 'tabpanel',
+			activeTab : 0,
+			border : false,
+			itemId : 'MShop.panel.locale.tabui',
+			plugins : [ 'ux.itemregistry' ]
+		}];
+
+		MShop.panel.locale.TabUi.superclass.initComponent.call(this);
+	}
+});
+
+Ext.reg('MShop.panel.locale.tabui', MShop.panel.locale.TabUi);
+
+Ext.ux.ItemRegistry.registerItem('MShop.MainTabPanel', 'MShop.panel.locale.tabui', MShop.panel.locale.TabUi, 80);/*!
  * Copyright (c) Metaways Infosystems GmbH, 2011
  * LGPLv3, http://www.arcavias.com/en/license
  */
@@ -14475,7 +14521,7 @@ MShop.panel.locale.ListUi = Ext.extend( MShop.panel.AbstractListUi, {
 
 	initComponent : function()
 	{
-		this.title = MShop.I18n.dt( 'client/extjs', 'Locale' );
+		this.title = MShop.I18n.dt( 'client/extjs', 'Available' );
 
 		MShop.panel.AbstractListUi.prototype.initActions.call( this );
 		MShop.panel.AbstractListUi.prototype.initToolbar.call( this );
@@ -14556,7 +14602,7 @@ MShop.panel.locale.ListUi = Ext.extend( MShop.panel.AbstractListUi, {
 Ext.reg('MShop.panel.locale.listui', MShop.panel.locale.ListUi);
 
 // hook this into the main tab panel
-Ext.ux.ItemRegistry.registerItem('MShop.MainTabPanel', 'MShop.panel.locale.listui', MShop.panel.locale.ListUi, 80);
+Ext.ux.ItemRegistry.registerItem('MShop.panel.locale.tabui', 'MShop.panel.locale.listui', MShop.panel.locale.ListUi, 10);
 /*!
  * Copyright (c) Metaways Infosystems GmbH, 2011
  * LGPLv3, http://www.arcavias.com/en/license
@@ -14694,7 +14740,7 @@ MShop.panel.locale.site.ListUi = Ext.extend( MShop.panel.AbstractListUi, {
 
 	initComponent : function()
 	{
-		this.title = MShop.I18n.dt( 'client/extjs', 'Locale site' );
+		this.title = MShop.I18n.dt( 'client/extjs', 'Site' );
 		
 		MShop.panel.AbstractListUi.prototype.initActions.call( this );
 		MShop.panel.AbstractListUi.prototype.initToolbar.call( this );
@@ -14826,7 +14872,7 @@ MShop.panel.locale.site.ListUi = Ext.extend( MShop.panel.AbstractListUi, {
 Ext.reg('MShop.panel.locale.site.listui', MShop.panel.locale.site.ListUi);
 
 // hook this into the main tab panel
-Ext.ux.ItemRegistry.registerItem('MShop.MainTabPanel', 'MShop.panel.locale.site.listui', MShop.panel.locale.site.ListUi, 80);
+Ext.ux.ItemRegistry.registerItem('MShop.panel.locale.tabui', 'MShop.panel.locale.site.listui', MShop.panel.locale.site.ListUi, 20);
 /*!
  * Copyright (c) Metaways Infosystems GmbH, 2014
  * LGPLv3, http://www.arcavias.com/en/license
@@ -14984,6 +15030,436 @@ MShop.panel.locale.site.ItemUi = Ext.extend( MShop.panel.AbstractListItemUi, {
 
 
 Ext.reg( 'MShop.panel.locale.site.itemui', MShop.panel.locale.site.ItemUi );/*!
+ * Copyright (c) Metaways Infosystems GmbH, 2014
+ * LGPLv3, http://www.arcavias.com/en/license
+ */
+
+
+Ext.ns( 'MShop.panel.locale.language' );
+
+MShop.panel.locale.language.ListUi = Ext.extend( MShop.panel.AbstractListUi, {
+
+	recordName : 'Locale_Language',
+	idProperty : 'locale.language.id',
+	siteidProperty : 'locale.language.siteid',
+	itemUiXType : 'MShop.panel.locale.language.itemui',
+
+	autoExpandColumn : 'locale-language-label',
+
+	filterConfig : {
+		filters : [ {
+			dataIndex : 'locale.language.label',
+			operator : '=~',
+			value : ''
+		} ]
+	},
+
+	sortInfo : {
+		field : 'locale.language.status',
+		direction : 'DESC'
+	},
+
+	initComponent : function()
+	{
+		this.title = MShop.I18n.dt( 'client/extjs', 'Language' );
+		
+		MShop.panel.AbstractListUi.prototype.initActions.call( this );
+		MShop.panel.AbstractListUi.prototype.initToolbar.call( this );
+
+		MShop.panel.locale.language.ListUi.superclass.initComponent.call( this );
+	},
+
+	getColumns : function()
+	{
+		return [
+			{
+				xtype : 'gridcolumn',
+				dataIndex : 'locale.language.id',
+				header : MShop.I18n.dt( 'client/extjs', 'ID' ),
+				sortable : true,
+				width : 50,
+				hidden : true
+			}, {
+				xtype : 'gridcolumn',
+				dataIndex : 'locale.language.status',
+				header : MShop.I18n.dt( 'client/extjs', 'Status' ),
+				sortable : true,
+				width : 50,
+				align: 'center',
+				renderer : this.statusColumnRenderer.createDelegate(this)
+			}, {
+				xtype : 'gridcolumn',
+				dataIndex : 'locale.language.code',
+				header : MShop.I18n.dt( 'client/extjs', 'Code' ),
+				sortable : true,
+				width : 100,
+				editable : false
+			}, {
+				xtype : 'gridcolumn',
+				dataIndex : 'locale.language.label',
+				header : MShop.I18n.dt( 'client/extjs', 'Label' ),
+				sortable : true,
+				width : 100,
+				editable : false,
+				id : 'locale-language-label'
+			}, {
+				xtype : 'datecolumn',
+				dataIndex : 'locale.language.ctime',
+				header : MShop.I18n.dt( 'client/extjs', 'Created' ),
+				sortable : true,
+				width : 130,
+				format : 'Y-m-d H:i:s',
+				editable : false,
+				hidden : true
+			}, {
+				xtype : 'datecolumn',
+				dataIndex : 'locale.language.mtime',
+				header : MShop.I18n.dt( 'client/extjs', 'Last modified' ),
+				sortable : true,
+				width : 130,
+				format : 'Y-m-d H:i:s',
+				editable : false,
+				hidden : true
+			}, {
+				xtype : 'gridcolumn',
+				dataIndex : 'locale.language.editor',
+				header : MShop.I18n.dt( 'client/extjs', 'Editor' ),
+				sortable : true,
+				width : 130,
+				editable : false,
+				hidden : true
+			}
+		];
+	}
+} );
+
+Ext.reg('MShop.panel.locale.language.listui', MShop.panel.locale.language.ListUi);
+
+// hook this into the main tab panel
+Ext.ux.ItemRegistry.registerItem('MShop.panel.locale.tabui', 'MShop.panel.locale.language.listui', MShop.panel.locale.language.ListUi, 30);
+/*!
+ * Copyright (c) Metaways Infosystems GmbH, 2014
+ * LGPLv3, http://www.arcavias.com/en/license
+ */
+
+
+Ext.ns( 'MShop.panel.locale.language' );
+
+
+/**
+ * @todo refactor some of these overloads to his abstracts
+ */
+MShop.panel.locale.language.ItemUi = Ext.extend( MShop.panel.AbstractListItemUi, {
+
+	recordName : 'Locale_Language',
+	idProperty : 'locale.language.id',
+	siteidProperty : 'locale.language.siteid',
+
+	initComponent : function() {
+
+		this.items = [ {
+			xtype : 'tabpanel',
+			activeTab : 0,
+			border : false,
+			itemId : 'MShop.panel.locale.language.ItemUi',
+			plugins : [ 'ux.itemregistry' ],
+			items : [ {
+				xtype : 'panel',
+				title : MShop.I18n.dt( 'client/extjs', 'Basic' ),
+				border : false,
+				layout : 'hbox',
+				layoutConfig : {
+					align : 'stretch'
+				},
+				itemId : 'MShop.panel.locale.language.ItemUi.BasicPanel',
+				plugins : [ 'ux.itemregistry' ],
+				defaults : {
+					bodyCssClass : this.readOnlyClass
+				},
+				items : [ {
+					xtype : 'form',
+					flex : 1,
+					ref : '../../mainForm',
+					autoScroll : true,
+					items : [ {
+						xtype : 'fieldset',
+						style: 'padding-right: 25px;',
+						border : false,
+						labelAlign : 'top',
+						defaults: {
+							readOnly : this.fieldsReadOnly,
+							anchor : '100%'
+						},
+						items : [ {
+							xtype : 'displayfield',
+							fieldLabel : MShop.I18n.dt( 'client/extjs', 'ID' ),
+							name : 'locale.language.id'
+						}, {
+							xtype : 'MShop.elements.status.combo',
+							name : 'locale.language.status'
+						}, {
+							xtype : 'textfield',
+							fieldLabel : MShop.I18n.dt( 'client/extjs', 'Code' ),
+							name : 'locale.language.code',
+							allowBlank : false,
+							maxLength : 32,
+							emptyText : MShop.I18n.dt( 'client/extjs', 'Unique code (required)' )
+						}, {
+							xtype : 'textfield',
+							fieldLabel : MShop.I18n.dt( 'client/extjs', 'Label' ),
+							name : 'locale.language.label',
+							allowBlank : false,
+							emptyText : MShop.I18n.dt( 'client/extjs', 'Internal name (required)' )
+						}, {
+							xtype : 'displayfield',
+							fieldLabel : MShop.I18n.dt( 'client/extjs', 'Created' ),
+							name : 'locale.language.ctime'
+						}, {
+							xtype : 'displayfield',
+							fieldLabel : MShop.I18n.dt( 'client/extjs', 'Last modified' ),
+							name : 'locale.language.mtime'
+						}, {
+							xtype : 'displayfield',
+							fieldLabel : MShop.I18n.dt( 'client/extjs', 'Editor' ),
+							name : 'locale.language.editor'
+						} ]
+					} ]
+				} ]
+			} ]
+		} ];
+		
+		MShop.panel.locale.language.ItemUi.superclass.initComponent.call( this );
+	},
+
+	afterRender : function()
+	{
+		var label = this.record ? this.record.data['locale.language.label'] : MShop.I18n.dt( 'client/extjs', 'new' );
+		//#: Locale language item panel title with language label ({0}) and language code ({1)}
+		var string = MShop.I18n.dt( 'client/extjs', 'Locale language: {0} ({1})' );
+		this.setTitle( String.format( string, label, MShop.config.site["locale.site.label"] ) );
+
+		MShop.panel.locale.language.ItemUi.superclass.afterRender.apply( this, arguments );
+	}
+
+} );
+
+Ext.reg( 'MShop.panel.locale.language.itemui', MShop.panel.locale.language.ItemUi );
+/*!
+ * Copyright (c) Metaways Infosystems GmbH, 2014
+ * LGPLv3, http://www.arcavias.com/en/license
+ */
+
+
+Ext.ns( 'MShop.panel.locale.currency' );
+
+MShop.panel.locale.currency.ListUi = Ext.extend( MShop.panel.AbstractListUi, {
+
+	recordName : 'Locale_Currency',
+	idProperty : 'locale.currency.id',
+	siteidProperty : 'locale.currency.siteid',
+	itemUiXType : 'MShop.panel.locale.currency.itemui',
+
+	autoExpandColumn : 'locale-currency-label',
+
+	filterConfig : {
+		filters : [ {
+			dataIndex : 'locale.currency.label',
+			operator : '=~',
+			value : ''
+		} ]
+	},
+
+	sortInfo : {
+		field : 'locale.currency.status',
+		direction : 'DESC'
+	},
+
+	initComponent : function()
+	{
+		this.title = MShop.I18n.dt( 'client/extjs', 'Currency' );
+		
+		MShop.panel.AbstractListUi.prototype.initActions.call( this );
+		MShop.panel.AbstractListUi.prototype.initToolbar.call( this );
+
+		MShop.panel.locale.currency.ListUi.superclass.initComponent.call( this );
+	},
+
+	getColumns : function()
+	{
+		return [
+			{
+				xtype : 'gridcolumn',
+				dataIndex : 'locale.currency.id',
+				header : MShop.I18n.dt( 'client/extjs', 'ID' ),
+				sortable : true,
+				width : 50,
+				hidden : true
+			}, {
+				xtype : 'gridcolumn',
+				dataIndex : 'locale.currency.status',
+				header : MShop.I18n.dt( 'client/extjs', 'Status' ),
+				sortable : true,
+				width : 50,
+				align: 'center',
+				renderer : this.statusColumnRenderer.createDelegate(this)
+			}, {
+				xtype : 'gridcolumn',
+				dataIndex : 'locale.currency.code',
+				header : MShop.I18n.dt( 'client/extjs', 'Code' ),
+				sortable : true,
+				width : 100,
+				editable : false
+			}, {
+				xtype : 'gridcolumn',
+				dataIndex : 'locale.currency.label',
+				header : MShop.I18n.dt( 'client/extjs', 'Label' ),
+				sortable : true,
+				width : 100,
+				editable : false,
+				id : 'locale-currency-label'
+			}, {
+				xtype : 'datecolumn',
+				dataIndex : 'locale.currency.ctime',
+				header : MShop.I18n.dt( 'client/extjs', 'Created' ),
+				sortable : true,
+				width : 130,
+				format : 'Y-m-d H:i:s',
+				editable : false,
+				hidden : true
+			}, {
+				xtype : 'datecolumn',
+				dataIndex : 'locale.currency.mtime',
+				header : MShop.I18n.dt( 'client/extjs', 'Last modified' ),
+				sortable : true,
+				width : 130,
+				format : 'Y-m-d H:i:s',
+				editable : false,
+				hidden : true
+			}, {
+				xtype : 'gridcolumn',
+				dataIndex : 'locale.currency.editor',
+				header : MShop.I18n.dt( 'client/extjs', 'Editor' ),
+				sortable : true,
+				width : 130,
+				editable : false,
+				hidden : true
+			}
+		];
+	}
+} );
+
+Ext.reg('MShop.panel.locale.currency.listui', MShop.panel.locale.currency.ListUi);
+
+// hook this into the main tab panel
+Ext.ux.ItemRegistry.registerItem('MShop.panel.locale.tabui', 'MShop.panel.locale.currency.listui', MShop.panel.locale.currency.ListUi, 30);
+/*!
+ * Copyright (c) Metaways Infosystems GmbH, 2014
+ * LGPLv3, http://www.arcavias.com/en/license
+ */
+
+
+Ext.ns( 'MShop.panel.locale.currency' );
+
+
+/**
+ * @todo refactor some of these overloads to his abstracts
+ */
+MShop.panel.locale.currency.ItemUi = Ext.extend( MShop.panel.AbstractListItemUi, {
+
+	recordName : 'Locale_Currency',
+	idProperty : 'locale.currency.id',
+	siteidProperty : 'locale.currency.siteid',
+
+	initComponent : function() {
+
+		this.items = [ {
+			xtype : 'tabpanel',
+			activeTab : 0,
+			border : false,
+			itemId : 'MShop.panel.locale.currency.ItemUi',
+			plugins : [ 'ux.itemregistry' ],
+			items : [ {
+				xtype : 'panel',
+				title : MShop.I18n.dt( 'client/extjs', 'Basic' ),
+				border : false,
+				layout : 'hbox',
+				layoutConfig : {
+					align : 'stretch'
+				},
+				itemId : 'MShop.panel.locale.currency.ItemUi.BasicPanel',
+				plugins : [ 'ux.itemregistry' ],
+				defaults : {
+					bodyCssClass : this.readOnlyClass
+				},
+				items : [ {
+					xtype : 'form',
+					flex : 1,
+					ref : '../../mainForm',
+					autoScroll : true,
+					items : [ {
+						xtype : 'fieldset',
+						style: 'padding-right: 25px;',
+						border : false,
+						labelAlign : 'top',
+						defaults: {
+							readOnly : this.fieldsReadOnly,
+							anchor : '100%'
+						},
+						items : [ {
+							xtype : 'displayfield',
+							fieldLabel : MShop.I18n.dt( 'client/extjs', 'ID' ),
+							name : 'locale.currency.id'
+						}, {
+							xtype : 'MShop.elements.status.combo',
+							name : 'locale.currency.status'
+						}, {
+							xtype : 'textfield',
+							fieldLabel : MShop.I18n.dt( 'client/extjs', 'Code' ),
+							name : 'locale.currency.code',
+							allowBlank : false,
+							maxLength : 32,
+							emptyText : MShop.I18n.dt( 'client/extjs', 'Unique code (required)' )
+						}, {
+							xtype : 'textfield',
+							fieldLabel : MShop.I18n.dt( 'client/extjs', 'Label' ),
+							name : 'locale.currency.label',
+							allowBlank : false,
+							emptyText : MShop.I18n.dt( 'client/extjs', 'Internal name (required)' )
+						}, {
+							xtype : 'displayfield',
+							fieldLabel : MShop.I18n.dt( 'client/extjs', 'Created' ),
+							name : 'locale.currency.ctime'
+						}, {
+							xtype : 'displayfield',
+							fieldLabel : MShop.I18n.dt( 'client/extjs', 'Last modified' ),
+							name : 'locale.currency.mtime'
+						}, {
+							xtype : 'displayfield',
+							fieldLabel : MShop.I18n.dt( 'client/extjs', 'Editor' ),
+							name : 'locale.currency.editor'
+						} ]
+					} ]
+				} ]
+			} ]
+		} ];
+		
+		MShop.panel.locale.currency.ItemUi.superclass.initComponent.call( this );
+	},
+
+	afterRender : function()
+	{
+		var label = this.record ? this.record.data['locale.currency.label'] : MShop.I18n.dt( 'client/extjs', 'new' );
+		//#: Locale currency item panel title with currency label ({0}) and currency code ({1)}
+		var string = MShop.I18n.dt( 'client/extjs', 'Locale currency: {0} ({1})' );
+		this.setTitle( String.format( string, label, MShop.config.site["locale.site.label"] ) );
+
+		MShop.panel.locale.currency.ItemUi.superclass.afterRender.apply( this, arguments );
+	}
+
+} );
+
+Ext.reg( 'MShop.panel.locale.currency.itemui', MShop.panel.locale.currency.ItemUi );
+/*!
  * Copyright (c) Metaways Infosystems GmbH, 2014
  * LGPLv3, http://www.arcavias.com/license
  */
