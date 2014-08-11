5f780e1cc4f566bcde8f1e93c376ddde9fb50bec
Norbert <n.sendetzky@metaways.de>
2014-08-11 18:09:21 +0200

    Updates documentation of session methods

--- lib/mwlib/src/MW/Session/Interface.php
+++ lib/mwlib/src/MW/Session/Interface.php
@@ -19,17 +19,23 @@ interface MW_Session_Interface
 	/**
 	 * Returns the value of the requested session key.
 	 *
+	 * If the returned value wasn't a string, it's decoded from its serialized
+	 * representation.
+	 *
 	 * @param string $name Key of the requested value in the session
-	 * @param string|null $default Value returned if requested key isn't found
-	 * @return string Value associated to the requested key
+	 * @param mixed $default Value returned if requested key isn't found
+	 * @return mixed Value associated to the requested key
 	 */
 	public function get( $name, $default = null );
 
 	/**
 	 * Sets the value for the specified key.
 	 *
+	 * If the value isn't a string, it's encoded into a serialized representation
+	 * and decoded again when using the get() method.
+	 *
 	 * @param string $name Key to the value which should be stored in the session
-	 * @param string $value Value that should be associated with the given key
+	 * @param mixed $value Value that should be associated with the given key
 	 * @return void
 	 */
 	public function set( $name, $value );
