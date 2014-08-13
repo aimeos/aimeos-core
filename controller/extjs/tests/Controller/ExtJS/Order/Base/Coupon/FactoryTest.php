059fd78b86ca9e12fdd147c38116970c44b929d9
Norbert <n.sendetzky@metaways.de>
2014-08-13 13:13:47 +0200

    Implements missing factory tests for order base coupon ExtJS controller

---
+++ controller/extjs/tests/Controller/ExtJS/Order/Base/Coupon/FactoryTest.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * @copyright Copyright (c) Metaways Infosystems GmbH, 2014
+ * @license LGPLv3, http://www.arcavias.com/en/license
+ */
+
+
+class Controller_ExtJS_Order_Base_Coupon_FactoryTest extends MW_Unittest_Testcase
+{
+	private $_object;
+
+
+	/**
+	 * Sets up the fixture, for example, opens a network connection.
+	 * This method is called before a test is executed.
+	 *
+	 * @access protected
+	 */
+	protected function setUp()
+	{
+	}
+
+
+	/**
+	 * Tears down the fixture, for example, closes a network connection.
+	 * This method is called after a test is executed.
+	 *
+	 * @access protected
+	 */
+	protected function tearDown()
+	{
+	}
+
+
+	public function testCreateController()
+	{
+		$obj = Controller_ExtJS_Order_Base_Coupon_Factory::createController( TestHelper::getContext() );
+		$this->assertInstanceOf( 'Controller_ExtJS_Interface', $obj);
+	}
+
+
+	public function testFactoryExceptionWrongName()
+	{
+		$this->setExpectedException( 'Controller_ExtJS_Exception' );
+		Controller_ExtJS_Order_Base_Coupon_Factory::createController(TestHelper::getContext(), 'Wrong$$$Name' );
+	}
+
+
+	public function testFactoryExceptionWrongClass()
+	{
+		$this->setExpectedException( 'Controller_ExtJS_Exception' );
+		Controller_ExtJS_Order_Base_Coupon_Factory::createController(TestHelper::getContext(), 'WrongClass' );
+	}
+
+
+	public function testFactoryExceptionWrongInterface()
+	{
+		$this->setExpectedException( 'Controller_ExtJS_Exception' );
+		Controller_ExtJS_Order_Base_Coupon_Factory::createController(TestHelper::getContext(), 'Factory' );
+	}
+
+}
