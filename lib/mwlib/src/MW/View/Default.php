265db526b67a5f3154015c65bafb1bc059d49d26
Norbert <n.sendetzky@metaways.de>
2014-08-13 09:29:12 +0200

    Improves @method annotations for __call() method in view

--- lib/mwlib/src/MW/View/Default.php
+++ lib/mwlib/src/MW/View/Default.php
@@ -11,15 +11,15 @@
 /**
  * Default view implementation.
  *
- * @method string|array config( string $name = null, mixed $default = null ) Returns the config value for the given key
- * @method string date( string $date ) Returns the formatted date
+ * @method string|array config(string $name = null, string|array $default = null) Returns the config value for the given key
+ * @method string date(string $date) Returns the formatted date
  * @method MW_View_Helper_Interface encoder() Returns the encoder object
- * @method string formparam( string|array $names ) Returns the name for the HTML form parameter
+ * @method string formparam(string|array $names) Returns the name for the HTML form parameter
  * @method MW_Mail_Message_Interface mail() Returns the e-mail message object
- * @method string number( integer|float|decimal $number, integer $decimals ) Returns the formatted number
- * @method mixed param( string|null $name, mixed $default ) Returns the parameter value
- * @method string translate( string $domain, string $singular, string $plural = '', integer $number = 1 ) Returns the translated string or the original one if no translation is available
- * @method string url( string|null $target, string|null $controller = null, string|null $action = null, array $params = array(), array $trailing = array(), array $config = array() ) Returns the URL assembled from the given arguments
+ * @method string number(integer|float|decimal $number, integer $decimals) Returns the formatted number
+ * @method string|array param(string|null $name, string|array $default) Returns the parameter value
+ * @method string translate(string $domain, string $singular, string $plural = '', integer $number = 1) Returns the translated string or the original one if no translation is available
+ * @method string url(string|null $target, string|null $controller = null, string|null $action = null, array $params = array(), array $trailing = array(), array $config = array()) Returns the URL assembled from the given arguments
  *
  * @package MW
  * @subpackage View
