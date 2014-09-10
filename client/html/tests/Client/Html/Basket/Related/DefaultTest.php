84c8fc237d5a157c14829d1eb8f27cf6ad6e59f3
Norbert <norbert@linuxnetworks.de>
2014-09-10 21:19:33 +0200

    Adds related basket html client with bought together part

---
+++ client/html/tests/Client/Html/Basket/Related/DefaultTest.php
@@ -0,0 +1,75 @@
+<?php
+
+/**
+ * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
+ */
+
+class Client_Html_Basket_Related_DefaultTest extends MW_Unittest_Testcase
+{
+	private $_object;
+	private $_context;
+
+
+	/**
+	 * Sets up the fixture, for example, opens a network connection.
+	 * This method is called before a test is executed.
+	 *
+	 * @access protected
+	 */
+	protected function setUp()
+	{
+		$this->_context = TestHelper::getContext();
+
+		$paths = TestHelper::getHtmlTemplatePaths();
+		$this->_object = new Client_Html_Basket_Related_Default( $this->_context, $paths );
+		$this->_object->setView( TestHelper::getView() );
+	}
+
+
+	/**
+	 * Tears down the fixture, for example, closes a network connection.
+	 * This method is called after a test is executed.
+	 *
+	 * @access protected
+	 */
+	protected function tearDown()
+	{
+		Controller_Frontend_Basket_Factory::createController( $this->_context )->clear();
+		unset( $this->_object );
+	}
+
+
+	public function testGetHeader()
+	{
+		$output = $this->_object->getHeader();
+		$this->assertNotNull( $output );
+	}
+
+
+	public function testGetBody()
+	{
+		$output = $this->_object->getBody();
+		$this->assertStringStartsWith( '<section class="arcavias basket-related">', $output );
+	}
+
+
+	public function testGetSubClient()
+	{
+		$client = $this->_object->getSubClient( 'bought', 'Default' );
+		$this->assertInstanceOf( 'Client_HTML_Interface', $client );
+	}
+
+
+	public function testGetSubClientInvalid()
+	{
+		$this->setExpectedException( 'Client_Html_Exception' );
+		$this->_object->getSubClient( 'invalid', 'invalid' );
+	}
+
+
+	public function testGetSubClientInvalidName()
+	{
+		$this->setExpectedException( 'Client_Html_Exception' );
+		$this->_object->getSubClient( '$$$', '$$$' );
+	}
+}
