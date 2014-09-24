1b794adf7bdabec24f75cc61e08cfbbd086f26cc
Norbert <norbert@linuxnetworks.de>
2014-09-24 22:45:59 +0200

    Implements factory tests for MAdmin domains

---
+++ lib/mshoplib/tests/MAdmin/Job/Manager/FactoryTest.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
+ */
+
+
+/**
+ * Test class for MAdmin_Job_Manager_Factory.
+ */
+class MAdmin_Job_Manager_FactoryTest extends MW_Unittest_Testcase
+{
+	public function testCreateManager()
+	{
+		$manager = MAdmin_Job_Manager_Factory::createManager( TestHelper::getContext() );
+		$this->assertInstanceOf( 'MShop_Common_Manager_Interface', $manager );
+	}
+
+
+	public function testCreateManagerName()
+	{
+		$manager = MAdmin_Job_Manager_Factory::createManager( TestHelper::getContext(), 'Default' );
+		$this->assertInstanceOf( 'MShop_Common_Manager_Interface', $manager );
+	}
+
+
+	public function testCreateManagerInvalidName()
+	{
+		$this->setExpectedException( 'MAdmin_Job_Exception' );
+		MAdmin_Job_Manager_Factory::createManager( TestHelper::getContext(), '%^' );
+	}
+
+
+	public function testCreateManagerNotExisting()
+	{
+		$this->setExpectedException( 'MShop_Exception' );
+		MAdmin_Job_Manager_Factory::createManager( TestHelper::getContext(), 'notexist' );
+	}
+
+}
---
+++ lib/mshoplib/tests/MAdmin/Log/Manager/FactoryTest.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
+ */
+
+
+/**
+ * Test class for MAdmin_Log_Manager_Factory.
+ */
+class MAdmin_Log_Manager_FactoryTest extends MW_Unittest_Testcase
+{
+	public function testCreateManager()
+	{
+		$manager = MAdmin_Log_Manager_Factory::createManager( TestHelper::getContext() );
+		$this->assertInstanceOf( 'MShop_Common_Manager_Interface', $manager );
+	}
+
+
+	public function testCreateManagerName()
+	{
+		$manager = MAdmin_Log_Manager_Factory::createManager( TestHelper::getContext(), 'Default' );
+		$this->assertInstanceOf( 'MShop_Common_Manager_Interface', $manager );
+	}
+
+
+	public function testCreateManagerInvalidName()
+	{
+		$this->setExpectedException( 'MAdmin_Log_Exception' );
+		MAdmin_Log_Manager_Factory::createManager( TestHelper::getContext(), '%^' );
+	}
+
+
+	public function testCreateManagerNotExisting()
+	{
+		$this->setExpectedException( 'MShop_Exception' );
+		MAdmin_Log_Manager_Factory::createManager( TestHelper::getContext(), 'notexist' );
+	}
+
+}
