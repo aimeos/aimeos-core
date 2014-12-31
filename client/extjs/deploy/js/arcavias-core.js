a1789fa8e0fb9700aad0f82b37a56aa4a8b6dcd0
Aimeos <aimeos@aimeos.org>
2014-12-31 10:40:13 +0100

    Update for concatenated ExtJS file

--- client/extjs/deploy/js/arcavias-core.js
+++ client/extjs/deploy/js/arcavias-core.js
@@ -8857,6 +8857,299 @@ Ext.ux.ItemRegistry.registerItem('MShop.panel.product.ItemUi', 'MShop.panel.prod
     }
 }, 50);
 /*!
+ * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
+ * @copyright Aimeos (aimeos.org), 2014
+ */
+
+Ext.ns('MShop.panel.product.property');
+
+/**
+ * Concrete ItemUi
+ * 
+ * @extends Mshop.panel.AbstractListItemUi
+ */
+MShop.panel.product.property.ItemUi = Ext.extend(MShop.panel.AbstractListItemUi, {
+
+    recordName : 'Product_Property',
+    idProperty : 'product.property.id',
+    siteidProperty : 'product.property.siteid',
+
+    initComponent : function() {
+
+        this.title = MShop.I18n.dt('client/extjs', 'Properties');
+        this.typeStore = MShop.GlobalStoreMgr.get('Product_Property_Type');
+
+        MShop.panel.AbstractItemUi.prototype.setSiteCheck(this);
+
+        this.items = [{
+            xtype : 'tabpanel',
+            activeTab : 0,
+            border : false,
+            itemId : 'MShop.panel.product.property.ItemUi',
+            plugins : ['ux.itemregistry'],
+            items : [{
+                xtype : 'panel',
+                title : MShop.I18n.dt('client/extjs', 'Basic'),
+                border : false,
+                layout : 'hbox',
+                layoutConfig : {
+                    align : 'stretch'
+                },
+                itemId : 'MShop.panel.product.property.ItemUi.BasicPanel',
+
+                plugins : ['ux.itemregistry'],
+                defaults : {
+                    bodyCssClass : this.readOnlyClass
+                },
+                items : [{
+                    xtype : 'form',
+                    title : MShop.I18n.dt('client/extjs', 'Details'),
+                    flex : 1,
+                    ref : '../../mainForm',
+                    autoScroll : true,
+                    items : [{
+                        xtype : 'fieldset',
+                        style : 'padding-right: 25px;',
+                        border : false,
+                        labelAlign : 'top',
+                        defaults : {
+                            readOnly : this.fieldsReadOnly,
+                            anchor : '100%'
+                        },
+                        items : [{
+                            xtype : 'hidden',
+                            name : 'product.property.parentid'
+                        }, {
+                            xtype : 'displayfield',
+                            fieldLabel : MShop.I18n.dt('client/extjs', 'ID'),
+                            name : 'product.property.id'
+                        }, {
+                            xtype : 'combo',
+                            fieldLabel : MShop.I18n.dt('client/extjs', 'Type'),
+                            name : 'product.property.typeid',
+                            mode : 'local',
+                            store : this.typeStore,
+                            displayField : 'product.property.type.label',
+                            valueField : 'product.property.type.id',
+                            forceSelection : true,
+                            triggerAction : 'all',
+                            allowBlank : false,
+                            typeAhead : true
+                        }, {
+                            xtype : 'MShop.elements.language.combo',
+                            name : 'product.property.languageid'
+                        }, {
+                            xtype : 'textfield',
+                            fieldLabel : MShop.I18n.dt('client/extjs', 'Value'),
+                            name : 'product.property.value',
+                            allowBlank : false,
+                            emptyText : MShop.I18n.dt('client/extjs', 'Property value (required)')
+                        }, {
+                            xtype : 'displayfield',
+                            fieldLabel : MShop.I18n.dt('client/extjs', 'Created'),
+                            name : 'product.property.ctime'
+                        }, {
+                            xtype : 'displayfield',
+                            fieldLabel : MShop.I18n.dt('client/extjs', 'Last modified'),
+                            name : 'product.property.mtime'
+                        }, {
+                            xtype : 'displayfield',
+                            fieldLabel : MShop.I18n.dt('client/extjs', 'Editor'),
+                            name : 'product.property.editor'
+                        }]
+                    }]
+                }]
+            }]
+        }];
+
+        MShop.panel.product.property.ItemUi.superclass.initComponent.call(this);
+    },
+
+    onSaveItem : function() {
+        // validate data
+        if(!this.mainForm.getForm().isValid() && this.fireEvent('validate', this) !== false) {
+            Ext.Msg.alert(MShop.I18n.dt('client/extjs', 'Invalid data'), MShop.I18n.dt('client/extjs',
+                'Please recheck your data'));
+            return;
+        }
+
+        this.saveMask.show();
+        this.isSaveing = true;
+
+        // force record to be saved!
+        this.record.dirty = true;
+
+        if(this.fireEvent('beforesave', this, this.record) === false) {
+            this.isSaveing = false;
+            this.saveMask.hide();
+        }
+
+        this.mainForm.getForm().updateRecord(this.record);
+        this.record.data['product.property.parentid'] = this.listUI.itemUi.record.id;
+
+        if(this.action == 'add' || this.action == 'copy') {
+            this.store.add(this.record);
+        }
+
+        // store async action is triggered. {@see onStoreWrite/onStoreException}
+        if(!this.store.autoSave) {
+            this.onAfterSave();
+        }
+    },
+
+    onStoreException : function() {
+        this.store.remove(this.record);
+        MShop.panel.product.property.ItemUi.superclass.onStoreException.apply(this, arguments);
+    }
+});
+
+Ext.reg('MShop.panel.product.property.itemui', MShop.panel.product.property.ItemUi);
+/*!
+ * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
+ * @copyright Aimeos (aimeos.org), 2014
+ */
+
+Ext.ns('MShop.panel.product.property');
+
+MShop.panel.product.property.ListUi = Ext.extend(MShop.panel.AbstractListUi, {
+
+    recordName : 'Product_Property',
+    idProperty : 'product.property.id',
+    siteidProperty : 'product.property.siteid',
+    itemUiXType : 'MShop.panel.product.property.itemui',
+
+    autoExpandColumn : 'product-property-value',
+
+    filterConfig : {
+        filters : [{
+            dataIndex : 'product.property.type.label',
+            operator : '=~',
+            value : ''
+        }]
+    },
+    
+    sortInfo : {
+        field : 'product.property.type.label',
+        direction : 'ASC'
+    },
+
+
+    initComponent : function() {
+        this.title = MShop.I18n.dt('client/extjs', 'Property');
+
+        MShop.panel.AbstractListUi.prototype.initActions.call(this);
+        MShop.panel.AbstractListUi.prototype.initToolbar.call(this);
+
+        MShop.panel.product.property.ListUi.superclass.initComponent.call(this);
+    },
+
+    afterRender : function() {
+        this.itemUi = this.findParentBy(function(c) {
+            return c.isXType(MShop.panel.AbstractItemUi, false);
+        });
+
+        MShop.panel.product.property.ListUi.superclass.afterRender.apply(this, arguments);
+    },
+
+    onBeforeLoad : function(store, options) {
+        this.setSiteParam(store);
+
+        if(this.domain) {
+            this.setDomainFilter(store, options);
+        }
+
+        options.params = options.params || {};
+        options.params.condition = {
+            '&&' : [{
+                '==' : {
+                    'product.property.parentid' : this.itemUi.record ? this.itemUi.record.id : null
+                }
+            }]
+        };
+
+    },
+
+    getColumns : function() {
+        this.typeStore = MShop.GlobalStoreMgr.get('Product_Property_Type');
+
+        return [
+            {
+                xtype : 'gridcolumn',
+                dataIndex : 'product.property.id',
+                header : MShop.I18n.dt('client/extjs', 'ID'),
+                sortable : true,
+                width : 50,
+                hidden : true
+            },
+            {
+                xtype : 'gridcolumn',
+                dataIndex : 'product.property.parentid',
+                header : MShop.I18n.dt('client/extjs', 'Product ID'),
+                width : 50,
+                hidden : true
+            },
+            {
+                xtype : 'gridcolumn',
+                dataIndex : 'product.property.typeid',
+                header : MShop.I18n.dt('client/extjs', 'Type'),
+                align : 'center',
+                renderer : this.typeColumnRenderer.createDelegate(this, [
+                    this.typeStore, "product.property.type.label"
+                    ], true)
+            },
+            {
+                xtype : 'gridcolumn',
+                dataIndex : 'product.property.languageid',
+                header : MShop.I18n.dt('client/extjs', 'Language'),
+                align : 'center',
+                sortable : true,
+                width : 50,
+                renderer : MShop.elements.language.renderer
+            },
+            {
+                xtype : 'gridcolumn',
+                dataIndex : 'product.property.value',
+                header : MShop.I18n.dt('client/extjs', 'Value'),
+                id : 'product-property-value',
+                sortable : true
+            },
+            {
+                xtype : 'datecolumn',
+                dataIndex : 'product.property.ctime',
+                header : MShop.I18n.dt('client/extjs', 'Created'),
+                format : 'Y-m-d H:i:s',
+                sortable : true,
+                width : 130,
+                editable : false,
+                hidden : true
+            },
+            {
+                xtype : 'datecolumn',
+                dataIndex : 'product.property.mtime',
+                header : MShop.I18n.dt('client/extjs', 'Last modified'),
+                format : 'Y-m-d H:i:s',
+                sortable : true,
+                width : 130,
+                editable : false,
+                hidden : true
+            },
+            {
+                xtype : 'gridcolumn',
+                dataIndex : 'product.property.editor',
+                header : MShop.I18n.dt('client/extjs', 'Editor'),
+                sortable : true,
+                width : 130,
+                editable : false,
+                hidden : true
+            }];
+    }
+});
+
+Ext.reg('MShop.panel.product.property.listui', MShop.panel.product.property.ListUi);
+
+//hook this into the product item tab panel
+Ext.ux.ItemRegistry.registerItem('MShop.panel.product.ItemUi', 'MShop.panel.product.property.listui', MShop.panel.product.property.ListUi, 5);
+/*!
  * Copyright (c) Metaways Infosystems GmbH, 2011
  * LGPLv3, http://www.arcavias.com/en/license
  */
