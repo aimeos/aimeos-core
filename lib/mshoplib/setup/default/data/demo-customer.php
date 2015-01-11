1e606156ec77ed2d29324bf1bc02f3304e345e09
Aimeos <aimeos@aimeos.org>
2015-01-11 14:15:18 +0100

    Create demo user with fixed password

--- lib/mshoplib/setup/default/data/demo-customer.php
+++ lib/mshoplib/setup/default/data/demo-customer.php
@@ -12,7 +12,6 @@ return array(
 		'address1' => 'Test street', 'address2' => '1', 'address3' => '', 'postal' => '1000',
 		'city' => 'Test city', 'state' => '', 'langid' => 'en', 'countryid' => 'DE',
 		'telephone' => '', 'email' => 'me@localhost', 'telefax' => '', 'website' => '',
-		'birthday' => null, 'password' => sha1( microtime(true) . getmypid() . rand() ),
-		'vtime' => null, 'status' => 1,
+		'birthday' => null, 'password' => 'test', 'vtime' => null, 'status' => 1,
 	),
 );
