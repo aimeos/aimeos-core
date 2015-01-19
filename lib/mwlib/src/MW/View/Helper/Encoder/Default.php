2691fed6071d4c26a7d4e282a6076c2338417471
Aimeos <aimeos@aimeos.org>
2015-01-19 15:50:35 +0100

    Improve encoder for url parts

--- lib/mwlib/src/MW/View/Helper/Encoder/Default.php
+++ lib/mwlib/src/MW/View/Helper/Encoder/Default.php
@@ -38,6 +38,7 @@ class MW_View_Helper_Encoder_Default
 	 * All attribute values must be surrounded by " (double quote)
 	 *
 	 * @param string $value Attribute string
+	 * @param integer $trust Zero to distrust the input, one (1) if you trust in it
 	 * @return string Escaped attribute string
 	 */
 	public function attr( $value, $trust = self::TAINT )
@@ -50,6 +51,7 @@ class MW_View_Helper_Encoder_Default
 	 * Escapes strings for HTML.
 	 *
 	 * @param string $value HTML string
+	 * @param integer $trust Zero to distrust the input, one (1) if you trust in it
 	 * @return string Escaped HTML string
 	 */
 	public function html( $value, $trust = self::TAINT )
@@ -67,6 +69,7 @@ class MW_View_Helper_Encoder_Default
 	 * All node values must be surrounded by <![CDATA[...]]>
 	 *
 	 * @param string $value XML string
+	 * @param integer $trust Zero to distrust the input, one (1) if you trust in it
 	 * @return string Escaped XML string
 	 */
 	public function xml( $value, $trust = self::TAINT )
@@ -83,10 +86,21 @@ class MW_View_Helper_Encoder_Default
 	 * Escapes strings for URLs.
 	 *
 	 * @param string $value URI/URL string
+	 * @param integer $trust Zero to distrust the input, one (1) if you trust in it
+	 * @param boolean $strip Stip HTML tags if they are part of the input
+	 * @param array $replace Associative list of characters or strings that should be replaced 
 	 * @return string Escaped URI/URL string
 	 */
-	public function url( $value, $trust = self::TAINT )
+	public function url( $value, $trust = self::TAINT, $strip = true, $replace = array( ' ' => '_' ) )
 	{
-		return urlencode( htmlspecialchars( $value, ENT_NOQUOTES, 'UTF-8' ) );
+		if( $strip !== false ) {
+			$value = strip_tags( $value );
+		}
+
+		foreach( $replace as $key => $val ) {
+			$value = str_replace( $key, $val, $value );
+		}
+
+		return urlencode( $value );
 	}
 }
