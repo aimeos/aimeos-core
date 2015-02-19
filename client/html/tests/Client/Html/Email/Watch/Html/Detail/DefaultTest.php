f3219a186d1e4f823330b8cced7a92825684a79c
aimeos <aimeos@aimeos.org>
2015-02-19 11:17:58 +0100

    Improve watch e-mails

--- client/html/tests/Client/Html/Email/Watch/Html/Detail/DefaultTest.php
+++ client/html/tests/Client/Html/Email/Watch/Html/Detail/DefaultTest.php
@@ -83,7 +83,7 @@ class Client_Html_Email_Watch_Html_Detail_DefaultTest extends MW_Unittest_Testca
 	{
 		$output = $this->_object->getBody();
 
-		$this->assertStringStartsWith( '<div class="common-summary-detail common-summary container">', $output );
+		$this->assertStringStartsWith( '<div class="common-summary-detail common-summary container content-block">', $output );
 		$this->assertContains( 'Cafe Noire Cappuccino', $output );
 		$this->assertContains( 'Cafe Noire Expresso', $output );
 	}
