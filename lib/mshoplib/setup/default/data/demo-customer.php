eb73974d0d0f0cffe66804bb10982481f39e7a22
Aimeos <aimeos@aimeos.org>
2015-03-27 13:39:06 +0100

    Extend customer address demo data

--- lib/mshoplib/setup/default/data/demo-customer.php
+++ lib/mshoplib/setup/default/data/demo-customer.php
@@ -13,5 +13,14 @@ return array(
 		'city' => 'Test city', 'state' => '', 'langid' => 'en', 'countryid' => 'DE',
 		'telephone' => '', 'email' => 'demo@example.com', 'telefax' => '', 'website' => '',
 		'birthday' => null, 'password' => 'test', 'vtime' => null, 'status' => 1,
+		'delivery' => array(
+			array(
+				'salutation' => 'mr', 'company' => 'Test company', 'vatid' => 'DE999999999',
+				'title' => '', 'firstname' => 'Test', 'lastname' => 'User',
+				'address1' => 'Test street', 'address2' => '1', 'address3' => '', 'postal' => '1000',
+				'city' => 'Test city', 'state' => '', 'langid' => 'en', 'countryid' => 'DE',
+				'telephone' => '', 'email' => 'demo@example.com', 'telefax' => '', 'website' => '',
+			),
+		),
 	),
 );
