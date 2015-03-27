74f8e0d5e2e7b1ddcf8002da4852c71a1a01b4b9
Aimeos <aimeos@aimeos.org>
2015-03-27 14:12:32 +0100

    Add state

--- lib/mshoplib/setup/default/data/demo-customer.php
+++ lib/mshoplib/setup/default/data/demo-customer.php
@@ -10,7 +10,7 @@ return array(
 		'code' => 'demo-test', 'label' => 'Test user', 'salutation' => 'mr',
 		'company' => 'Test company', 'vatid' => 'DE999999999', 'title' => '', 'firstname' => 'Test', 'lastname' => 'User',
 		'address1' => 'Test street', 'address2' => '1', 'address3' => '', 'postal' => '10000',
-		'city' => 'Test city', 'state' => '', 'langid' => 'en', 'countryid' => 'US',
+		'city' => 'Test city', 'state' => 'CA', 'langid' => 'en', 'countryid' => 'US',
 		'telephone' => '', 'email' => 'demo@example.com', 'telefax' => '', 'website' => '',
 		'birthday' => null, 'password' => 'test', 'vtime' => null, 'status' => 1,
 		'delivery' => array(
@@ -18,7 +18,7 @@ return array(
 				'salutation' => 'mrs', 'company' => 'Demo company', 'vatid' => 'DE999999999',
 				'title' => '', 'firstname' => 'Test', 'lastname' => 'User',
 				'address1' => 'Demo street', 'address2' => '100', 'address3' => '', 'postal' => '12345',
-				'city' => 'Demo city', 'state' => '', 'langid' => 'en', 'countryid' => 'US',
+				'city' => 'Demo city', 'state' => 'NY', 'langid' => 'en', 'countryid' => 'US',
 				'telephone' => '', 'email' => 'demo@example.com', 'telefax' => '', 'website' => '',
 			),
 		),
