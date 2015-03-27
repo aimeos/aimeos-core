510016151ef3dcd34085fc221b70d89afd9186d3
Aimeos <aimeos@aimeos.org>
2015-03-27 13:56:18 +0100

    Change default address format for English speaking countries

--- lib/mshoplib/setup/default/data/demo-customer.php
+++ lib/mshoplib/setup/default/data/demo-customer.php
@@ -9,16 +9,16 @@ return array(
 	array(
 		'code' => 'demo-test', 'label' => 'Test user', 'salutation' => 'mr',
 		'company' => 'Test company', 'vatid' => 'DE999999999', 'title' => '', 'firstname' => 'Test', 'lastname' => 'User',
-		'address1' => 'Test street', 'address2' => '1', 'address3' => '', 'postal' => '1000',
-		'city' => 'Test city', 'state' => '', 'langid' => 'en', 'countryid' => 'DE',
+		'address1' => 'Test street', 'address2' => '1', 'address3' => '', 'postal' => '10000',
+		'city' => 'Test city', 'state' => 'CA', 'langid' => 'en', 'countryid' => 'US',
 		'telephone' => '', 'email' => 'demo@example.com', 'telefax' => '', 'website' => '',
 		'birthday' => null, 'password' => 'test', 'vtime' => null, 'status' => 1,
 		'delivery' => array(
 			array(
-				'salutation' => 'mr', 'company' => 'Test company', 'vatid' => 'DE999999999',
+				'salutation' => 'mrs', 'company' => 'Demo company', 'vatid' => 'DE999999999',
 				'title' => '', 'firstname' => 'Test', 'lastname' => 'User',
-				'address1' => 'Test street', 'address2' => '1', 'address3' => '', 'postal' => '1000',
-				'city' => 'Test city', 'state' => '', 'langid' => 'en', 'countryid' => 'DE',
+				'address1' => 'Demo street', 'address2' => '100', 'address3' => '', 'postal' => '12345',
+				'city' => 'Demo city', 'state' => 'NY', 'langid' => 'en', 'countryid' => 'US',
 				'telephone' => '', 'email' => 'demo@example.com', 'telefax' => '', 'website' => '',
 			),
 		),
