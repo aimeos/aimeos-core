21a9fb537adc436a34abd03741b317af8605241f
Aimeos <aimeos@aimeos.org>
2015-04-18 15:47:56 +0200

    Fixes faceted search for Chrome

--- client/html/themes/aimeos.js
+++ client/html/themes/aimeos.js
@@ -579,18 +579,6 @@ AimeosCatalogFilter = {
 
 
 	/**
-	 * Submits the form when clicking on filter attribute input fields
-	 */
-	setupAttributeInputSubmit: function() {
-
-		$(".catalog-filter-attribute input.attr-item").on("click", function() {
-			$(this).parents(".catalog-filter form").submit();
-			$(".catalog-list").fadeTo(1000, 0.5);
-		});
-	},
-
-
-	/**
 	 * Initialize the catalog filter actions
 	 */
 	init: function() {
