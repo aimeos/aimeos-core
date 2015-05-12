c8fb8bf47da0d823870b7c34fcd4ed18b691061e
Aimeos <aimeos@aimeos.org>
2015-05-12 22:48:27 +0200

    Improve tests for job controller factory

---
+++ controller/jobs/tests/Controller/Jobs/AbstractTest.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
+ * @copyright Aimeos (aimeos.org), 2015
+ */
+
+
+class Controller_Jobs_AbstractTest extends MW_Unittest_Testcase
+{
+	private $_object;
+
+
+	public function setUp()
+	{
+		$context = TestHelper::getContext();
+		$arcavias = TestHelper::getArcavias();
+
+		$this->_object = new Controller_Jobs_TestAbstract( $context, $arcavias );
+	}
+
+
+	public function testGetTypeItemNotFound()
+	{
+		$this->setExpectedException( 'Controller_Jobs_Exception' );
+		$this->_object->getTypeItemPublic( 'product/type', 'product', 'test' );
+	}
+
+
+	public function testGetTemplateNotFound()
+	{
+		$this->setExpectedException( 'Controller_Jobs_Exception' );
+		$this->_object->getTemplatePublic( 'test', 'test' );
+	}
+}
+
+
+
+class Controller_Jobs_TestAbstract extends Controller_Jobs_Abstract
+{
+	public function getTemplatePublic( $confpath, $default )
+	{
+		$this->_getTemplate( $confpath, $default );
+	}
+
+	public function getTypeItemPublic( $prefix, $domain, $code )
+	{
+		$this->_getTypeItem( $prefix, $domain, $code );
+	}
+}
