53ae05749f14fce5f1f22c2f5a795ae48c22d870
Aimeos <aimeos@aimeos.org>
2015-08-24 19:43:48 +0200

    Fixed displayed products catalog list promo client

--- client/html/src/Client/Html/Catalog/List/Promo/Default.php
+++ client/html/src/Client/Html/Catalog/List/Promo/Default.php
@@ -287,7 +287,7 @@ class Client_Html_Catalog_List_Promo_Default
 				$catId = $view->listCurrentCatItem->getId();
 
 				$controller = Controller_Frontend_Factory::createController( $context, 'catalog' );
-				$filter = $controller->createIndexFilterCategory( $catId, 'position', '+', 0, $size, 'promotion' );
+				$filter = $controller->createIndexFilterCategory( $catId, 'relevance', '+', 0, $size, 'promotion' );
 				$products = $controller->getIndexItems( $filter, $domains, $total );
 			}
 
