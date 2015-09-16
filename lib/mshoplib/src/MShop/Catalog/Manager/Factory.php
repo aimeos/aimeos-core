1dd1c824b6b88405cda60d3fdb6787e87560d822
Aimeos <aimeos@aimeos.org>
2015-09-16 10:34:43 +0200

    Moved documentation for manager factories

--- lib/mshoplib/src/MShop/Catalog/Manager/Factory.php
+++ lib/mshoplib/src/MShop/Catalog/Manager/Factory.php
@@ -28,6 +28,39 @@ class MShop_Catalog_Manager_Factory
 	 */
 	public static function createManager( MShop_Context_Item_Interface $context, $name = null )
 	{
+		/** classes/catalog/manager/name
+		 * Class name of the used catalog manager implementation
+		 *
+		 * Each default manager can be replace by an alternative imlementation.
+		 * To use this implementation, you have to set the last part of the class
+		 * name as configuration value so the manager factory knows which class it
+		 * has to instantiate.
+		 *
+		 * For example, if the name of the default class is
+		 *
+		 *  MShop_Catalog_Manager_Default
+		 *
+		 * and you want to replace it with your own version named
+		 *
+		 *  MShop_Catalog_Manager_Mymanager
+		 *
+		 * then you have to set the this configuration option:
+		 *
+		 *  classes/catalog/manager/name = Mymanager
+		 *
+		 * The value is the last part of your own class name and it's case sensitive,
+		 * so take care that the configuration value is exactly named like the last
+		 * part of the class name.
+		 *
+		 * The allowed characters of the class name are A-Z, a-z and 0-9. No other
+		 * characters are possible! You should always start the last part of the class
+		 * name with an upper case character and continue only with lower case characters
+		 * or numbers. Avoid chamel case names like "MyManager"!
+		 *
+		 * @param string Last part of the class name
+		 * @since 2014.03
+		 * @category Developer
+		 */
 		if( $name === null ) {
 			$name = $context->getConfig()->get( 'classes/catalog/manager/name', 'Default' );
 		}
@@ -42,6 +75,81 @@ class MShop_Catalog_Manager_Factory
 		$classname = 'MShop_Catalog_Manager_' . $name;
 
 		$manager = self::_createManager( $context, $classname, $iface );
+
+		/** mshop/catalog/manager/decorators/excludes
+		 * Excludes decorators added by the "common" option from the catalog manager
+		 *
+		 * Decorators extend the functionality of a class by adding new aspects
+		 * (e.g. log what is currently done), executing the methods of the underlying
+		 * class only in certain conditions (e.g. only for logged in users) or
+		 * modify what is returned to the caller.
+		 *
+		 * This option allows you to remove a decorator added via
+		 * "mshop/common/manager/decorators/default" before they are wrapped
+		 * around the catalog manager.
+		 *
+		 *  mshop/catalog/manager/decorators/excludes = array( 'decorator1' )
+		 *
+		 * This would remove the decorator named "decorator1" from the list of
+		 * common decorators ("MShop_Common_Manager_Decorator_*") added via
+		 * "mshop/common/manager/decorators/default" for the catalog manager.
+		 *
+		 * @param array List of decorator names
+		 * @since 2014.03
+		 * @category Developer
+		 * @see mshop/common/manager/decorators/default
+		 * @see mshop/catalog/manager/decorators/global
+		 * @see mshop/catalog/manager/decorators/local
+		 */
+
+		/** mshop/catalog/manager/decorators/global
+		 * Adds a list of globally available decorators only to the catalog manager
+		 *
+		 * Decorators extend the functionality of a class by adding new aspects
+		 * (e.g. log what is currently done), executing the methods of the underlying
+		 * class only in certain conditions (e.g. only for logged in users) or
+		 * modify what is returned to the caller.
+		 *
+		 * This option allows you to wrap global decorators
+		 * ("MShop_Common_Manager_Decorator_*") around the catalog manager.
+		 *
+		 *  mshop/catalog/manager/decorators/global = array( 'decorator1' )
+		 *
+		 * This would add the decorator named "decorator1" defined by
+		 * "MShop_Common_Manager_Decorator_Decorator1" only to the catalog controller.
+		 *
+		 * @param array List of decorator names
+		 * @since 2014.03
+		 * @category Developer
+		 * @see mshop/common/manager/decorators/default
+		 * @see mshop/catalog/manager/decorators/excludes
+		 * @see mshop/catalog/manager/decorators/local
+		 */
+
+		/** mshop/catalog/manager/decorators/local
+		 * Adds a list of local decorators only to the catalog manager
+		 *
+		 * Decorators extend the functionality of a class by adding new aspects
+		 * (e.g. log what is currently done), executing the methods of the underlying
+		 * class only in certain conditions (e.g. only for logged in users) or
+		 * modify what is returned to the caller.
+		 *
+		 * This option allows you to wrap local decorators
+		 * ("MShop_Common_Manager_Decorator_*") around the catalog manager.
+		 *
+		 *  mshop/catalog/manager/decorators/local = array( 'decorator2' )
+		 *
+		 * This would add the decorator named "decorator2" defined by
+		 * "MShop_Common_Manager_Decorator_Decorator2" only to the catalog
+		 * controller.
+		 *
+		 * @param array List of decorator names
+		 * @since 2014.03
+		 * @category Developer
+		 * @see mshop/common/manager/decorators/default
+		 * @see mshop/catalog/manager/decorators/excludes
+		 * @see mshop/catalog/manager/decorators/global
+		 */
 		return self::_addManagerDecorators( $context, $manager, 'catalog' );
 	}
 }
--- lib/mshoplib/src/MShop/Media/Manager/Factory.php
+++ lib/mshoplib/src/MShop/Media/Manager/Factory.php
@@ -29,6 +29,39 @@ class MShop_Media_Manager_Factory
 	 */
 	public static function createManager( MShop_Context_Item_Interface $context, $name = null )
 	{
+		/** classes/media/manager/name
+		 * Class name of the used media manager implementation
+		 *
+		 * Each default manager can be replace by an alternative imlementation.
+		 * To use this implementation, you have to set the last part of the class
+		 * name as configuration value so the manager factory knows which class it
+		 * has to instantiate.
+		 *
+		 * For example, if the name of the default class is
+		 *
+		 *  MShop_Media_Manager_Default
+		 *
+		 * and you want to replace it with your own version named
+		 *
+		 *  MShop_Media_Manager_Mymanager
+		 *
+		 * then you have to set the this configuration option:
+		 *
+		 *  classes/media/manager/name = Mymanager
+		 *
+		 * The value is the last part of your own class name and it's case sensitive,
+		 * so take care that the configuration value is exactly named like the last
+		 * part of the class name.
+		 *
+		 * The allowed characters of the class name are A-Z, a-z and 0-9. No other
+		 * characters are possible! You should always start the last part of the class
+		 * name with an upper case character and continue only with lower case characters
+		 * or numbers. Avoid chamel case names like "MyManager"!
+		 *
+		 * @param string Last part of the class name
+		 * @since 2014.03
+		 * @category Developer
+		 */
 		if( $name === null ) {
 			$name = $context->getConfig()->get( 'classes/media/manager/name', 'Default' );
 		}
@@ -43,6 +76,81 @@ class MShop_Media_Manager_Factory
 		$classname = 'MShop_Media_Manager_' . $name;
 
 		$manager = self::_createManager( $context, $classname, $iface );
+
+		/** mshop/media/manager/decorators/excludes
+		 * Excludes decorators added by the "common" option from the media manager
+		 *
+		 * Decorators extend the functionality of a class by adding new aspects
+		 * (e.g. log what is currently done), executing the methods of the underlying
+		 * class only in certain conditions (e.g. only for logged in users) or
+		 * modify what is returned to the caller.
+		 *
+		 * This option allows you to remove a decorator added via
+		 * "mshop/common/manager/decorators/default" before they are wrapped
+		 * around the media manager.
+		 *
+		 *  mshop/media/manager/decorators/excludes = array( 'decorator1' )
+		 *
+		 * This would remove the decorator named "decorator1" from the list of
+		 * common decorators ("MShop_Common_Manager_Decorator_*") added via
+		 * "mshop/common/manager/decorators/default" for the media manager.
+		 *
+		 * @param array List of decorator names
+		 * @since 2014.03
+		 * @category Developer
+		 * @see mshop/common/manager/decorators/default
+		 * @see mshop/media/manager/decorators/global
+		 * @see mshop/media/manager/decorators/local
+		 */
+
+		/** mshop/media/manager/decorators/global
+		 * Adds a list of globally available decorators only to the media manager
+		 *
+		 * Decorators extend the functionality of a class by adding new aspects
+		 * (e.g. log what is currently done), executing the methods of the underlying
+		 * class only in certain conditions (e.g. only for logged in users) or
+		 * modify what is returned to the caller.
+		 *
+		 * This option allows you to wrap global decorators
+		 * ("MShop_Common_Manager_Decorator_*") around the media manager.
+		 *
+		 *  mshop/media/manager/decorators/global = array( 'decorator1' )
+		 *
+		 * This would add the decorator named "decorator1" defined by
+		 * "MShop_Common_Manager_Decorator_Decorator1" only to the media controller.
+		 *
+		 * @param array List of decorator names
+		 * @since 2014.03
+		 * @category Developer
+		 * @see mshop/common/manager/decorators/default
+		 * @see mshop/media/manager/decorators/excludes
+		 * @see mshop/media/manager/decorators/local
+		 */
+
+		/** mshop/media/manager/decorators/local
+		 * Adds a list of local decorators only to the media manager
+		 *
+		 * Decorators extend the functionality of a class by adding new aspects
+		 * (e.g. log what is currently done), executing the methods of the underlying
+		 * class only in certain conditions (e.g. only for logged in users) or
+		 * modify what is returned to the caller.
+		 *
+		 * This option allows you to wrap local decorators
+		 * ("MShop_Common_Manager_Decorator_*") around the media manager.
+		 *
+		 *  mshop/media/manager/decorators/local = array( 'decorator2' )
+		 *
+		 * This would add the decorator named "decorator2" defined by
+		 * "MShop_Common_Manager_Decorator_Decorator2" only to the media
+		 * controller.
+		 *
+		 * @param array List of decorator names
+		 * @since 2014.03
+		 * @category Developer
+		 * @see mshop/common/manager/decorators/default
+		 * @see mshop/media/manager/decorators/excludes
+		 * @see mshop/media/manager/decorators/global
+		 */
 		return self::_addManagerDecorators( $context, $manager, 'media' );
 	}
 
