fce70bf0c93129e8fa5560111540ed17cbc52eed
Aimeos <aimeos@aimeos.org>
2015-11-11 20:51:59 +0100

    Added missing CSRF token for catalog filter

--- client/html/templates/catalog/filter/body-default.php
+++ client/html/templates/catalog/filter/body-default.php
@@ -11,7 +11,7 @@
  * The destination can be a page ID like in a content management system or the
  * module of a software development framework. This "target" must contain or know
  * the controller that should be called by the generated URL.
- * 
+ *
  * @param string Destination of the URL
  * @since 2014.03
  * @category Developer
@@ -27,7 +27,7 @@ $listTarget = $this->config( 'client/html/catalog/list/url/target' );
  * In Model-View-Controller (MVC) applications, the controller contains the methods
  * that create parts of the output displayed in the generated HTML page. Controller
  * names are usually alpha-numeric.
- * 
+ *
  * @param string Name of the controller
  * @since 2014.03
  * @category Developer
@@ -43,7 +43,7 @@ $listController = $this->config( 'client/html/catalog/list/url/controller', 'cat
  * In Model-View-Controller (MVC) applications, actions are the methods of a
  * controller that create parts of the output displayed in the generated HTML page.
  * Action names are usually alpha-numeric.
- * 
+ *
  * @param string Name of the action
  * @since 2014.03
  * @category Developer
@@ -65,7 +65,7 @@ $listAction = $this->config( 'client/html/catalog/list/url/action', 'list' );
  * framework. This is because the infrastructure of the application is used for
  * generating the URLs. The full list of available config options is referenced
  * in the "see also" section of this page.
- * 
+ *
  * @param string Associative list of configuration options
  * @since 2014.03
  * @category Developer
@@ -97,6 +97,9 @@ $enc = $this->encoder();
 	<nav>
 		<h1><?php echo $enc->html( $this->translate( 'client/html', 'Filter' ), $enc::TRUST ); ?></h1>
 		<form method="POST" action="<?php echo $enc->attr( $this->url( $listTarget, $listController, $listAction, $listParams, array(), $listConfig ) ); ?>">
+<!-- catalog.filter.csrf -->
+<?php echo $this->csrf()->formfield(); ?>
+<!-- catalog.filter.csrf -->
 <?php echo $this->get( 'filterBody' ); ?>
 		</form>
 	</nav>
