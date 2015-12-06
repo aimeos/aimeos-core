3b90932a5cf9916b940e6920a7d32438ff03576b
Norbert <norbert@linuxnetworks.de>
2015-12-06 18:53:42 +0100

    Adds a service decorator for testing if products are downloads or not

---
+++ lib/mshoplib/src/MShop/Service/Provider/Decorator/Download.php
@@ -0,0 +1,94 @@
+<?php
+
+/**
+ * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
+ * @copyright Aimeos (aimeos.org), 2015
+ * @package MShop
+ * @subpackage Service
+ */
+
+
+namespace Aimeos\MShop\Service\Provider\Decorator;
+
+
+/**
+ * Download check decorator for service providers.
+ *
+ * @package MShop
+ * @subpackage Service
+ */
+class Download
+	extends \Aimeos\MShop\Service\Provider\Decorator\Base
+{
+	private $beConfig = array(
+		'download.all' => array(
+			'code' => 'download.all',
+			'internalcode'=> 'download.all',
+			'label'=> 'Check products: "1" = all must be downloads, "0" = at least one is no download',
+			'type'=> 'boolean',
+			'internaltype'=> 'boolean',
+			'default'=> null,
+			'required'=> false,
+		),
+	);
+
+
+	/**
+	 * Checks the backend configuration attributes for validity.
+	 *
+	 * @param array $attributes Attributes added by the shop owner in the administraton interface
+	 * @return array An array with the attribute keys as key and an error message as values for all attributes that are
+	 * 	known by the provider but aren't valid
+	 */
+	public function checkConfigBE( array $attributes )
+	{
+		$error = $this->getProvider()->checkConfigBE( $attributes );
+		$error += $this->checkConfig( $this->beConfig, $attributes );
+
+		return $error;
+	}
+
+
+	/**
+	 * Returns the configuration attribute definitions of the provider to generate a list of available fields and
+	 * rules for the value of each field in the administration interface.
+	 *
+	 * @return array List of attribute definitions implementing \Aimeos\MW\Common\Critera\Attribute\Iface
+	 */
+	public function getConfigBE()
+	{
+		$list = $this->getProvider()->getConfigBE();
+
+		foreach( $this->beConfig as $key => $config ) {
+			$list[$key] = new \Aimeos\MW\Criteria\Attribute\Standard( $config );
+		}
+
+		return $list;
+	}
+
+
+	/**
+	 * Checks if the service provider should be available.
+	 *
+	 * Tests products in basket if they have a download attribute. The method
+	 * returns true if "download.all" is "1" and all products contain the
+	 * attribute resp. if "download.all" is "0" and at least one product
+	 * contains no download attribute.
+	 *
+	 * @param \Aimeos\MShop\Order\Item\Base\Iface $basket Basket object
+	 * @return boolean True if payment provider can be used, false if not
+	 */
+	public function isAvailable( \Aimeos\MShop\Order\Item\Base\Iface $basket )
+	{
+		$val = (bool) $this->getConfigValue( array( 'download.all' ) );
+
+		foreach( $basket->getProducts() as $product )
+		{
+			if( ((bool) count( $product->getAttributes( 'download' ) )) !== $val ) {
+				return !$val;
+			}
+		}
+
+		return $this->getProvider()->isAvailable( $basket );
+	}
+}
---
+++ lib/mshoplib/tests/MShop/Service/Provider/Decorator/DownloadTest.php
@@ -0,0 +1,121 @@
+<?php
+
+/**
+ * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
+ * @copyright Aimeos (aimeos.org), 2015
+ */
+
+
+namespace Aimeos\MShop\Service\Provider\Decorator;
+
+
+class DownloadTest extends \PHPUnit_Framework_TestCase
+{
+	private $object;
+	private $context;
+	private $servItem;
+	private $mockProvider;
+
+
+	protected function setUp()
+	{
+		$this->context = \TestHelper::getContext();
+
+		$servManager = \Aimeos\MShop\Service\Manager\Factory::createManager( $this->context );
+		$this->servItem = $servManager->createItem();
+
+		$this->mockProvider = $this->getMockBuilder( '\\Aimeos\\MShop\\Service\\Provider\\Decorator\\Example' )
+			->disableOriginalConstructor()->getMock();
+
+		$this->object = new \Aimeos\MShop\Service\Provider\Decorator\Download( $this->context, $this->servItem, $this->mockProvider );
+	}
+
+
+	protected function tearDown()
+	{
+		\Aimeos\MShop\Order\Manager\Factory::injectManager( '\\Aimeos\\MShop\\Order\\Manager\\StandardMock', null );
+	}
+
+
+	public function testGetConfigBE()
+	{
+		$result = $this->object->getConfigBE();
+
+		$this->assertArrayHasKey( 'download.all', $result );
+	}
+
+
+	public function testCheckConfigBEOK()
+	{
+		$this->mockProvider->expects( $this->once() )
+			->method( 'checkConfigBE' )
+			->will( $this->returnValue( array() ) );
+
+		$attributes = array( 'download.all' => '1' );
+		$result = $this->object->checkConfigBE( $attributes );
+
+		$this->assertEquals( 1, count( $result ) );
+		$this->assertInternalType( 'null', $result['download.all'] );
+	}
+
+
+	public function testCheckConfigBEFailure()
+	{
+		$this->mockProvider->expects( $this->once() )
+			->method( 'checkConfigBE' )
+			->will( $this->returnValue( array() ) );
+
+		$attributes = array( 'download.all' => array() );
+		$result = $this->object->checkConfigBE( $attributes );
+
+		$this->assertEquals( 1, count( $result ) );
+		$this->assertInternalType( 'string', $result['download.all'] );
+	}
+
+
+	public function testIsAvailableNoConfig()
+	{
+		$this->mockProvider->expects( $this->once() )
+			->method( 'isAvailable' )
+			->will( $this->returnValue( true ) );
+
+		$this->assertTrue( $this->object->isAvailable( $this->getOrderBaseItem() ) );
+	}
+
+
+	public function testIsAvailableOK()
+	{
+		$this->servItem->setConfig( array( 'download.all' => '0' ) );
+
+		$this->mockProvider->expects( $this->once() )
+			->method( 'isAvailable' )
+			->will( $this->returnValue( true ) );
+
+		$this->assertTrue( $this->object->isAvailable( $this->getOrderBaseItem() ) );
+	}
+
+
+	public function testIsAvailableFailure()
+	{
+		$this->servItem->setConfig( array( 'download.all' => '1' ) );
+
+		$this->assertFalse( $this->object->isAvailable( $this->getOrderBaseItem() ) );
+	}
+
+
+	protected function getOrderBaseItem()
+	{
+		$manager = \Aimeos\MShop\Factory::createManager( $this->context, 'order' );
+
+		$search = $manager->createSearch();
+		$search->setConditions( $search->compare( '==', 'order.datepayment', '2008-02-15 12:34:56' ) );
+		$result = $manager->searchItems( $search );
+
+		if( ( $item = reset( $result ) ) === false ) {
+			throw new \Exception( 'No order item found' );
+		}
+
+		$baseManager = \Aimeos\MShop\Factory::createManager( $this->context, 'order/base' );
+		return $baseManager->load( $item->getBaseId() );
+	}
+}
