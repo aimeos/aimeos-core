2d21210693f164ff47ad68cfdf8f7d1bf81e2905
Aimeos <aimeos@aimeos.org>
2015-12-20 19:35:29 +0100

    Implemented JQAdm part for managing product stock levels

---
+++ admin/jqadm/templates/product/item-stock-default.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
+ * @copyright Aimeos (aimeos.org), 2015
+ */
+
+$enc = $this->encoder();
+
+?>
+<div class="product-item-stock card panel">
+	<div id="product-item-stock" class="header card-header" role="tab" data-toggle="collapse" data-parent="#accordion" href="#product-item-stock-data" aria-expanded="true" aria-controls="product-item-stock-data">
+		<?php echo $enc->html( $this->translate( 'admin', 'Stock level' ) ); ?>
+	</div>
+	<div id="product-item-stock-data" class="item-stock card-block panel-collapse collapse" role="tabpanel" aria-labelledby="product-item-stock">
+		<table class="stock-list table table-default">
+			<thead>
+				<tr>
+			  		<th class="stock-warehouse"><?php echo $enc->html( $this->translate( 'admin', 'Warehouse' ) ); ?></th>
+			  		<th class="stock-stocklevel"><?php echo $enc->html( $this->translate( 'admin', 'Stock level' ) ); ?></th>
+			  		<th class="stock-databack"><?php echo $enc->html( $this->translate( 'admin', 'Back in stock' ) ); ?></th>
+					<th class="actions"><div class="btn btn-primary fa fa-plus"></div></th>
+				</tr>
+			</thead>
+			<tbody>
+<?php foreach( $this->get( 'stockItems', array() ) as $whid => $item ) : ?>
+				<tr>
+			  		<td class="stock-warehouse">
+						<select class="form-control" name="stock[product.stock.warehouseid][]">
+<?php	foreach( $this->get( 'stockWarehouses', array() ) as $warehouse ) : ?>
+<?php		if( $whid === $warehouse->getId() ) : ?>
+							<option value="<?php echo $enc->attr( $warehouse->getId() ); ?>" selected="selected"><?php echo $enc->html( $warehouse->getLabel() ) ?></option>
+<?php		else : ?>
+							<option value="<?php echo $enc->attr( $warehouse->getId() ); ?>"><?php echo $enc->html( $warehouse->getLabel() ) ?></option>
+<?php		endif; ?>
+<?php	endforeach; ?>
+						</select>
+					</td>
+			  		<td class="stock-stocklevel"><input type="text" class="form-control" name="stock[product.stock.stocklevel][]" value="<?php echo $enc->attr( $item->getStockLevel() ); ?>" /></td>
+					<td class="stock-databack"><input type="text" class="form-control" name="stock[product.stock.dateback][]" value="<?php echo $enc->attr( $item->getDateBack() ); ?>" /></td>
+					<td class="actions"><div class="btn btn-danger fa fa-trash"></div></td>
+				</tr>
+<?php endforeach; ?>
+				<tr class="prototype">
+			  		<td class="stock-warehouse">
+						<select class="form-control" name="stock[product.stock.warehouseid][]">
+<?php foreach( $this->get( 'stockWarehouses', array() ) as $warehouse ) : ?>
+							<option value="<?php echo $enc->attr( $warehouse->getId() ); ?>"><?php echo $enc->html( $warehouse->getLabel() ) ?></option>
+<?php endforeach; ?>
+						</select>
+					</td>
+			  		<td class="stock-stocklevel"><input type="text" class="form-control" name="stock[product.stock.stocklevel][]" disabled="disabled" /></td>
+					<td class="stock-databack"><input type="text" class="form-control" name="stock[product.stock.dateback][]" disabled="disabled" /></td>
+					<td class="actions"><div class="btn btn-danger fa fa-trash"></div></td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+</div>
---
+++ admin/jqadm/tests/Admin/JQAdm/Product/Stock/StandardTest.php
@@ -0,0 +1,103 @@
+<?php
+
+/**
+ * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
+ * @copyright Aimeos (aimeos.org), 2015
+ */
+
+
+namespace Aimeos\Admin\JQAdm\Product\Stock;
+
+
+class StandardTest extends \PHPUnit_Framework_TestCase
+{
+	private $context;
+	private $object;
+	private $view;
+
+
+	protected function setUp()
+	{
+		$this->view = \TestHelperJqadm::getView();
+		$this->context = \TestHelperJqadm::getContext();
+		$templatePaths = \TestHelperJqadm::getTemplatePaths();
+
+		$this->object = new \Aimeos\Admin\JQAdm\Product\Stock\Standard( $this->context, $templatePaths );
+		$this->object->setView( $this->view );
+	}
+
+
+	protected function tearDown()
+	{
+		unset( $this->object );
+	}
+
+
+	public function testCreate()
+	{
+		$manager = \Aimeos\MShop\Factory::createManager( $this->context, 'product' );
+
+		$this->view->item = $manager->createItem();
+		$result = $this->object->create();
+
+		$this->assertNull( $this->view->get( 'errors' ) );
+		$this->assertStringStartsWith( '<div class="product-item-stock', $result );
+	}
+
+
+	public function testCopy()
+	{
+		$manager = \Aimeos\MShop\Factory::createManager( $this->context, 'product' );
+
+		$this->view->item = $manager->findItem( 'CNC' );
+		$result = $this->object->copy();
+
+		$this->assertNull( $this->view->get( 'errors' ) );
+		$this->assertContains( 'value="1200"', $result );
+	}
+
+
+	public function testGet()
+	{
+		$manager = \Aimeos\MShop\Factory::createManager( $this->context, 'product' );
+
+		$this->view->item = $manager->findItem( 'CNC' );
+		$result = $this->object->get();
+
+		$this->assertNull( $this->view->get( 'errors' ) );
+		$this->assertContains( 'value="1200"', $result );
+	}
+
+
+	public function testSave()
+	{
+		$manager = \Aimeos\MShop\Factory::createManager( $this->context, 'product' );
+		$whManager = \Aimeos\MShop\Factory::createManager( $this->context, 'product/stock/warehouse' );
+
+		$item = $manager->findItem( 'CNC' );
+		$item->setCode( 'CNC_copy' );
+		$item->setId( null );
+
+		$manager->saveItem( $item );
+
+
+		$param = array(
+			'stock' => array(
+				'product.stock.warehouseid' => array( $whManager->findItem( 'default' )->getId() ),
+				'product.stock.dateback' => array( '2000-01-01 00:00:00' ),
+				'product.stock.stocklevel' => array( '-1' ),
+			),
+		);
+
+		$helper = new \Aimeos\MW\View\Helper\Param\Standard( $this->view, $param );
+		$this->view->addHelper( 'param', $helper );
+		$this->view->item = $item;
+
+		$result = $this->object->save();
+
+		$manager->deleteItem( $item->getId() );
+
+		$this->assertNull( $this->view->get( 'errors' ) );
+		$this->assertNull( $result );
+	}
+}
