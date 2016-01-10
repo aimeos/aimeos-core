a81ec445ebe95c423e9fb6ce2f398d6f8a48f4c2
Aimeos <aimeos@aimeos.org>
2016-01-10 17:11:38 +0100

    Remove unnecessary code

--- lib/mwlib/tests/MW/View/Helper/Content/StandardTest.php
+++ lib/mwlib/tests/MW/View/Helper/Content/StandardTest.php
@@ -13,12 +13,6 @@ class StandardTest extends \PHPUnit_Framework_TestCase
 	private $object;
 
 
-	/**
-	 * Sets up the fixture, for example, opens a network connection.
-	 * This method is called before a test is executed.
-	 *
-	 * @access protected
-	 */
 	protected function setUp()
 	{
 		$view = new \Aimeos\MW\View\Standard();
@@ -30,33 +24,9 @@ class StandardTest extends \PHPUnit_Framework_TestCase
 	}
 
 
-	/**
-	 * Tears down the fixture, for example, closes a network connection.
-	 * This method is called after a test is executed.
-	 *
-	 * @access protected
-	 */
 	protected function tearDown()
 	{
 		unset( $this->object );
-	}
-
-
-	public function testTransformRelativeUrlFromConfig()
-	{
-		$view = new \Aimeos\MW\View\Standard();
-
-		$helper = new \Aimeos\MW\View\Helper\Encoder\Standard( $view );
-		$view->addHelper( 'encoder', $helper );
-
-		$helper = new \Aimeos\MW\View\Helper\Config\Standard( $view, \TestHelperHtml::getContext()->getConfig() );
-		$view->addHelper( 'config', $helper );
-
-		$this->object = new \Aimeos\MW\View\Helper\Content\Standard( $view );
-
-
-		$output = $this->object->transform( 'path/to/resource' );
-		$this->assertEquals( '/path/to/resource', $output );
 	}
 
 
