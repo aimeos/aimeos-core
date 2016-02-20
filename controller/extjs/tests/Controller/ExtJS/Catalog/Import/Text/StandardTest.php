6c3647026feaca031985e1747be9a8330471d8f2
Aimeos <aimeos@aimeos.org>
2016-02-20 21:08:12 +0100

    Fixed tests for PHP 7

--- controller/extjs/tests/Controller/ExtJS/Catalog/Import/Text/StandardTest.php
+++ controller/extjs/tests/Controller/ExtJS/Catalog/Import/Text/StandardTest.php
@@ -154,13 +154,20 @@ class StandardTest extends \PHPUnit_Framework_TestCase
 
 	public function testUploadFile()
 	{
+		$helper = $this->getMockBuilder( '\Aimeos\MW\View\Helper\Request\Standard' )
+			->disableOriginalConstructor()
+			->getMock();
+
+		$view = new \Aimeos\MW\View\Standard();
+		$view->addHelper( 'request', $helper );
+
 		$object = $this->getMockBuilder( '\Aimeos\Controller\ExtJS\Catalog\Import\Text\Standard' )
 			->setConstructorArgs( array( $this->context ) )
 			->setMethods( array( 'storeFile' ) )
 			->getMock();
 		$object->expects( $this->once() )->method( 'storeFile' )->will( $this->returnValue( 'file.txt' ) );
 
-		$this->context->setView( new \Aimeos\MW\View\Standard() );
+		$this->context->setView( $view );
 
 		$params = new \stdClass();
 		$params->items = 'file.txt';
