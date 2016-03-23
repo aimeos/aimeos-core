f31c0934fa5784a37c05a5891b5cc793957163a6
Aimeos <aimeos@aimeos.org>
2016-03-23 18:50:09 +0100

    Implemented product attribute managment

---
+++ admin/jqadm/tests/Admin/JQAdm/Product/Attribute/StandardTest.php
@@ -0,0 +1,119 @@
+<?php
+
+/**
+ * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
+ * @copyright Aimeos (aimeos.org), 2016
+ */
+
+
+namespace Aimeos\Admin\JQAdm\Product\Attribute;
+
+
+class StandardTest extends \PHPUnit_Framework_TestCase
+{
+	private $context;
+	private $object;
+	private $view;
+
+
+	protected function setUp()
+	{
+		$this->view = \TestHelperJqadm::getView();
+		$this->context = \TestHelperJqadm::getContext();
+		$templatePaths = \TestHelperJqadm::getTemplatePaths();
+
+		$this->object = new \Aimeos\Admin\JQAdm\Product\Attribute\Standard( $this->context, $templatePaths );
+		$this->object->setView( $this->view );
+	}
+
+
+	protected function tearDown()
+	{
+		unset( $this->object );
+	}
+
+
+	public function testCreate()
+	{
+		$manager = \Aimeos\MShop\Factory::createManager( $this->context, 'product' );
+
+		$this->view->item = $manager->createItem();
+		$result = $this->object->create();
+
+		$this->assertContains( 'Attributes', $result );
+		$this->assertNull( $this->view->get( 'errors' ) );
+	}
+
+
+	public function testCopy()
+	{
+		$manager = \Aimeos\MShop\Factory::createManager( $this->context, 'product' );
+
+		$this->view->item = $manager->findItem( 'CNC', array( 'attribute' ) );
+		$result = $this->object->copy();
+
+		$this->assertNull( $this->view->get( 'errors' ) );
+		$this->assertContains( 'xs', $result );
+	}
+
+
+	public function testDelete()
+	{
+		$result = $this->object->delete();
+
+		$this->assertNull( $this->view->get( 'errors' ) );
+		$this->assertNull( $result );
+	}
+
+
+	public function testGet()
+	{
+		$manager = \Aimeos\MShop\Factory::createManager( $this->context, 'product' );
+
+		$this->view->item = $manager->findItem( 'CNC', array( 'attribute' ) );
+		$result = $this->object->get();
+
+		$this->assertNull( $this->view->get( 'errors' ) );
+		$this->assertContains( 'xs', $result );
+	}
+
+
+	public function testSave()
+	{
+		$manager = \Aimeos\MShop\Factory::createManager( $this->context, 'product' );
+		$attrManager = \Aimeos\MShop\Factory::createManager( $this->context, 'attribute' );
+
+		$item = $manager->findItem( 'CNC' );
+		$item->setCode( 'jqadm-test-attribute' );
+		$item->setId( null );
+
+		$manager->saveItem( $item );
+
+
+		$param = array(
+			'attribute' => array(
+				'product.lists.id' => array( '' ),
+				'product.lists.refid' => array( $attrManager->findItem( 'xs', array(), 'product', 'size' )->getId() ),
+			),
+		);
+
+		$helper = new \Aimeos\MW\View\Helper\Param\Standard( $this->view, $param );
+		$this->view->addHelper( 'param', $helper );
+		$this->view->item = $item;
+
+		$result = $this->object->save();
+
+		$item = $manager->getItem( $item->getId(), array( 'attribute' ) );
+		$manager->deleteItem( $item->getId() );
+
+		$this->assertNull( $this->view->get( 'errors' ) );
+		$this->assertNull( $result );
+		$this->assertEquals( 1, count( $item->getListItems( 'attribute' ) ) );
+	}
+
+
+	public function testSearch()
+	{
+		$this->assertNull( $this->object->search() );
+	}
+}
