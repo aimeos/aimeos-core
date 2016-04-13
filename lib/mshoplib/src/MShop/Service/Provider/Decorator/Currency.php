448c348b824adff7eea2f799b2a0d7a0e5fe0406
Aimeos <aimeos@aimeos.org>
2016-04-13 14:13:27 +0200

    Implemented currency-limiting service decorator

---
+++ lib/mshoplib/src/MShop/Service/Provider/Decorator/Currency.php
@@ -0,0 +1,116 @@
+<?php
+
+/**
+ * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
+ * @copyright Aimeos (aimeos.org), 2016
+ * @package MShop
+ * @subpackage Service
+ */
+
+
+namespace Aimeos\MShop\Service\Provider\Decorator;
+
+
+/**
+ * Currency-limiting decorator for service providers.
+ *
+ * @package MShop
+ * @subpackage Service
+ */
+class Currency
+	extends \Aimeos\MShop\Service\Provider\Decorator\Base
+	implements \Aimeos\MShop\Service\Provider\Decorator\Iface
+{
+	private $beConfig = array(
+		'currency.include' => array(
+			'code' => 'currency.include',
+			'internalcode'=> 'currency.include',
+			'label'=> 'List of currencies allowed for the service item',
+			'type'=> 'string',
+			'internaltype'=> 'string',
+			'default'=> '',
+			'required'=> false,
+		),
+		'currency.exclude' => array(
+			'code' => 'currency.exclude',
+			'internalcode'=> 'currency.exclude',
+			'label'=> 'List of countries not allowed for the service item',
+			'type'=> 'string',
+			'internaltype'=> 'string',
+			'default'=> '',
+			'required'=> false,
+		),
+	);
+
+
+	/**
+	 * Checks the backend configuration attributes for validity.
+	 *
+	 * @param array $attributes Attributes added by the shop owner in the administraton interface
+	 * @return array An array with the attribute keys as key and an error message as values for all attributes that are
+	 * 	known by the provider but aren't valid
+	 */
+	public function checkConfigBE( array $attributes )
+	{
+		$error = $this->getProvider()->checkConfigBE( $attributes );
+		$error += $this->checkConfig( $this->beConfig, $attributes );
+
+		return $error;
+	}
+
+
+	/**
+	 * Returns the configuration attribute definitions of the provider to generate a list of available fields and
+	 * rules for the value of each field in the administration interface.
+	 *
+	 * @return array List of attribute definitions implementing \Aimeos\MW\Common\Critera\Attribute\Iface
+	 */
+	public function getConfigBE()
+	{
+		$list = $this->getProvider()->getConfigBE();
+
+		foreach( $this->beConfig as $key => $config ) {
+			$list[$key] = new \Aimeos\MW\Criteria\Attribute\Standard( $config );
+		}
+
+		return $list;
+	}
+
+
+
+
+	/**
+	 * Checks if the country code is allowed for the service provider.
+	 *
+	 * @param \Aimeos\MShop\Order\Item\Base\Iface $basket Basket object
+	 * @return boolean True if payment provider can be used, false if not
+	 */
+	public function isAvailable( \Aimeos\MShop\Order\Item\Base\Iface $basket )
+	{
+		$code = strtoupper( $basket->getPrice()->getCurrencyId() );
+
+		if( $this->checkCurrencyCode( $code, 'currency.include' ) === false
+			|| $this->checkCurrencyCode( $code, 'currency.exclude' ) === true
+		) {
+			return false;
+		}
+
+		return $this->getProvider()->isAvailable( $basket );
+	}
+
+
+	/**
+	 * Checks if the currency code is in the list of codes specified by the given key
+	 *
+	 * @param string $code Three letter ISO currency code in upper case
+	 * @param string $key Configuration key referring to the currency code configuration
+	 */
+	protected function checkCurrencyCode( $code, $key )
+	{
+		if( ( $str = $this->getConfigValue( array( $key ) ) ) === null ) {
+			return null;
+		}
+
+		return in_array( $code, explode( ',', str_replace( ' ', '', strtoupper( $str ) ) ) );
+	}
+}
---
+++ lib/mshoplib/tests/MShop/Service/Provider/Decorator/CurrencyTest.php
@@ -0,0 +1,171 @@
+<?php
+
+/**
+ * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
+ * @copyright Aimeos (aimeos.org), 2016
+ */
+
+
+namespace Aimeos\MShop\Service\Provider\Decorator;
+
+
+class CurrencyTest extends \PHPUnit_Framework_TestCase
+{
+	private $object;
+	private $basket;
+	private $context;
+	private $servItem;
+	private $mockProvider;
+
+
+	protected function setUp()
+	{
+		$this->context = \TestHelperMShop::getContext();
+
+		$servManager = \Aimeos\MShop\Factory::createManager( $this->context, 'service' );
+		$this->servItem = $servManager->createItem();
+
+		$this->mockProvider = $this->getMockBuilder( '\\Aimeos\\MShop\\Service\\Provider\\Decorator\\Currency' )
+			->disableOriginalConstructor()->getMock();
+
+		$this->basket = \Aimeos\MShop\Order\Manager\Factory::createManager( $this->context )
+			->getSubManager( 'base' )->createItem();
+
+		$this->object = new \Aimeos\MShop\Service\Provider\Decorator\Currency( $this->mockProvider, $this->context, $this->servItem );
+	}
+
+
+	protected function tearDown()
+	{
+		unset( $this->object, $this->basket, $this->mockProvider, $this->servItem, $this->context );
+	}
+
+
+	public function testGetConfigBE()
+	{
+		$this->mockProvider->expects( $this->once() )
+			->method( 'getConfigBE' )
+			->will( $this->returnValue( array() ) );
+
+		$result = $this->object->getConfigBE();
+
+		$this->assertArrayHasKey( 'currency.include', $result );
+		$this->assertArrayHasKey( 'currency.exclude', $result );
+	}
+
+
+	public function testCheckConfigBE()
+	{
+		$this->mockProvider->expects( $this->once() )
+			->method( 'checkConfigBE' )
+			->will( $this->returnValue( array() ) );
+
+		$attributes = array(
+			'currency.include' => ' EUR , USD ',
+			'currency.exclude' => ' EUR , USD ',
+		);
+		$result = $this->object->checkConfigBE( $attributes );
+
+		$this->assertEquals( 2, count( $result ) );
+		$this->assertInternalType( 'null', $result['currency.include'] );
+		$this->assertInternalType( 'null', $result['currency.exclude'] );
+	}
+
+
+	public function testCheckConfigBENoConfig()
+	{
+		$this->mockProvider->expects( $this->once() )
+			->method( 'checkConfigBE' )
+			->will( $this->returnValue( array() ) );
+
+		$result = $this->object->checkConfigBE( array() );
+
+		$this->assertEquals( 2, count( $result ) );
+		$this->assertInternalType( 'null', $result['currency.include'] );
+		$this->assertInternalType( 'null', $result['currency.exclude'] );
+	}
+
+
+	public function testCheckConfigBEFailure()
+	{
+		$this->mockProvider->expects( $this->once() )
+			->method( 'checkConfigBE' )
+			->will( $this->returnValue( array() ) );
+
+		$attributes = array(
+			'currency.include' => array(),
+			'currency.exclude' => 1,
+		);
+		$result = $this->object->checkConfigBE( $attributes );
+
+		$this->assertEquals( 2, count( $result ) );
+		$this->assertInternalType( 'string', $result['currency.include'] );
+		$this->assertInternalType( 'string', $result['currency.exclude'] );
+	}
+
+
+	public function testIsAvailableNoConfig()
+	{
+		$this->servItem->setConfig( array() );
+
+		$this->mockProvider->expects( $this->once() )
+			->method( 'isAvailable' )
+			->will( $this->returnValue( true ) );
+
+		$this->assertTrue( $this->object->isAvailable( $this->basket ) );
+	}
+
+
+	public function testIsAvailableNoInclude()
+	{
+		$this->servItem->setConfig( array( 'currency.include' => '' ) );
+
+		$this->mockProvider->expects( $this->never() )->method( 'isAvailable' );
+
+		$this->assertFalse( $this->object->isAvailable( $this->basket ) );
+	}
+
+
+	public function testIsAvailableNoExclude()
+	{
+		$this->servItem->setConfig( array( 'currency.exclude' => '' ) );
+
+		$this->mockProvider->expects( $this->once() )
+			->method( 'isAvailable' )
+			->will( $this->returnValue( true ) );
+
+		$this->assertTrue( $this->object->isAvailable( $this->basket ) );
+	}
+
+
+	public function testIsAvailableExclude()
+	{
+		$this->servItem->setConfig( array( 'currency.exclude' => 'EUR' ) );
+
+		$this->mockProvider->expects( $this->never() )->method( 'isAvailable' );
+
+		$this->assertFalse( $this->object->isAvailable( $this->basket ) );
+	}
+
+
+	public function testIsAvailableInclude()
+	{
+		$this->servItem->setConfig( array( 'currency.include' => 'EUR' ) );
+
+		$this->mockProvider->expects( $this->once() )
+			->method( 'isAvailable' )
+			->will( $this->returnValue( true ) );
+
+		$this->assertTrue( $this->object->isAvailable( $this->basket ) );
+	}
+
+
+	public function testIsAvailableIncludeFailure()
+	{
+		$this->servItem->setConfig( array( 'currency.include' => 'USD' ) );
+
+		$this->mockProvider->expects( $this->never() )->method( 'isAvailable' );
+
+		$this->assertFalse( $this->object->isAvailable( $this->basket ) );
+	}
+}
