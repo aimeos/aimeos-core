41a5c2b7135e1fbbc474c11b5ce372c69268320c
Aimeos <aimeos@aimeos.org>
2016-05-22 18:09:14 +0200

    Fixed indexes

--- lib/mshoplib/setup/default/schema/index-mysql.sql
+++ lib/mshoplib/setup/default/schema/index-mysql.sql
@@ -3,3 +3,5 @@
 --
 
 CREATE INDEX "idx_msindte_p_s_lt_la_ty_do_va" ON "mshop_index_text" ("prodid", "siteid", "listtype", "langid", "type", "domain", "value"(16));
+
+CREATE FULLTEXT INDEX "idx_msindte_value" ON "mshop_index_text" ("value");
--- lib/mshoplib/setup/default/schema/index-pgsql.sql
+++ lib/mshoplib/setup/default/schema/index-pgsql.sql
@@ -3,4 +3,5 @@
 --
 
 CREATE INDEX "idx_msindte_p_s_lt_la_ty_do_va" ON "mshop_index_text" ("prodid", "siteid", "listtype", "langid", "type", "domain", "value");
+
 CREATE INDEX "idx_msindte_value" ON mshop_index_text USING GIN (to_tsvector('english', value));
