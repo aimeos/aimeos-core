965ceb742f75f00cf20b7b3fd53b6bb8beb3859c
Aimeos <aimeos@aimeos.org>
2016-05-25 13:41:49 +0200

    Fixed tree factory test

--- lib/mwlib/tests/MW/Tree/FactoryTest.php
+++ lib/mwlib/tests/MW/Tree/FactoryTest.php
@@ -1,51 +1,29 @@
 <?php
 
+/**
+ * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
+ * @copyright Metaways Infosystems GmbH, 2011
+ * @copyright Aimeos (aimeos.org), 2015-2016
+ */
+
+
 namespace Aimeos\MW\Tree;
 
 
-/**
- * Test class for \Aimeos\MW\Tree\Factory.
- *
- * @copyright Metaways Infosystems GmbH, 2011
- * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
- * @copyright Aimeos (aimeos.org), 2015
- */
 class FactoryTest extends \PHPUnit_Framework_TestCase
 {
-	private $object;
-	private $dbm;
-	private $config;
-
-	/**
-	 * Sets up the fixture, for example, opens a network connection.
-	 * This method is called before a test is executed.
-	 *
-	 * @access protected
-	 */
-	protected function setUp()
-	{
-		$this->object = \Aimeos\MW\Tree\Factory::createManager('LDAP', array(), null );
-	}
-
-	/**
-	 * Tears down the fixture, for example, closes a network connection.
-	 * This method is called after a test is executed.
-	 *
-	 * @access protected
-	 */
-	protected function tearDown()
-	{
-		unset( $this->object );
-	}
-
 	public function testFactory()
 	{
-		$this->assertInstanceOf('\\Aimeos\\MW\\Tree\\Manager\\Iface', $this->object);
+		$dbm = \Aimeos\MW\DB\Factory::createManager( \TestHelperMw::getConfig() );
+
+		$this->setExpectedException('\Aimeos\MW\Tree\Exception');
+		\Aimeos\MW\Tree\Factory::createManager( 'DBNestedSet', array(), $dbm );
 	}
+
 
 	public function testFactoryFail()
 	{
-		$this->setExpectedException('\\Aimeos\\MW\\Tree\\Exception');
-		$this->object = \Aimeos\MW\Tree\Factory::createManager('notDefined', array(), null );
+		$this->setExpectedException('\Aimeos\MW\Tree\Exception');
+		\Aimeos\MW\Tree\Factory::createManager( 'invalid', array(), null );
 	}
 }
