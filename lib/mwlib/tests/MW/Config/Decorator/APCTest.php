2cb2ca0332682f9480c624c2c0968c581685b93a
Aimeos <aimeos@aimeos.org>
2016-05-25 17:41:43 +0200

    Code cleanup

--- lib/mwlib/tests/MW/Config/Decorator/APCTest.php
+++ lib/mwlib/tests/MW/Config/Decorator/APCTest.php
@@ -1,26 +1,20 @@
 <?php
 
+/**
+ * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
+ * @copyright Metaways Infosystems GmbH, 2011
+ * @copyright Aimeos (aimeos.org), 2015-2016
+ */
+
+
 namespace Aimeos\MW\Config\Decorator;
 
 
-/**
- * Test class for \Aimeos\MW\Config\Decorator\APC.
- *
- * @copyright Metaways Infosystems GmbH, 2011
- * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
- * @copyright Aimeos (aimeos.org), 2015
- */
 class APCTest extends \PHPUnit_Framework_TestCase
 {
 	private $object;
 
 
-	/**
-	 * Sets up the fixture, for example, opens a network connection.
-	 * This method is called before a test is executed.
-	 *
-	 * @access protected
-	 */
 	protected function setUp()
 	{
 		if( function_exists( 'apc_store' ) === false ) {
@@ -31,21 +25,19 @@ class APCTest extends \PHPUnit_Framework_TestCase
 		$this->object = new \Aimeos\MW\Config\Decorator\APC( $conf, 'test:' );
 	}
 
-	/**
-	 * Tears down the fixture, for example, closes a network connection.
-	 * This method is called after a test is executed.
-	 *
-	 * @access protected
-	 */
+
 	protected function tearDown()
 	{
+		unset( $this->object );
 	}
+
 
 	public function testGetSet()
 	{
 		$this->object->set( 'resource/db/host', '127.0.0.1' );
 		$this->assertEquals( '127.0.0.1', $this->object->get( 'resource/db/host', '127.0.0.2' ) );
 	}
+
 
 	public function testGetDefault()
 	{
--- lib/mwlib/tests/MW/Translation/Decorator/APCTest.php
+++ lib/mwlib/tests/MW/Translation/Decorator/APCTest.php
@@ -1,26 +1,20 @@
 <?php
 
+/**
+ * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
+ * @copyright Metaways Infosystems GmbH, 2011
+ * @copyright Aimeos (aimeos.org), 2015-2016
+ */
+
+
 namespace Aimeos\MW\Translation\Decorator;
 
 
-/**
- * Test class for \Aimeos\MW\Translation\Decorator\APC.
- *
- * @copyright Metaways Infosystems GmbH, 2011
- * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
- * @copyright Aimeos (aimeos.org), 2015
- */
 class APCTest extends \PHPUnit_Framework_TestCase
 {
 	private $object;
 
 
-	/**
-	 * Sets up the fixture, for example, opens a network connection.
-	 * This method is called before a test is executed.
-	 *
-	 * @access protected
-	 */
 	protected function setUp()
 	{
 		if( function_exists( 'apc_store' ) === false ) {
@@ -31,25 +25,24 @@ class APCTest extends \PHPUnit_Framework_TestCase
 		$this->object = new \Aimeos\MW\Translation\Decorator\APC( $trans, 'i18n' );
 	}
 
-	/**
-	 * Tears down the fixture, for example, closes a network connection.
-	 * This method is called after a test is executed.
-	 *
-	 * @access protected
-	 */
+
 	protected function tearDown()
 	{
+		unset( $this->object );
 	}
+
 
 	public function testDt()
 	{
 		$this->assertEquals( 'test', $this->object->dt( 'domain', 'test' ) );
 	}
 
+
 	public function testDn()
 	{
 		$this->assertEquals( 'tests', $this->object->dn( 'domain', 'test', 'tests', 2 ) );
 	}
+
 
 	public function testGetLocale()
 	{
