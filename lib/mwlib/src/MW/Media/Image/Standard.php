390380a8e09a1dafb972bfe9fcf3febd2bbf7b3b
Peter Bolch <peter@bolch-steinegger.de>
2016-07-08 16:01:22 +0200

    Preserve png alpha channel (#75)
    Those two lines preserve the alpha channel for png's. For now opaque regions are black after saving a file.

--- lib/mwlib/src/MW/Media/Image/Standard.php
+++ lib/mwlib/src/MW/Media/Image/Standard.php
@@ -105,6 +105,14 @@ class Standard
 					$quality = (int) $this->options['image']['png']['quality'];
 				}
 
+				if( @imagealphablending($this->image, false) === false ){
+					throw new \Aimeos\MW\Media\Exception( sprintf( 'There was an error during the png processing in gdlib (alphablending)') );
+				}
+
+				if( @imagesavealpha($this->image, true) === false ){
+					throw new \Aimeos\MW\Media\Exception( sprintf( 'There was an error during the png processing in gdlib(imagesavealpha)') );
+				}
+				
 				if( @imagepng( $this->image, $filename, $quality ) === false ) {
 					throw new \Aimeos\MW\Media\Exception( sprintf( 'Unable to save image to file "%1$s"', $filename ) );
 				}
