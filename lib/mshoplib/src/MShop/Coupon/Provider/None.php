929429de4f748319a7a2e376170c2a41bb82b216
Aimeos <aimeos@aimeos.org>
2016-08-10 15:32:12 +0200

    Added "None" coupon provider that does nothing

---
+++ lib/mshoplib/src/MShop/Coupon/Provider/None.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * @copyright Aimeos (aimeos.org), 2016
+ * @package MShop
+ * @subpackage Coupon
+ */
+
+
+namespace Aimeos\MShop\Coupon\Provider;
+
+
+/**
+ * No coupon model
+ *
+ * @package MShop
+ * @subpackage Coupon
+ */
+class FreeShipping
+	extends \Aimeos\MShop\Coupon\Provider\Factory\Base
+	implements \Aimeos\MShop\Coupon\Provider\Factory\Iface
+{
+	/**
+	 * Adds the result of a coupon to the order base instance.
+	 *
+	 * @param \Aimeos\MShop\Order\Item\Base\Iface $base Basic order of the customer
+	 */
+	public function addCoupon( \Aimeos\MShop\Order\Item\Base\Iface $base )
+	{
+		if( $this->getObject()->isAvailable( $base ) === false ) {
+			return;
+		}
+
+		$base->addCoupon( $this->getCode(), array() );
+	}
+}
---
+++ lib/mshoplib/tests/MShop/Coupon/Provider/NoneTest.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
+ * @copyright Aimeos (aimeos.org), 2016
+ */
+
+
+namespace Aimeos\MShop\Coupon\Provider;
+
+
+class NoneTest extends \PHPUnit_Framework_TestCase
+{
+	private $object;
+	private $orderBase;
+
+
+	protected function setUp()
+	{
+		$context = \TestHelperMShop::getContext();
+		$priceManager = \Aimeos\MShop\Price\Manager\Factory::createManager( $context );
+		$item = \Aimeos\MShop\Coupon\Manager\Factory::createManager( $context )->createItem();
+
+		// Don't create order base item by createItem() as this would already register the plugins
+		$this->orderBase = new \Aimeos\MShop\Order\Item\Base\Standard( $priceManager->createItem(), $context->getLocale() );
+		$this->object = new \Aimeos\MShop\Coupon\Provider\Example( $context, $item, '1234' );
+	}
+
+
+	protected function tearDown()
+	{
+		unset( $this->object );
+		unset( $this->orderBase );
+	}
+
+
+	public function testAddCoupon()
+	{
+		$this->object->addCoupon( $this->orderBase );
+	}
+}
