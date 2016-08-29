ef8625ab903f2fc0fc0268af912df4dcdcf3ce5d
Aimeos <aimeos@aimeos.org>
2016-08-29 13:44:37 +0200

    Fixed tag type domain for product lists

--- lib/mshoplib/setup/default/data/type.php
+++ lib/mshoplib/setup/default/data/type.php
@@ -104,7 +104,7 @@ return array(
 		array( 'domain' => 'service', 'code' => 'default', 'label' => 'Standard', 'status' => 1 ),
 		array( 'domain' => 'supplier', 'code' => 'default', 'label' => 'Standard', 'status' => 1 ),
 		array( 'domain' => 'text', 'code' => 'default', 'label' => 'Standard', 'status' => 1 ),
-		array( 'domain' => 'product/tag', 'code' => 'default', 'label' => 'Standard', 'status' => 1 ),
+		array( 'domain' => 'tag', 'code' => 'default', 'label' => 'Standard', 'status' => 1 ),
 		array( 'domain' => 'product', 'code' => 'suggestion', 'label' => 'Suggestion', 'status' => 1 ),
 		array( 'domain' => 'product', 'code' => 'bought-together', 'label' => 'Bought together', 'status' => 1 ),
 	),
