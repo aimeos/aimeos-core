dedff54fbb586b05a22b558b357c2fee96e55ece
Aimeos <aimeos@aimeos.org>
2017-01-09 19:11:31 +0100

    Implemented ImageMagick media class

---
+++ lib/mwlib/src/MW/Media/Image/Imagick.php
@@ -0,0 +1,122 @@
+<?php
+
+/**
+ * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
+ * @copyright Aimeos (aimeos.org), 2017
+ * @package MW
+ * @subpackage Media
+ */
+
+
+namespace Aimeos\MW\Media\Image;
+
+
+/**
+ * Default image class using ImageMagick.
+ *
+ * @package MW
+ * @subpackage Media
+ */
+class Imagick
+	extends \Aimeos\MW\Media\Image\Base
+	implements \Aimeos\MW\Media\Image\Iface
+{
+	private $image;
+	private $options;
+
+
+	/**
+	 * Initializes the new image object.
+	 *
+	 * @param string $content File content
+	 * @param string $mimetype Mime type of the media data
+	 * @param array $options Associative list of configuration options
+	 * @throws \Aimeos\MW\Media\Exception If image couldn't be retrieved from the given file name
+	 */
+	public function __construct( $content, $mimetype, array $options )
+	{
+		parent::__construct( $mimetype );
+
+		$this->options = $options;
+
+		try
+		{
+			$this->image = new \Imagick( array() );
+			$this->image->readImageBlob( $content );
+		}
+		catch( \Exception $e )
+		{
+			throw new \Aimeos\MW\Media\Exception( $e->getMessage() );
+		}
+	}
+
+
+	/**
+	 * Stores the media data at the given file name.
+	 *
+	 * @param string|null $filename File name to save the data into or null to return the data
+	 * @param string|null $mimetype Mime type to save the content as or null to leave the mime type unchanged
+	 * @return string|null File content if file name is null or null if data is saved to the given file name
+	 * @throws \Aimeos\MW\Media\Exception If image couldn't be saved to the given file name
+	 */
+	public function save( $filename = null, $mimetype = null )
+	{
+		if( $mimetype === null ) {
+			$mimetype = $this->getMimeType();
+		}
+
+		$quality = 25;
+		$mime = explode( '/', $mimetype );
+
+		if( isset( $this->options['image'][ $mime[1] ]['quality'] ) ) {
+			$quality = 100 - (int) $this->options['image'][ $mime[1] ]['quality'];
+		}
+
+		try
+		{
+			$this->image->setImageFormat( $mime[1] );
+
+			if( $filename === null ) {
+				return $this->image->getImageBlob();
+			}
+
+			$this->image->writeImage( $filename );
+		}
+		catch( \Exception $e )
+		{
+			throw new \Aimeos\MW\Media\Exception( $e->getMessage() );
+		}
+	}
+
+
+	/**
+	 * Scales the image to the given width and height.
+	 *
+	 * @param integer $width New width of the image
+	 * @param integer $height New height of the image
+	 * @param boolean $fit True to keep the width/height ratio of the image
+	 * @return \Aimeos\MW\Media\Iface Self object for method chaining
+	 */
+	public function scale( $width, $height, $fit = true )
+	{
+		if( $fit === true )
+		{
+			$w = $this->image->getImageWidth();
+			$h = $this->image->getImageHeight();
+
+			list( $width, $height ) = $this->getSizeFitted( $w, $h, $width, $height );
+
+			if( $w <= $width && $h <= $height ) {
+				return $this;
+			}
+		}
+
+		try {
+			$this->image->resizeImage( $width, $height, \Imagick::FILTER_POINT, 1 );
+		} catch( \Exception $e ) {
+			throw new \Aimeos\MW\Media\Exception( $e->getMessage() );
+		}
+
+		return $this;
+	}
+}
---
+++ lib/mwlib/tests/MW/Media/Image/ImagickTest.php
@@ -0,0 +1,175 @@
+<?php
+
+/**
+ * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
+ * @copyright Aimeos (aimeos.org), 2017
+ */
+
+
+namespace Aimeos\MW\Media\Image;
+
+
+class ImagickTest extends \PHPUnit_Framework_TestCase
+{
+	public function testConstructGif()
+	{
+		$ds = DIRECTORY_SEPARATOR;
+		$content = file_get_contents( dirname( __DIR__ ) . $ds . '_testfiles' . $ds . 'image.gif' );
+
+		$media = new \Aimeos\MW\Media\Image\Imagick( $content, 'image/gif', array() );
+
+		$this->assertEquals( 'image/gif', $media->getMimetype() );
+	}
+
+
+	public function testConstructJpeg()
+	{
+		$ds = DIRECTORY_SEPARATOR;
+		$content = file_get_contents( dirname( __DIR__ ) . $ds . '_testfiles' . $ds . 'image.jpg' );
+
+		$media = new \Aimeos\MW\Media\Image\Imagick( $content, 'image/jpeg', array() );
+
+		$this->assertEquals( 'image/jpeg', $media->getMimetype() );
+	}
+
+
+	public function testConstructPng()
+	{
+		$ds = DIRECTORY_SEPARATOR;
+		$content = file_get_contents( dirname( __DIR__ ) . $ds . '_testfiles' . $ds . 'image.png' );
+
+		$media = new \Aimeos\MW\Media\Image\Imagick( $content, 'image/png', array() );
+
+		$this->assertEquals( 'image/png', $media->getMimetype() );
+	}
+
+
+	public function testConstructImageException()
+	{
+		$ds = DIRECTORY_SEPARATOR;
+		$content = file_get_contents( dirname( __DIR__ ) . $ds . '_testfiles' . $ds . 'application.txt' );
+
+		$this->setExpectedException( '\\Aimeos\\MW\\Media\\Exception' );
+		new \Aimeos\MW\Media\Image\Imagick( $content, 'text/plain', array() );
+	}
+
+
+	public function testSaveGif()
+	{
+		$ds = DIRECTORY_SEPARATOR;
+		$content = file_get_contents( dirname( __DIR__ ) . $ds . '_testfiles' . $ds . 'image.png' );
+		$dest = dirname( dirname( dirname( __DIR__ ) ) ) . $ds . 'tmp' . $ds . 'media.gif';
+
+		$media = new \Aimeos\MW\Media\Image\Imagick( $content, 'image/png', array() );
+		$media->save( $dest, 'image/gif' );
+
+		$this->assertEquals( true, file_exists( $dest ) );
+		unlink( $dest );
+	}
+
+
+	public function testSaveGifInvalidDest()
+	{
+		$ds = DIRECTORY_SEPARATOR;
+		$content = file_get_contents( dirname( __DIR__ ) . $ds . '_testfiles' . $ds . 'image.png' );
+		$dest = __DIR__ . $ds . 'notexisting' . $ds . 'media.gif';
+
+		$media = new \Aimeos\MW\Media\Image\Imagick( $content, 'image/png', array() );
+
+		$this->setExpectedException( '\\Aimeos\\MW\\Media\\Exception' );
+		$media->save( $dest, 'image/gif' );
+	}
+
+
+	public function testSaveJpeg()
+	{
+		$ds = DIRECTORY_SEPARATOR;
+		$content = file_get_contents( dirname( __DIR__ ) . $ds . '_testfiles' . $ds . 'image.png' );
+		$dest = dirname( dirname( dirname( __DIR__ ) ) ) . $ds . 'tmp' . $ds . 'media.jpg';
+
+		$media = new \Aimeos\MW\Media\Image\Imagick( $content, 'image/gif', array() );
+		$media->save( $dest, 'image/jpeg' );
+
+		$this->assertEquals( true, file_exists( $dest ) );
+		unlink( $dest );
+	}
+
+
+	public function testSaveJpegInvalidDest()
+	{
+		$ds = DIRECTORY_SEPARATOR;
+		$content = file_get_contents( dirname( __DIR__ ) . $ds . '_testfiles' . $ds . 'image.png' );
+		$dest = __DIR__ . $ds . 'notexisting' . $ds . 'media.jpg';
+
+		$media = new \Aimeos\MW\Media\Image\Imagick( $content, 'image/png', array() );
+
+		$this->setExpectedException( '\\Aimeos\\MW\\Media\\Exception' );
+		$media->save( $dest, 'image/jpeg' );
+	}
+
+
+	public function testSavePng()
+	{
+		$ds = DIRECTORY_SEPARATOR;
+		$content = file_get_contents( dirname( __DIR__ ) . $ds . '_testfiles' . $ds . 'image.gif' );
+		$dest = dirname( dirname( dirname( __DIR__ ) ) ) . $ds . 'tmp' . $ds . 'media.png';
+
+		$media = new \Aimeos\MW\Media\Image\Imagick( $content, 'image/gif', array() );
+		$media->save( $dest, 'image/png' );
+
+		$this->assertEquals( true, file_exists( $dest ) );
+		unlink( $dest );
+	}
+
+
+	public function testSavePngInvalidDest()
+	{
+		$ds = DIRECTORY_SEPARATOR;
+		$content = file_get_contents( dirname( __DIR__ ) . $ds . '_testfiles' . $ds . 'image.gif' );
+		$dest = __DIR__ . $ds . 'notexisting' . $ds . 'media.png';
+
+		$media = new \Aimeos\MW\Media\Image\Imagick( $content, 'image/gif', array() );
+
+		$this->setExpectedException( '\\Aimeos\\MW\\Media\\Exception' );
+		$media->save( $dest, 'image/png' );
+	}
+
+
+	public function testScale()
+	{
+		$ds = DIRECTORY_SEPARATOR;
+		$content = file_get_contents( dirname( __DIR__ ) . $ds . '_testfiles' . $ds . 'image.png' );
+
+		$media = new \Aimeos\MW\Media\Image\Imagick( $content, 'image/png', array() );
+		$info = getimagesizefromstring( $media->scale( 100, 100, false )->save( null, 'image/png' ) );
+
+		$this->assertEquals( 100, $info[0] );
+		$this->assertEquals( 100, $info[1] );
+	}
+
+
+	public function testScaleFit()
+	{
+		$ds = DIRECTORY_SEPARATOR;
+		$content = file_get_contents( dirname( __DIR__ ) . $ds . '_testfiles' . $ds . 'image.png' );
+
+		$media = new \Aimeos\MW\Media\Image\Imagick( $content, 'image/png', array() );
+		$info = getimagesizefromstring( $media->scale( 5, 100 )->save( null, 'image/png' ) );
+
+		$this->assertEquals( 5, $info[0] );
+		$this->assertEquals( 5, $info[1] );
+	}
+
+
+	public function testScaleFitInside()
+	{
+		$ds = DIRECTORY_SEPARATOR;
+		$content = file_get_contents( dirname( __DIR__ ) . $ds . '_testfiles' . $ds . 'image.png' );
+
+		$media = new \Aimeos\MW\Media\Image\Imagick( $content, 'image/png', array() );
+		$info = getimagesizefromstring( $media->scale( 100, 100 )->save( null, 'image/png' ) );
+
+		$this->assertEquals( 10, $info[0] );
+		$this->assertEquals( 10, $info[1] );
+	}
+}
