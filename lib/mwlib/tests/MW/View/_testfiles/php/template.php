a5fbd3f5280dca8e8b4ab3917e8dba1229e9d802
Aimeos <aimeos@aimeos.org>
2017-01-13 02:25:56 +0100

    Satisfy coding style checks

--- lib/mwlib/tests/MW/View/_testfiles/php/template.php
+++ lib/mwlib/tests/MW/View/_testfiles/php/template.php
@@ -1,5 +1,4 @@
 Number of files:
 <?php
-$quantity = ( isset( $this->quantity ) ? $this->quantity : 0 );
-echo $quantity . ' ' . $this->translate( 'test', 'File', 'Files', $quantity );
-?>
+	$quantity = ( isset( $this->quantity ) ? $this->quantity : 0 );
+	echo $quantity . ' ' . $this->translate( 'test', 'File', 'Files', $quantity );
