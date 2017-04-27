17199ebccb9b3f78c244cf1a71f073e5c4729f79
Aimeos <aimeos@aimeos.org>
2017-04-27 19:22:37 +0200

    Revert "Fixed interval calculation in Time decorator"
    This reverts commit 87ee0984da05210324f785b0139a547b607c74a8.

--- lib/mshoplib/src/MShop/Service/Provider/Decorator/Time.php
+++ lib/mshoplib/src/MShop/Service/Provider/Decorator/Time.php
@@ -110,13 +110,15 @@ class Time
 		$list = [];
 		$feconfig = $this->feConfig;
 
+		$minute = date( 'i' );
+		$minute += 15 - $minute % 15;
+
 		try
 		{
-			$minute = date( 'i' );
 			$service = $basket->getService( \Aimeos\MShop\Order\Item\Base\Service\Base::TYPE_DELIVERY );
 
 			if( ( $value = $service->getAttribute( 'time', 'delivery' ) ) == '' ) {
-				$feconfig['time.hourminute']['default'] = date( 'H:i', time() + ($minute + 15 - ($minute % 15)) * 60 );
+				$feconfig['time.hourminute']['default'] = date( 'H' ) . ':' . $minute;
 			} else {
 				$feconfig['time.hourminute']['default'] = $value;
 			}
