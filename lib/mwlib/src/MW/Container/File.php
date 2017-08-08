8a1aed0ed3267c4884f4f5379867285f217444a8
Aimeos <aimeos@aimeos.org>
2017-08-08 15:46:10 +0200

    Implemented new file container for single files

---
+++ lib/mwlib/src/MW/Container/File.php
@@ -0,0 +1,157 @@
+<?php
+
+/**
+ * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
+ * @copyright Aimeos (aimeos.org), 2017
+ * @package MW
+ * @subpackage Container
+ */
+
+
+namespace Aimeos\MW\Container;
+
+
+/**
+ * Implementation of a single file container
+ *
+ * @package MW
+ * @subpackage Container
+ */
+class File
+	extends \Aimeos\MW\Container\Base
+	implements \Aimeos\MW\Container\Iface
+{
+	private $content;
+	private $classname;
+	private $pointer = true;
+
+
+	/**
+	 * Opens an existing file or creates a new one.
+	 *
+	 * @param string $resourcepath Path to the resource like a file
+	 * @param string $format Format of the content objects inside the container
+	 * @param array $options Associative list of key/value pairs for configuration
+	 */
+	public function __construct( $resourcepath, $format, array $options = [] )
+	{
+		$this->classname = '\\Aimeos\\MW\\Container\\Content\\' . $format;
+
+		if( class_exists( $this->classname ) === false ) {
+			throw new \Aimeos\MW\Container\Exception( sprintf( 'Unknown format "%1$s"', $format ) );
+		}
+
+		parent::__construct( $resourcepath, $options );
+	}
+
+
+	/**
+	 * Creates a new content object.
+	 *
+	 * @param string $name Name of the content
+	 * @return \Aimeos\MW\Container\Content\Iface New content object
+	 */
+	public function create( $name )
+	{
+		if( $this->content ) {
+			throw new \Aimeos\MW\Container\Exception( sprint( 'Only one content object is possible within a file container' ) );
+		}
+
+		return new $this->classname( $this->getName(), $name, $this->getOptions() );
+	}
+
+
+	/**
+	 * Adds a content object to the container.
+	 *
+	 * @param \Aimeos\MW\Container\Content\Iface $content Content object
+	 */
+	public function add( \Aimeos\MW\Container\Content\Iface $content )
+	{
+		if( $this->content ) {
+			throw new \Aimeos\MW\Container\Exception( sprint( 'Only one content object is possible within a file container' ) );
+		}
+
+		$this->content = $content;
+	}
+
+
+	/**
+	 * Returns the element specified by its name.
+	 *
+	 * @param string $name Name of the content object that should be returned
+	 * @return \Aimeos\MW\Container\Content\Iface Content object
+	 */
+	public function get( $name )
+	{
+		if( $this->content == null ) {
+			$this->content = new $this->classname( $this->getName(), $name, $this->getOptions() );
+		}
+
+		return $this->content;
+	}
+
+
+	/**
+	 * Cleans up and saves the container.
+	 */
+	public function close()
+	{
+		$this->content->close();
+	}
+
+
+	/**
+	 * Returns the current element.
+	 *
+	 * @return \Aimeos\MW\Container\Content\Iface Current content object
+	 */
+	public function current()
+	{
+		if( $this->content == null ) {
+			$this->content = new $this->classname( $this->getName(), basename( $this->getName() ), $this->getOptions() );
+		}
+
+		return $this->content;
+	}
+
+
+	/**
+	 * Returns the key of the current element.
+	 *
+	 * @return string Position within the directory
+	 */
+	public function key()
+	{
+		return $this->getName();
+	}
+
+
+	/**
+	 * Moves forward to next element.
+	 */
+	public function next()
+	{
+		$this->pointer = false;
+	}
+
+
+	/**
+	 * Rewinds the file pointer to the beginning.
+	 */
+	public function rewind()
+	{
+		$this->pointer = true;
+	}
+
+
+	/**
+	 * Checks if the current position is valid.
+	 *
+	 * @return boolean True on success or false on failure
+	 */
+	public function valid()
+	{
+		return $this->pointer;
+	}
+}
---
+++ lib/mwlib/tests/MW/Container/FileTest.php
@@ -0,0 +1,92 @@
+<?php
+
+/**
+ * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
+ * @copyright Aimeos (aimeos.org), 2017
+ */
+
+namespace Aimeos\MW\Container;
+
+
+class FileTest extends \PHPUnit\Framework\TestCase
+{
+	public function testNewFile()
+	{
+		$filepath = 'tmp' . DIRECTORY_SEPARATOR . 'testdir' . DIRECTORY_SEPARATOR . 'testfile.csv';
+
+		$file = new \Aimeos\MW\Container\File( $filepath, 'CSV' );
+		$file->add( $file->create( 'test' ) );
+		$file->close();
+
+		$filepath = $file->getName();
+
+		$check = file_exists( $filepath );
+		unlink( $filepath );
+
+		$this->assertTrue( $check );
+		$this->assertEquals( $filepath, $file->getName() );
+		$this->assertFalse( file_exists( $filepath ) );
+	}
+
+
+	public function testAdd()
+	{
+		$ds = DIRECTORY_SEPARATOR;
+		$file = new \Aimeos\MW\Container\File( 'tmp' . $ds . 'testdir' . $ds . 'testfile.csv', 'CSV' );
+
+		$content = $file->create( 'test' );
+		$content->add( array( 'test', 'file', 'data' ) );
+
+		$file->add( $content );
+		$file->close();
+
+		$filepath = $file->getName();
+
+		$actual = file_get_contents( $filepath );
+		$expected = '"test","file","data"' . "\n";
+
+		unlink( $filepath );
+
+		$this->assertEquals( $expected, $actual );
+		$this->assertFalse( file_exists( $filepath ) );
+	}
+
+
+	public function testGet()
+	{
+		$ds = DIRECTORY_SEPARATOR;
+		$file = new \Aimeos\MW\Container\File( __DIR__ . $ds . '_testdir' . $ds . 'testfile.csv', 'CSV' );
+
+		$this->assertInstanceOf( '\\Aimeos\\MW\\Container\\Content\\Iface', $file->get( 'testfile.csv' ) );
+	}
+
+
+	public function testIterator()
+	{
+		$ds = DIRECTORY_SEPARATOR;
+		$file = new \Aimeos\MW\Container\File( __DIR__ . $ds . '_testdir' . $ds . 'testfile.csv', 'CSV' );
+
+		$expected = array(
+			'testfile.csv' => 1,
+		);
+
+		$actual = [];
+		foreach( $file as $entry )
+		{
+			$rows = [];
+			foreach( $entry as $row ) {
+				$rows[] = $row;
+			}
+
+			// test if rewind works
+			$rows = [];
+			foreach( $entry as $row ) {
+				$rows[] = $row;
+			}
+
+			$actual[ $entry->getName() ] = count( $rows );
+		}
+
+		$this->assertEquals( $expected, $actual );
+	}
+}
