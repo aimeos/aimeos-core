5b9dd4beb398e29693b2e05b5e60769c505911af
Aimeos <aimeos@aimeos.org>
2017-08-11 18:30:11 +0200

    Added supplier products to unittest data

--- lib/mshoplib/setup/unittest/data/supplier-list.php
+++ lib/mshoplib/setup/unittest/data/supplier-list.php
@@ -7,10 +7,14 @@
 
 return array (
 	'supplier/lists/type' => array (
+		'product/default' => array( 'domain' => 'product', 'code' => 'default', 'label' => 'Standard', 'status' => 1 ),
 		'text/default' => array( 'domain' => 'text', 'code' => 'default', 'label' => 'Standard', 'status' => 1 ),
 	),
 
 	'supplier/lists' => array (
+		array( 'parentid' => 'supplier/unitCode001', 'typeid' => 'product/default', 'domain' => 'product', 'refid' => 'product/CNC', 'start' => '2010-01-01 00:00:00', 'end' => '2100-01-01 00:00:00', 'config' => [], 'pos' => 1, 'status' => 1 ),
+		array( 'parentid' => 'supplier/unitCode001', 'typeid' => 'product/default', 'domain' => 'product', 'refid' => 'product/CNE', 'start' => '2010-01-01 00:00:00', 'end' => null, 'config' => [], 'pos' => 2, 'status' => 1 ),
+
 		array( 'parentid' => 'supplier/unitCode001', 'typeid' => 'text/default', 'domain' => 'text', 'refid' => 'text/supplier/description', 'start' => '2010-01-01 00:00:00', 'end' => '2100-01-01 00:00:00', 'config' => [], 'pos' => 1, 'status' => 1 ),
 		array( 'parentid' => 'supplier/unitCode001', 'typeid' => 'text/default', 'domain' => 'text', 'refid' => 'text/customer/notify', 'start' => '2010-01-01 00:00:00', 'end' => '2022-01-01 00:00:00', 'config' => [], 'pos' => 2, 'status' => 1 ),
 		array( 'parentid' => 'supplier/unitCode001', 'typeid' => 'text/default', 'domain' => 'text', 'refid' => 'text/customer/newsletter', 'start' => '2010-01-01 00:00:00', 'end' => '2022-01-01 00:00:00', 'config' => [], 'pos' => 3, 'status' => 1 ),
