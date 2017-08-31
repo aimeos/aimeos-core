52c730cdd4fcaed3cdc51fdefac1e8995c27d57e
Aimeos <aimeos@aimeos.org>
2017-08-31 12:02:23 +0200

    Implements supplier limiting decorator for coupons

---
+++ lib/mshoplib/src/MShop/Coupon/Provider/Decorator/Supplier.php
@@ -0,0 +1,83 @@
+<?php
+
+/**
+ * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
+ * @copyright Aimeos (aimeos.org), 2017
+ * @package MShop
+ * @subpackage Coupon
+ */
+
+
+namespace Aimeos\MShop\Coupon\Provider\Decorator;
+
+
+/**
+ * Decorator for restricting coupons to suppliers
+ *
+ * @package MShop
+ * @subpackage Coupon
+ */
+class Supplier
+	extends \Aimeos\MShop\Coupon\Provider\Decorator\Base
+	implements \Aimeos\MShop\Coupon\Provider\Decorator\Iface
+{
+	private $beConfig = array(
+		'supplier.code' => array(
+			'code' => 'supplier.code',
+			'internalcode' => 'supplier.code',
+			'label' => 'Required code of the supplier',
+			'type' => 'string',
+			'internaltype' => 'string',
+			'default' => '',
+			'required' => true,
+		),
+	);
+
+
+	/**
+	 * Checks the backend configuration attributes for validity.
+	 *
+	 * @param array $attributes Attributes added by the shop owner in the administraton interface
+	 * @return array An array with the attribute keys as key and an error message as values for all attributes that are
+	 * 	known by the provider but aren't valid
+	 */
+	public function checkConfigBE( array $attributes )
+	{
+		return $this->checkConfig( $this->beConfig, $attributes );
+	}
+
+
+	/**
+	 * Returns the configuration attribute definitions of the provider to generate a list of available fields and
+	 * rules for the value of each field in the administration interface.
+	 *
+	 * @return array List of attribute definitions implementing \Aimeos\MW\Common\Critera\Attribute\Iface
+	 */
+	public function getConfigBE()
+	{
+		return $this->getConfigItems( $this->beConfig );
+	}
+
+
+	/**
+	 * Checks for requirements.
+	 *
+	 * @param \Aimeos\MShop\Order\Item\Base\Iface $base Basic order of the customer
+	 * @return boolean True if the requirements are met, false if not
+	 */
+	public function isAvailable( \Aimeos\MShop\Order\Item\Base\Iface $base )
+	{
+		$services = $base->getServices();
+
+		if( isset( $services[\Aimeos\MShop\Order\Item\Base\Service\Base::TYPE_DELIVERY] ) )
+		{
+			$service = $services[\Aimeos\MShop\Order\Item\Base\Service\Base::TYPE_DELIVERY];
+
+			if( $service->getAttribute( 'supplier.code', 'delivery' ) === $this->getConfigValue( 'supplier.code' ) ) {
+				return parent::isAvailable( $base );
+			}
+		}
+
+		return false;
+	}
+}
---
+++ lib/mshoplib/tests/MShop/Coupon/Provider/Decorator/SupplierTest.php
@@ -0,0 +1,105 @@
+<?php
+
+/**
+ * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
+ * @copyright Aimeos (aimeos.org), 2017
+ */
+
+
+namespace Aimeos\MShop\Coupon\Provider\Decorator;
+
+
+class SupplierTest extends \PHPUnit\Framework\TestCase
+{
+	private $object;
+	private $context;
+	private $orderBase;
+	private $couponItem;
+
+
+	protected function setUp()
+	{
+		$orderProducts = [];
+		$this->context = \TestHelperMShop::getContext();
+		$this->couponItem = \Aimeos\MShop\Coupon\Manager\Factory::createManager( $this->context )->createItem();
+
+		$provider = new \Aimeos\MShop\Coupon\Provider\Example( $this->context, $this->couponItem, 'abcd' );
+		$this->object = new \Aimeos\MShop\Coupon\Provider\Decorator\Supplier( $provider, $this->context, $this->couponItem, 'abcd');
+		$this->object->setObject( $this->object );
+
+		$priceManager = \Aimeos\MShop\Factory::createManager( $this->context, 'price' );
+		$serviceManager = \Aimeos\MShop\Factory::createManager( $this->context, 'service' );
+		$service = $serviceManager->findItem( 'unitcode' );
+
+		$orderServiceAttrManager = \Aimeos\MShop\Factory::createManager( $this->context, 'order/base/service/attribute' );
+		$orderServiceAttr = $orderServiceAttrManager->createItem();
+		$orderServiceAttr->setCode( 'supplier.code' );
+		$orderServiceAttr->setType( 'delivery' );
+		$orderServiceAttr->setValue( 'berlin' );
+
+		$orderServiceManager = \Aimeos\MShop\Factory::createManager( $this->context, 'order/base/service' );
+		$orderService = $orderServiceManager->createItem();
+		$orderService->copyFrom( $service );
+		$orderService->setAttributes( [$orderServiceAttr] );
+
+		$this->orderBase = new \Aimeos\MShop\Order\Item\Base\Standard( $priceManager->createItem(), $this->context->getLocale() );
+		$this->orderBase->setService( $orderService, \Aimeos\MShop\Order\Item\Base\Service\Base::TYPE_DELIVERY );
+	}
+
+
+	protected function tearDown()
+	{
+		unset( $this->object );
+		unset( $this->orderBase );
+		unset( $this->couponItem );
+	}
+
+
+	public function testGetConfigBE()
+	{
+		$result = $this->object->getConfigBE();
+
+		$this->assertArrayHasKey( 'supplier.code', $result );
+	}
+
+
+	public function testCheckConfigBE()
+	{
+		$attributes = ['supplier.code' => 'test'];
+		$result = $this->object->checkConfigBE( $attributes );
+
+		$this->assertEquals( 1, count( $result ) );
+		$this->assertInternalType( 'null', $result['supplier.code'] );
+	}
+
+
+	public function testCheckConfigBEFailure()
+	{
+		$result = $this->object->checkConfigBE( [] );
+
+		$this->assertEquals( 1, count( $result ) );
+		$this->assertInternalType( 'string', $result['supplier.code'] );
+	}
+
+
+	public function testIsAvailable()
+	{
+		$this->couponItem->setConfig( array( 'supplier.code' => 'berlin' ) );
+
+		$this->assertTrue( $this->object->isAvailable( $this->orderBase ) );
+	}
+
+
+	public function testIsAvailableWrongSupplier()
+	{
+		$this->couponItem->setConfig( array( 'supplier.code' => 'hamburg' ) );
+
+		$this->assertFalse( $this->object->isAvailable( $this->orderBase ) );
+	}
+
+
+	public function testIsAvailableNoSupplier()
+	{
+		$this->assertFalse( $this->object->isAvailable( $this->orderBase ) );
+	}
+}
