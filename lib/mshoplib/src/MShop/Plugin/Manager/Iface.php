56f83f4d55fbdffb374ed8576754d85b94203399
Aimeos <aimeos@aimeos.org>
2017-09-09 15:29:05 +0200

    Added missing methods to plugin interface

--- lib/mshoplib/src/MShop/Plugin/Manager/Iface.php
+++ lib/mshoplib/src/MShop/Plugin/Manager/Iface.php
@@ -2,7 +2,6 @@
 
 /**
  * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
- * @copyright Metaways Infosystems GmbH, 2011
  * @copyright Aimeos (aimeos.org), 2015-2017
  * @package MShop
  * @subpackage Plugin
@@ -13,11 +12,26 @@ namespace Aimeos\MShop\Plugin\Manager;
 
 
 /**
- * Generic plugin manager interface for creating and registering plugins.
+ * Plugin manager interface
  * @package MShop
  * @subpackage Plugin
  */
-interface Iface
-	extends \Aimeos\MShop\Common\Manager\Factory\Iface
+interface Iface extends \Aimeos\MShop\Common\Manager\Factory\Iface
 {
+	/**
+	 * Returns the plugin provider which is responsible for the plugin item
+	 *
+	 * @param \Aimeos\MShop\Plugin\Item\Iface $item Plugin item object
+	 * @return \Aimeos\MShop\Plugin\Provider\Iface Returns the decoratad plugin provider object
+	 * @throws \Aimeos\MShop\Plugin\Exception If provider couldn't be found
+	 */
+	public function getProvider( \Aimeos\MShop\Plugin\Item\Iface $item );
+
+	/**
+	 * Registers plugins to the given publisher.
+	 *
+	 * @param \Aimeos\MW\Observer\Publisher\Iface $publisher Publisher object
+	 * @param string $type Unique plugin type code
+	 */
+	public function register( \Aimeos\MW\Observer\Publisher\Iface $publisher, $type );
 }
