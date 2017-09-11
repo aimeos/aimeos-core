75ac038549caf95f7816b519e034e0cf6f326bed
Aimeos <aimeos@aimeos.org>
2017-09-11 15:06:29 +0200

    Fixes exception name

--- lib/mshoplib/src/MShop/Service/Provider/Decorator/Costs.php
+++ lib/mshoplib/src/MShop/Service/Provider/Decorator/Costs.php
@@ -76,7 +76,7 @@ class Costs
 		$config = $this->getServiceItem()->getConfig();
 
 		if( !isset( $config['costs.percent'] ) ) {
-			throw new \Aimeos\MShop\Service\Provider\Exception( sprintf( 'Missing configuration "%1$s"', 'costs.percent' ) );
+			throw new \Aimeos\MShop\Service\Exception( sprintf( 'Missing configuration "%1$s"', 'costs.percent' ) );
 		}
 
 		$value = $basket->getPrice()->getValue() * $config['costs.percent'] / 100;
