77305e3bd461560a08167392f92869757a6856a7
Aimeos <aimeos@aimeos.org>
2018-05-07 13:35:30 +0200

    Minor fix for documentation headers

--- lib/mshoplib/tests/MShop/Service/Provider/Decorator/BaseTest.php
+++ lib/mshoplib/tests/MShop/Service/Provider/Decorator/BaseTest.php
@@ -1,12 +1,14 @@
 <?php
 
-namespace Aimeos\MShop\Service\Provider\Decorator;
-
-
 /**
  * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
  * @copyright Aimeos (aimeos.org), 2015-2017
  */
+
+
+namespace Aimeos\MShop\Service\Provider\Decorator;
+
+
 class BaseTest extends \PHPUnit\Framework\TestCase
 {
 	private $mock;
