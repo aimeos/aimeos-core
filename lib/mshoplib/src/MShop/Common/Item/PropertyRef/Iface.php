325f51002ba74d347ae706f95b91e38458280e0e
Aimeos <aimeos@aimeos.org>
2018-06-19 09:30:33 +0200

    Fixed interface for items handling properties

--- lib/mshoplib/src/MShop/Common/Item/PropertyRef/Iface.php
+++ lib/mshoplib/src/MShop/Common/Item/PropertyRef/Iface.php
@@ -22,19 +22,19 @@ interface Iface
 	/**
 	 * Adds a new property item or overwrite an existing one
 	 *
-	 * @param \Aimeos\MShop\Product\Item\Property\Iface $item New or existing property item
+	 * @param \Aimeos\MShop\Common\Item\Property\Iface $item New or existing property item
 	 * @return \Aimeos\MShop\Common\Item\Iface Self object for method chaining
 	 */
-	public function addPropertyItem( \Aimeos\MShop\Product\Item\Property\Iface $item );
+	public function addPropertyItem( \Aimeos\MShop\Common\Item\Property\Iface $item );
 
 	/**
 	 * Removes an existing property item
 	 *
-	 * @param \Aimeos\MShop\Product\Item\Property\Iface $item Existing property item
+	 * @param \Aimeos\MShop\Common\Item\Property\Iface $item Existing property item
 	 * @return \Aimeos\MShop\Common\Item\Iface Self object for method chaining
 	 * @throws \Aimeos\MShop\Exception If given property item isn't found
 	 */
-	public function deletePropertyItem( \Aimeos\MShop\Product\Item\Property\Iface $item );
+	public function deletePropertyItem( \Aimeos\MShop\Common\Item\Property\Iface $item );
 
 	/**
 	 * Removes a list of existing property items
@@ -57,7 +57,7 @@ interface Iface
 	 *
 	 * @param string|null $type Name of the property item type or null for all
 	 * @param boolean $active True to return only active items, false to return all
-	 * @return \Aimeos\MShop\Product\Item\Property\Iface[] Associative list of property IDs as keys and property items as values
+	 * @return \Aimeos\MShop\Common\Item\Property\Iface[] Associative list of property IDs as keys and property items as values
 	 */
 	public function getPropertyItems( $type = null, $active = true );
 }
