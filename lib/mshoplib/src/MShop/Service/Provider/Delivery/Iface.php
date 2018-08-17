910f8686931272acd9fe6103e56ed54b1819654a
Aimeos <aimeos@aimeos.org>
2018-08-17 13:24:20 +0200

    New processBatch() method for delivery service providers

--- lib/mshoplib/src/MShop/Service/Provider/Delivery/Iface.php
+++ lib/mshoplib/src/MShop/Service/Provider/Delivery/Iface.php
@@ -12,7 +12,7 @@ namespace Aimeos\MShop\Service\Provider\Delivery;
 
 
 /**
- * Interface with specific methods for delivery providers.
+ * Interface with specific methods for delivery providers
  *
  * @package MShop
  * @subpackage Service
@@ -20,10 +20,18 @@ namespace Aimeos\MShop\Service\Provider\Delivery;
 interface Iface extends \Aimeos\MShop\Service\Provider\Iface, \Aimeos\MShop\Service\Provider\Factory\Iface
 {
 	/**
-	 * Sends the order details to the ERP system for further processing.
+	 * Sends the order details to the ERP system for further processing
 	 *
 	 * @param \Aimeos\MShop\Order\Item\Iface $order Order invoice object to process
 	 * @return null
 	 */
 	public function process( \Aimeos\MShop\Order\Item\Iface $order );
+
+	/**
+	 * Sends the details of all orders to the ERP system for further processing
+	 *
+	 * @param \Aimeos\MShop\Order\Item\Iface[] $orders List of order invoice objects
+	 * @return null
+	 */
+	public function processBatch( array $orders );
 }
