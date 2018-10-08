fc6b65d51c0c3518fc9c0ce3b68fd30377adecac
Aimeos <aimeos@aimeos.org>
2018-10-08 09:54:47 +0200

    Limit text index for PostgreSQL

--- lib/mshoplib/setup/default/schema/order-pgsql.sql
+++ lib/mshoplib/setup/default/schema/order-pgsql.sql
@@ -2,6 +2,6 @@
 -- PostgreSQL specific database definitions
 --
 
-CREATE INDEX "idx_msordbaprat_si_cd_va" ON "mshop_order_base_product_attr" ("siteid", "code", "value");
+CREATE INDEX "idx_msordbaprat_si_cd_va" ON "mshop_order_base_product_attr" ("siteid", "code", left("value", 16));
 
-CREATE INDEX "idx_msordbaseat_si_cd_va" ON "mshop_order_base_service_attr" ("siteid", "code", "value");
+CREATE INDEX "idx_msordbaseat_si_cd_va" ON "mshop_order_base_service_attr" ("siteid", "code", left("value", 16));
