0a2db7006dcd3ecf6ed440c2f3f92826950445a5
Aimeos <aimeos@aimeos.org>
2018-11-12 16:18:13 +0100

    Fixed reusing unitperf customer item

--- lib/mshoplib/setup/unitperf/CustomerAddPerfData.php
+++ lib/mshoplib/setup/unitperf/CustomerAddPerfData.php
@@ -54,12 +54,12 @@ class CustomerAddPerfData extends \Aimeos\MW\Setup\Task\Base
 		$customerManager = \Aimeos\MShop\Customer\Manager\Factory::createManager( $this->additional );
 
 		try {
-			$customerItem = $customerManager->findItem( 'demo-test', ['customer/address'] );
+			$customerItem = $customerManager->findItem( 'unitperf@example.com', ['customer/address'] );
 		} catch( \Exception $e ) {
 			$customerItem = $customerManager->createItem();
 		}
 
-		$customerItem->setCode( 'demo-test' );
+		$customerItem->setCode( 'unitperf@example.com' );
 		$customerItem->setLabel( 'Test demo unitperf user' );
 		$customerItem->setPassword( sha1( microtime( true ) . getmypid() . rand() ) );
 		$customerItem->setStatus( 1 );
