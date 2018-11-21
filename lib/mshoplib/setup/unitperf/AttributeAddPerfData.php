86271fab44379c00775aadbc8005719e281eb523
Aimeos <aimeos@aimeos.org>
2018-11-21 18:52:06 +0100

    Fixed dependencies

--- lib/mshoplib/setup/unitperf/AttributeAddPerfData.php
+++ lib/mshoplib/setup/unitperf/AttributeAddPerfData.php
@@ -32,7 +32,7 @@ class AttributeAddPerfData extends \Aimeos\MW\Setup\Task\Base
 	 */
 	public function getPreDependencies()
 	{
-		return ['MShopAddTypeDataUnitperf', 'LocaleAddPerfData'];
+		return ['MShopAddTypeDataUnitperf', 'LocaleAddPerfData', 'MShopSetLocale'];
 	}
 
 
@@ -111,7 +111,6 @@ class AttributeAddPerfData extends \Aimeos\MW\Setup\Task\Base
 				$attrManager->saveItem( $item );
 			}
 		}
-
 	}
 
 
