9dd3d0ff6f3bbc49456bb7bb9cff0fb98988c5cc
Aimeos <aimeos@aimeos.org>
2019-06-20 21:29:45 +0200

    Streamlined index manager tests for MySQL and PostgreSQL

--- lib/mshoplib/tests/MShop/Index/Manager/MySQLTest.php
+++ lib/mshoplib/tests/MShop/Index/Manager/MySQLTest.php
@@ -16,33 +16,6 @@ class MySQLTest extends \PHPUnit\Framework\TestCase
 	private $editor;
 
 
-	public static function setUpBeforeClass()
-	{
-		$context = clone \TestHelperMShop::getContext();
-		$config = $context->getConfig();
-		$dbadapter = $config->get( 'resource/db-index/adapter', $config->get( 'resource/db/adapter' ) );
-
-		if( $dbadapter === 'mysql' )
-		{
-			$context->getConfig()->set( 'mshop/index/manager/text/name', 'MySQL' );
-			$manager = new \Aimeos\MShop\Index\Manager\MySQL( $context );
-			$productManager = \Aimeos\MShop\Product\Manager\Factory::create( $context );
-
-			$search = $productManager->createSearch();
-			$conditions = array(
-				$search->compare( '==', 'product.code', array( 'CNC', 'CNE' ) ),
-				$search->compare( '==', 'product.editor', $context->getEditor() )
-			);
-			$search->setConditions( $search->combine( '&&', $conditions ) );
-			$result = $productManager->searchItems( $search, array( 'attribute', 'price', 'text', 'product' ) );
-
-			foreach( $result as $item ) {
-				$manager->saveItem( $item );
-			}
-		}
-	}
-
-
 	protected function setUp()
 	{
 		$context = clone \TestHelperMShop::getContext();
@@ -66,56 +39,25 @@ class MySQLTest extends \PHPUnit\Framework\TestCase
 	}
 
 
-	public function testGetSearchAttributes()
-	{
-		$list = $this->object->getSearchAttributes();
-
-		foreach( $list as $attribute ) {
-			$this->assertInstanceOf( \Aimeos\MW\Criteria\Attribute\Iface::class, $attribute );
-		}
-	}
-
-
-	public function testSearchItemsRelevance()
+	public function testSearchItemsSub()
 	{
 		$total = 0;
-		$search = $this->object->createSearch()->setSlice( 0, 1 );
+		$search = $this->object->createSearch();
+		$search->setSlice( 0, 1 );
 
-		$func = $search->createFunction( 'index.text:relevance', array( 'de', 'Cafe' ) );
-		$conditions = array(
-			$search->compare( '>', $func, 0 ), // text relevance
+		$expr = array(
+			$search->compare( '!=', 'index.attribute.id', null ),
+			$search->compare( '!=', 'index.catalog.id', null ),
+			$search->compare( '!=', 'index.supplier.id', null ),
+			$search->compare( '>=', $search->createFunction( 'index.price:value', ['EUR'] ), 0 ),
+			$search->compare( '>=', $search->createFunction( 'index.text:name', ['de'] ), '' ),
 			$search->compare( '==', 'product.editor', $this->editor )
 		);
-		$search->setConditions( $search->combine( '&&', $conditions ) );
+
+		$search->setConditions( $search->combine( '&&', $expr ) );
 		$result = $this->object->searchItems( $search, [], $total );
 
 		$this->assertEquals( 1, count( $result ) );
 		$this->assertEquals( 2, $total );
-	}
-
-
-	public function testSearchItemsName()
-	{
-		$total = 0;
-		$search = $this->object->createSearch()->setSlice( 0, 1 );
-
-		$func = $search->createFunction( 'index.text:name', array( 'de' ) );
-		$conditions = array(
-			$search->compare( '~=', $func, 'Noir' ), // text value
-			$search->compare( '==', 'product.editor', $this->editor )
-		);
-		$search->setConditions( $search->combine( '&&', $conditions ) );
-		$sortfunc = $search->createFunction( 'sort:index.text:name', array( 'de' ) );
-		$search->setSortations( array( $search->sort( '+', $sortfunc ) ) );
-		$result = $this->object->searchItems( $search, [], $total );
-
-		$this->assertEquals( 1, count( $result ) );
-		$this->assertEquals( 2, $total );
-	}
-
-
-	public function testOptimize()
-	{
-		$this->object->optimize();
 	}
 }
