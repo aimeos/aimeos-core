ad11bfe37ccd08db6592121f26adf46ae81ae31c
Aimeos <aimeos@aimeos.org>
2019-07-30 15:48:31 +0200

    Implements delivery type decorator (partial, collective)

--- lib/mshoplib/src/MShop/Service/Provider/Decorator/Delivery.php
+++ lib/mshoplib/src/MShop/Service/Provider/Decorator/Delivery.php
@@ -2,7 +2,7 @@
 
 /**
  * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
- * @copyright Aimeos (aimeos.org), 2018
+ * @copyright Aimeos (aimeos.org), 2019
  * @package MShop
  * @subpackage Service
  */
@@ -12,38 +12,69 @@ namespace Aimeos\MShop\Service\Provider\Decorator;
 
 
 /**
- * Date decorator for service providers
+ * Delivery type decorator for service providers
  *
  * @package MShop
  * @subpackage Service
  */
-class Date
+class Delivery
 	extends \Aimeos\MShop\Service\Provider\Decorator\Base
 	implements \Aimeos\MShop\Service\Provider\Decorator\Iface
 {
 	private $beConfig = array(
-		'date.minimumdays' => array(
-			'code' => 'date.minimumdays',
-			'internalcode' => 'date.minimumdays',
-			'label' => 'Miniumn number of days to wait when selecting dates',
-			'type' => 'integer',
-			'internaltype' => 'integer',
+		'delivery.partial' => array(
+			'code' => 'delivery.partial',
+			'internalcode' => 'delivery.partial',
+			'label' => 'Choice of partitial delivery',
+			'type' => 'boolean',
+			'internaltype' => 'boolean',
+			'default' => '0',
+			'required' => false,
+		),
+		'delivery.collective' => array(
+			'code' => 'delivery.collective',
+			'internalcode' => 'delivery.collective',
+			'label' => 'Choice of collective delivery',
+			'type' => 'boolean',
+			'internaltype' => 'boolean',
 			'default' => '0',
 			'required' => false,
 		),
 	);
 
 	private $feConfig = array(
-		'date.value' => array(
-			'code' => 'date.value',
-			'internalcode' => 'value',
-			'label' => 'Delivery date',
-			'type' => 'date',
-			'internaltype' => 'date',
-			'default' => '',
+		'delivery.type' => array(
+			'code' => 'delivery.type',
+			'internalcode' => 'type',
+			'label' => 'Delivery type',
+			'type' => 'list',
+			'internaltype' => 'integer',
+			'default' => [1 => 'complete delivery'],
 			'required' => true
 		),
 	);
+
+
+	/**
+	 * Initializes a new service provider object using the given context object.
+	 *
+	 * @param \Aimeos\MShop\Service\Provider\Iface $provider Service provider or decorator
+	 * @param \Aimeos\MShop\Context\Item\Iface $context Context object with required objects
+	 * @param \Aimeos\MShop\Service\Item\Iface $serviceItem Service item with configuration for the provider
+	 */
+	public function __construct( \Aimeos\MShop\Service\Provider\Iface $provider,
+		\Aimeos\MShop\Context\Item\Iface $context, \Aimeos\MShop\Service\Item\Iface $serviceItem )
+	{
+		parent::__construct( $provider, $context, $serviceItem );
+
+		if( $this->getConfigValue( 'delivery.partial', 0 ) ) {
+			$this->feConfig['delivery.type']['default'][0] = 'partial delivery';
+		}
+
+		if( $this->getConfigValue( 'delivery.collective', 0 ) ) {
+			$this->feConfig['delivery.type']['default'][2] = 'collective delivery';
+		}
+	}
 
 
 	/**
@@ -87,14 +118,15 @@ class Date
 
 		try
 		{
-			$days = $this->getConfigValue( 'date.minimumdays', 0 );
+			$values = $this->feConfig['delivery.type']['default'];
+
 			$type = \Aimeos\MShop\Order\Item\Base\Service\Base::TYPE_DELIVERY;
 			$service = $this->getBasketService( $basket, $type, $this->getServiceItem()->getCode() );
 
-			if( ( $value = $service->getAttribute( 'date.value', 'delivery' ) ) == '' ) {
-				$feconfig['date.value']['default'] = date( 'Y-m-d', time() + 86400 * $days );
+			if( ( $value = $service->getAttribute( 'delivery.type', 'delivery' ) ) != '' ) {
+				$feconfig['delivery.type']['default'] = $this->sort( $values, $value );
 			} else {
-				$feconfig['date.value']['default'] = $value;
+				$feconfig['delivery.type']['default'] = $values;
 			}
 		}
 		catch( \Aimeos\MShop\Service\Exception $e ) {} // If service isn't available
@@ -113,18 +145,34 @@ class Date
 	public function checkConfigFE( array $attributes )
 	{
 		$result = $this->getProvider()->checkConfigFE( $attributes );
-		$result = array_merge( $result, $this->checkConfig( $this->feConfig, $attributes ) );
+		$result += array_merge( $result, $this->checkConfig( $this->feConfig, $attributes ) );
 
-		if( $result['date.value'] !== null ) {
-			return $result;
-		}
-
-		$minimum = date( 'Y-m-d', time() + 86400 * $this->getConfigValue( 'date.minimumdays', 0 ) );
-
-		if( $attributes['date.value'] < $minimum ) {
-			$result['date.value'] = sprintf( 'Date value before "%1$s"', $minimum );
+		if( isset( $attributes['delivery.type'] )
+			&& !isset( $this->feConfig['delivery.type']['default'][$attributes['delivery.type']] )
+		) {
+			$result['delivery.type'] = $this->getContext()->getI18n()->dt( 'mshop', 'Invalid delivery type' );
 		}
 
 		return $result;
+	}
+
+
+	/**
+	 * Sorts the entry with the given key to the first position
+	 *
+	 * @param array $values Associative list of keys and codes
+	 * @param integer $value Key that should be at first position
+	 * @return array Sorted associative array
+	 */
+	protected function sort( array $values, $value )
+	{
+		if( !isset( $values[$value] ) ) {
+			return $values;
+		}
+
+		$code = $values[$value];
+		unset( $values[$value] );
+
+		return [$value => $code] + $values;
 	}
 }
