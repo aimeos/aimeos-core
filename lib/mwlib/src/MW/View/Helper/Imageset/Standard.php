effd2278bee93a2c52273b5f92da143aa35e758d
Aimeos <aimeos@aimeos.org>
2019-08-24 19:03:08 +0200

    Implemented image srcset view helper

---
+++ lib/mwlib/src/MW/View/Helper/Imageset/Standard.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
+ * @copyright Aimeos (aimeos.org), 2019
+ * @package MW
+ * @subpackage View
+ */
+
+
+namespace Aimeos\MW\View\Helper\Imageset;
+
+
+/**
+ * View helper class for creating an image srcset string
+ *
+ * @package MW
+ * @subpackage View
+ */
+class Standard
+	extends \Aimeos\MW\View\Helper\Base
+	implements \Aimeos\MW\View\Helper\Imageset\Iface
+{
+	/**
+	 * Returns the image srcset value for the given image list
+	 *
+	 * @param array $images List of widths as keys and URLs as values
+	 * @return string Image srcset value
+	 */
+	public function transform( array $images )
+	{
+		$srcset = [];
+		$view = $this->getView();
+
+		foreach( $images as $type => $path ) {
+			$srcset[] = $view->content( $path ) . ' ' . $type . 'w';
+		}
+
+		return join( ', ', $srcset );
+	}
+}
---
+++ lib/mwlib/tests/MW/View/Helper/Imageset/StandardTest.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
+ * @copyright Aimeos (aimeos.org), 2019
+ */
+
+
+namespace Aimeos\MW\View\Helper\Imageset;
+
+
+class StandardTest extends \PHPUnit\Framework\TestCase
+{
+	private $object;
+
+
+	protected function setUp()
+	{
+		$view = new \Aimeos\MW\View\Standard();
+		$view->addHelper( 'content', new \Aimeos\MW\View\Helper\Content\Standard( $view, '/path/to' ) );
+
+		$this->object = new \Aimeos\MW\View\Helper\Imageset\Standard( $view );
+	}
+
+
+	protected function tearDown()
+	{
+		unset( $this->object );
+	}
+
+
+	public function testTransform()
+	{
+		$expected = '/path/to/image-1.jpg 100w, /path/to/image-2.jpg 200w';
+		$images = ['100' => 'image-1.jpg', '200' => 'image-2.jpg'];
+
+		$this->assertEquals( $expected, $this->object->transform( $images ) );
+	}
+}
