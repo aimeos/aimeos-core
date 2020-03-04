9ec27a2765cbe806bf21f3823e41dbb3faeaad0c
Aimeos <aimeos@aimeos.org>
2020-03-04 13:18:14 +0100

    Adapt DB mqueue implementation for SQL Server

--- lib/mwlib/src/MW/MQueue/Standard.php
+++ lib/mwlib/src/MW/MQueue/Standard.php
@@ -50,12 +50,37 @@ class Standard extends Base implements Iface
 	{
 		if( !isset( $this->queues[$name] ) )
 		{
+			$adapter = $this->getConfig( 'db/adapter' );
+
 			$sql = array(
-				'insert' => $this->getConfig( 'sql/insert', 'INSERT INTO madmin_queue (queue, cname, rtime, message) VALUES (?, ?, ?, ?)' ),
-				'reserve' => $this->getConfig( 'sql/reserve', 'UPDATE madmin_queue SET cname = ?, rtime = ? WHERE id IN ( SELECT * FROM ( SELECT id FROM madmin_queue WHERE queue = ? AND rtime < ? LIMIT 1 ) AS t )' ),
-				'get' => $this->getConfig( 'sql/get', 'SELECT * FROM madmin_queue WHERE queue = ? AND cname = ? AND rtime = ? LIMIT 1' ),
-				'delete' => $this->getConfig( 'sql/delete', 'DELETE FROM madmin_queue WHERE id = ? AND queue = ?' ),
+				'insert' => $this->getConfig( 'sql/insert', '
+					INSERT INTO madmin_queue (queue, cname, rtime, message) VALUES (?, ?, ?, ?)
+				' ),
+				'reserve' => $this->getConfig( 'sql/reserve', $adapter !== 'mysql' ? '
+					UPDATE madmin_queue SET cname = ?, rtime = ? WHERE id IN (
+						SELECT id FROM (
+							SELECT id FROM madmin_queue WHERE queue = ? AND rtime < ?
+							ORDER BY id OFFSET 0 ROWS FETCH NEXT 1 ROWS ONLY
+						) AS t
+					)
+				' : '
+					UPDATE madmin_queue SET cname = ?, rtime = ? WHERE id IN (
+						SELECT id FROM (
+							SELECT id FROM madmin_queue WHERE queue = ? AND rtime < ? ORDER BY id LIMIT 1
+						) AS t
+					)
+				' ),
+				'get' => $this->getConfig( 'sql/get', $adapter !== 'mysql' ? '
+					SELECT * FROM madmin_queue WHERE queue = ? AND cname = ? AND rtime = ?
+					ORDER BY id OFFSET 0 ROWS FETCH NEXT 1 ROWS ONLY
+				' : '
+					SELECT * FROM madmin_queue WHERE queue = ? AND cname = ? AND rtime = ? ORDER BY id LIMIT 1
+				' ),
+				'delete' => $this->getConfig( 'sql/delete', '
+					DELETE FROM madmin_queue WHERE id = ? AND queue = ?
+				' ),
 			);
+
 			$rtime = $this->getConfig( 'releasetime', 60 );
 
 			$this->queues[$name] = new \Aimeos\MW\MQueue\Queue\Standard( $this->conn, $name, $sql, $rtime );
--- lib/mwlib/tests/MW/MQueue/Queue/StandardTest.php
+++ lib/mwlib/tests/MW/MQueue/Queue/StandardTest.php
@@ -57,11 +57,40 @@ class StandardTest extends \PHPUnit\Framework\TestCase
 			'db' => \TestHelperMw::getConfig()->get( 'resource/db' ),
 			'sql' => array(
 				'insert' => 'INSERT INTO mw_mqueue_test (queue, cname, rtime, message) VALUES (?, ?, ?, ?)',
-				'reserve' => 'UPDATE mw_mqueue_test SET cname = ?, rtime = ? WHERE id IN ( SELECT * FROM ( SELECT id FROM mw_mqueue_test WHERE queue = ? AND rtime < ? LIMIT 1 ) AS t )',
-				'get' => 'SELECT * FROM mw_mqueue_test WHERE queue = ? AND cname = ? AND rtime = ? LIMIT 1',
 				'delete' => 'DELETE FROM mw_mqueue_test WHERE id = ? AND queue = ?',
 			),
 		);
+
+		if( \TestHelperMw::getConfig()->get( 'resource/db/adapter' ) === 'mysql' )
+		{
+			$config['sql']['reserve'] = '
+				UPDATE mw_mqueue_test SET cname = ?, rtime = ? WHERE id IN (
+					SELECT id FROM (
+						SELECT id FROM mw_mqueue_test WHERE queue = ? AND rtime < ? ORDER BY id LIMIT 1
+					) AS t
+				)
+			';
+			$config['sql']['get'] = '
+				SELECT * FROM mw_mqueue_test WHERE queue = ? AND cname = ? AND rtime = ?
+				ORDER BY id LIMIT 1
+			';
+		}
+		else
+		{
+			$config['sql']['reserve'] = '
+				UPDATE mw_mqueue_test SET cname = ?, rtime = ? WHERE id IN (
+					SELECT id FROM (
+						SELECT id FROM mw_mqueue_test WHERE queue = ? AND rtime < ?
+						ORDER BY id OFFSET 0 ROWS FETCH NEXT 1 ROWS ONLY
+					) AS t
+				)
+			';
+			$config['sql']['get'] = '
+				SELECT * FROM mw_mqueue_test WHERE queue = ? AND cname = ? AND rtime = ?
+				ORDER BY id OFFSET 0 ROWS FETCH NEXT 1 ROWS ONLY
+			';
+		}
+
 		$mqueue = new \Aimeos\MW\MQueue\Standard( $config );
 		$this->object = $mqueue->getQueue( 'email' );
 	}
