c4d94737b4c8a5a706ca7d034dc00a6d8bf8e0ac
Aimeos <aimeos@aimeos.org>
2020-03-05 14:13:37 +0100

    Removed PDO connection timeout for compatibility to SQL Server

--- lib/mwlib/src/MW/DB/Connection/PDO.php
+++ lib/mwlib/src/MW/DB/Connection/PDO.php
@@ -53,7 +53,6 @@ class PDO extends \Aimeos\MW\DB\Connection\Base implements \Aimeos\MW\DB\Connect
 		$attr[\PDO::ATTR_ERRMODE] = \PDO::ERRMODE_EXCEPTION;
 		$attr[\PDO::ATTR_ORACLE_NULLS] = \PDO::NULL_NATURAL;
 		$attr[\PDO::ATTR_STRINGIFY_FETCHES] = false;
-		$attr[\PDO::ATTR_TIMEOUT] = 10;
 
 		$pdo = new \PDO( $dsn, $user, $pass, $attr );
 		$conn = $this->connection;
