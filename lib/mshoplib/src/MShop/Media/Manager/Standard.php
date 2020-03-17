14354b26914c130847572c5c01eee84e066e641b
Aimeos <aimeos@aimeos.org>
2020-03-17 13:53:14 +0100

    Fixed log message for invalid media previews

--- lib/mshoplib/src/MShop/Media/Manager/Standard.php
+++ lib/mshoplib/src/MShop/Media/Manager/Standard.php
@@ -707,7 +707,7 @@ class Standard
 			{
 				if( ( $row['media.previews'] = json_decode( $config = $row['media.previews'], true ) ) === null )
 				{
-					$msg = sprintf( 'Invalid JSON as result of search for ID "%2$s" in "%1$s": %3$s', 'mshop_media.config', $row['media.id'], $config );
+					$msg = sprintf( 'Invalid JSON as result of search for ID "%2$s" in "%1$s": %3$s', 'mshop_media.previews', $row['media.id'], $config );
 					$this->getContext()->getLogger()->log( $msg, \Aimeos\MW\Logger\Base::WARN );
 				}
 				$map[$row['media.id']] = $row;
