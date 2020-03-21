5a866eaee391a9389d3fef31010378bf63a924b1
Aimeos <aimeos@aimeos.org>
2020-03-21 18:18:26 +0100

    Added customer/group attribute type

--- lib/mshoplib/setup/default/data/type.php
+++ lib/mshoplib/setup/default/data/type.php
@@ -19,6 +19,7 @@ return array(
 		array( 'domain' => 'product', 'code' => 'text', 'label' => 'Text', 'status' => 1 ),
 		array( 'domain' => 'product', 'code' => 'date', 'label' => 'Date', 'status' => 1 ),
 		array( 'domain' => 'product', 'code' => 'interval', 'label' => 'Interval', 'status' => 1 ),
+		array( 'domain' => 'product', 'code' => 'customer/group', 'label' => 'Customer group ID', 'status' => 1 ),
 	),
 
 	'attribute/lists/type' => array(
