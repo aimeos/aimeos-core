340c1fba8ffbd36f3927ded98fb061da4874bcdf
Aimeos <aimeos@aimeos.org>
2020-03-24 13:15:17 +0100

    Allow event product to be added to the basket before they start

--- lib/mshoplib/src/MShop/Plugin/Provider/Order/ProductGone.php
+++ lib/mshoplib/src/MShop/Plugin/Provider/Order/ProductGone.php
@@ -94,8 +94,9 @@ class ProductGone
 
 			$start = $product->getDateStart();
 			$end = $product->getDateEnd();
+			$type = $product->getType();
 
-			if( ( ( $start !== null ) && ( $start >= $now ) ) || ( ( $end !== null ) && ( $now > $end ) ) ) {
+			if( ( $type !== 'event' && $start !== null && $start >= $now ) || ( $end !== null && $now > $end ) ) {
 				$notAvailable[$position] = 'gone.timeframe';
 			}
 		}
