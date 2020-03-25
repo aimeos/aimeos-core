37b677c948849fe407b72db71bbef69d5177dffe
Aimeos <aimeos@aimeos.org>
2020-03-25 13:10:28 +0100

    Added custom date and text attributes

--- lib/mshoplib/setup/default/data/attribute.php
+++ lib/mshoplib/setup/default/data/attribute.php
@@ -4,9 +4,13 @@
  * @copyright Aimeos (aimeos.org), 2017-2020
  */
 
-return [
-	[
-		'attribute.domain' => 'product', 'attribute.type' => 'price', 'attribute.code' => 'custom',
-		'attribute.label' => 'Custom product price', 'attribute.position' => 0, 'attribute.status' => 1
-	],
-];
+return [[
+	'attribute.domain' => 'product', 'attribute.type' => 'price', 'attribute.code' => 'custom',
+	'attribute.label' => 'Custom price', 'attribute.position' => 0, 'attribute.status' => 1
+],[
+	'attribute.domain' => 'product', 'attribute.type' => 'date', 'attribute.code' => 'custom',
+	'attribute.label' => 'Custom date', 'attribute.position' => 0, 'attribute.status' => 1
+],[
+	'attribute.domain' => 'product', 'attribute.type' => 'text', 'attribute.code' => 'custom',
+	'attribute.label' => 'Custom text', 'attribute.position' => 0, 'attribute.status' => 1
+]];
