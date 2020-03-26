77b304be3c07f6fa0d17b403fc06e71deb57bce1
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2020-03-26 17:53:40 +0100

    Scrutinizer Auto-Fixes (#193)
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- lib/mshoplib/setup/default/data/attribute.php
+++ lib/mshoplib/setup/default/data/attribute.php
@@ -7,10 +7,10 @@
 return [[
 	'attribute.domain' => 'product', 'attribute.type' => 'price', 'attribute.code' => 'custom',
 	'attribute.label' => 'Custom price', 'attribute.position' => 0, 'attribute.status' => 1
-],[
+], [
 	'attribute.domain' => 'product', 'attribute.type' => 'date', 'attribute.code' => 'custom',
 	'attribute.label' => 'Custom date', 'attribute.position' => 0, 'attribute.status' => 1
-],[
+], [
 	'attribute.domain' => 'product', 'attribute.type' => 'text', 'attribute.code' => 'custom',
 	'attribute.label' => 'Custom text', 'attribute.position' => 0, 'attribute.status' => 1
 ]];
