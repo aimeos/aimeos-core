c82e75101fb9485153b721b89559c7c054f27050
Aimeos <aimeos@aimeos.org>
2020-03-29 17:11:43 +0200

    Use the URL segment of the category if available

--- lib/mshoplib/src/MShop/Catalog/Item/Standard.php
+++ lib/mshoplib/src/MShop/Catalog/Item/Standard.php
@@ -132,6 +132,22 @@ class Standard
 
 
 	/**
+	 * Returns the localized text type of the item or the internal label if no name is available.
+	 *
+	 * @param string $type Text type to be returned
+	 * @return string Specified text type or label of the item
+	 */
+	public function getName( string $type = 'name' ) : string
+	{
+		if( ( $item = $this->getRefItems( 'text', $type )->first() ) !== null ) {
+			return $item->getContent();
+		}
+
+		return $type === 'url' ? $this->getUrl() : $this->getLabel();
+	}
+
+
+	/**
 	 * Returns the URL segment for the catalog item.
 	 *
 	 * @return string URL segment of the catalog item
