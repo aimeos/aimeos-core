c479772d4999ec78580991809435b469bebc172b
Aimeos <aimeos@aimeos.org>
2020-03-31 15:22:17 +0200

    Allow NULL values for singular string

--- lib/mwlib/src/MW/View/Helper/Translate/Iface.php
+++ lib/mwlib/src/MW/View/Helper/Translate/Iface.php
@@ -23,11 +23,11 @@ interface Iface extends \Aimeos\MW\View\Helper\Iface
 	 * Returns the translated string or the original one if no translation is available.
 	 *
 	 * @param string $domain Translation domain from core or an extension
-	 * @param string $singular Singular form of the text to translate
+	 * @param string|null $singular Singular form of the text to translate
 	 * @param string|null $plural Plural form of the text, used if $number is greater than one
 	 * @param int $number Amount of things relevant for the plural form
 	 * @param bool $force Return string untranslated if no translation is available
 	 * @return string|null Translated string or NULL if no translation is available and force parameter is FALSE
 	 */
-	public function transform( string $domain, string $singular, ?string $plural = null, int $number = 1, bool $force = true ) : ?string;
+	public function transform( string $domain, ?string $singular, ?string $plural = null, int $number = 1, bool $force = true ) : ?string;
 }
--- lib/mwlib/src/MW/View/Helper/Translate/Standard.php
+++ lib/mwlib/src/MW/View/Helper/Translate/Standard.php
@@ -43,21 +43,21 @@ class Standard
 	 * Returns the translated string or the original one if no translation is available.
 	 *
 	 * @param string $domain Translation domain from core or an extension
-	 * @param string $singular Singular form of the text to translate
+	 * @param string|null $singular Singular form of the text to translate
 	 * @param string|null $plural Plural form of the text, used if $number is greater than one
 	 * @param int $number Amount of things relevant for the plural form
 	 * @param bool $force Return string untranslated if no translation is available
 	 * @return string|null Translated string or NULL if no translation is available and force parameter is FALSE
 	 */
-	public function transform( string $domain, string $singular, ?string $plural = null, int $number = 1, bool $force = true ) : ?string
+	public function transform( string $domain, ?string $singular, ?string $plural = null, int $number = 1, bool $force = true ) : ?string
 	{
 		if( $plural )
 		{
-			$trans = $this->translator->dn( $domain, $singular, $plural, $number );
-			return $trans === $plural && $force === false ? null : $trans;
+			$trans = $this->translator->dn( $domain, (string) $singular, $plural, $number );
+			return $trans !== $plural || $force ? $trans : null;
 		}
 
-		$trans = $this->translator->dt( $domain, $singular );
-		return $trans === $singular && $force === false ? null : $trans;
+		$trans = $this->translator->dt( $domain, (string) $singular );
+		return $trans !== $singular || $force ? $trans : null;
 	}
 }
