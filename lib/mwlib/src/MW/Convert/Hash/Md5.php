3247c364b85f54e8e529a2ef94381e024f2e725f
Aimeos <aimeos@aimeos.org>
2020-04-13 14:11:57 +0200

    Added converter for MD5 hashes

---
+++ lib/mwlib/src/MW/Convert/Hash/Md5.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
+ * @copyright Aimeos (aimeos.org), 2020
+ * @package MW
+ * @subpackage Convert
+ */
+
+
+namespace Aimeos\MW\Convert\Hash;
+
+
+/**
+ * Converts numbers with comma to point as decimal separator
+ *
+ * @package MW
+ * @subpackage Convert
+ */
+class Md5 implements \Aimeos\MW\Convert\Iface
+{
+	/**
+	 * Translates a value to another one.
+	 *
+	 * @param mixed $value Value to translate
+	 * @return mixed Translated value
+	 */
+	public function translate( $value )
+	{
+		return md5( $value );
+	}
+
+
+	/**
+	 * Reverses the translation of the value.
+	 *
+	 * @param mixed $value Value to reverse
+	 * @return mixed Reversed translation
+	 */
+	public function reverse( $value )
+	{
+		return $value;
+	}
+}
