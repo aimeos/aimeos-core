7cc9af9f6a5dec6eee45fc20ac64aa3b86495340
Aimeos <aimeos@aimeos.org>
2020-05-07 18:29:32 +0200

    Relaxed check for subscription period

--- lib/mshoplib/src/MShop/Subscription/Item/Standard.php
+++ lib/mshoplib/src/MShop/Subscription/Item/Standard.php
@@ -145,7 +145,7 @@ class Standard
 	 */
 	public function setInterval( string $value ) : \Aimeos\MShop\Subscription\Item\Iface
 	{
-		if( preg_match( '/^P[0-9]+Y[0-9]+M[0-9]+W[0-9]+D$/', $value ) !== 1 ) {
+		if( strlen( $value ) > 1 && preg_match( '/^P([0-9]+Y)?([0-9]+M)?([0-9]+W)?([0-9]+D)?$/', $value ) !== 1 ) {
 			throw new \Aimeos\MShop\Subscription\Exception( sprintf( 'Invalid time interval format "%1$s"', $value ) );
 		}
 
