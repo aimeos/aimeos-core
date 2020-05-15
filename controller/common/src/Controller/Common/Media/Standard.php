811b9491817debde02fce31992859d8c6e91bbbd
Aimeos <aimeos@aimeos.org>
2020-05-15 14:05:03 +0200

    Allow media file names with sanitized parts

--- controller/common/src/Controller/Common/Media/Standard.php
+++ controller/common/src/Controller/Common/Media/Standard.php
@@ -395,7 +395,8 @@ class Standard
 		 */
 		$list = $this->context->getConfig()->get( 'controller/common/media/standard/extensions', [] );
 
-		$filename = md5( $filename . getmypid() . microtime( true ) );
+		$filename = trim( preg_replace( '/[^A-Za-z0-9]+/', '_', $filename ), '_' );
+		$filename = substr( md5( $filename . getmypid() . microtime( true ) ), -8 ) . '_' . $filename;
 		$ext = isset( $list[$mimeext] ) ? '.' . $list[$mimeext] : ( ctype_alpha( $mimeext ) ? '.' . $mimeext : '' );
 
 		return "${type}/${filename[0]}/${filename[1]}/${filename}${ext}";
