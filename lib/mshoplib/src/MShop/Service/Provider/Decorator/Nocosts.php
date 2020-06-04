d89274baa13e038f13c934f7b7246eb968be0a1a
Aimeos <aimeos@aimeos.org>
2020-06-04 18:19:43 +0200

    Implemented Nocosts service decorator to avoid costs per item when using click&collect

---
+++ lib/mshoplib/src/MShop/Service/Provider/Decorator/Nocosts.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
+ * @copyright Aimeos (aimeos.org), 2020
+ * @package MShop
+ * @subpackage Service
+ */
+
+
+namespace Aimeos\MShop\Service\Provider\Decorator;
+
+
+/**
+ * Decorator for service providers setting costs to zero.
+ *
+ * @package MShop
+ * @subpackage Service
+ */
+class Nocosts
+	extends \Aimeos\MShop\Service\Provider\Decorator\Base
+	implements \Aimeos\MShop\Service\Provider\Decorator\Iface
+{
+	/**
+	 * Returns the costs per item as negative value to get no costs at all.
+	 *
+	 * @param \Aimeos\MShop\Order\Item\Base\Iface $basket Basket object
+	 * @return \Aimeos\MShop\Price\Item\Iface Price item containing the price, shipping, rebate
+	 */
+	public function calcPrice( \Aimeos\MShop\Order\Item\Base\Iface $basket ) : \Aimeos\MShop\Price\Item\Iface
+	{
+		$costs = 0;
+		$price = $this->getProvider()->calcPrice( $basket );
+
+		foreach( $basket->getProducts() as $product ) {
+			$costs += $product->getPrice()->getCosts() * $product->getQuantity();
+		}
+
+		return $price->setCosts( -$costs );
+	}
+}
