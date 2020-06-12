10ee87991463f5c56757a2dbdbcc46c861e6dc4b
Aimeos <aimeos@aimeos.org>
2020-06-12 11:10:24 +0200

    Adds service provider decorator checking for basket value of 0.00

---
+++ lib/mshoplib/src/MShop/Service/Provider/Decorator/Free.php
@@ -0,0 +1,81 @@
+<?php
+
+/**
+ * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
+ * @copyright Aimeos (aimeos.org), 2020
+ * @package MShop
+ * @subpackage Service
+ */
+
+
+namespace Aimeos\MShop\Service\Provider\Decorator;
+
+
+/**
+ * Checks if basket total value is 0.00 and enables/disables option
+ *
+ * @package MShop
+ * @subpackage Service
+ */
+class Free
+	extends \Aimeos\MShop\Service\Provider\Decorator\Base
+	implements \Aimeos\MShop\Service\Provider\Decorator\Iface
+{
+	private $beConfig = [
+		'free.show' => [
+			'code' => 'free.show',
+			'internalcode' => 'free.show',
+			'label' => 'Enable or disable option if basket total is 0.00',
+			'type' => 'boolean',
+			'internaltype' => 'boolean',
+			'default' => '',
+			'required' => true,
+		],
+	];
+
+
+	/**
+	 * Checks the backend configuration attributes for validity.
+	 *
+	 * @param array $attributes Attributes added by the shop owner in the administraton interface
+	 * @return array An array with the attribute keys as key and an error message as values for all attributes that are
+	 * 	known by the provider but aren't valid
+	 */
+	public function checkConfigBE( array $attributes ) : array
+	{
+		$error = $this->getProvider()->checkConfigBE( $attributes );
+		$error += $this->checkConfig( $this->beConfig, $attributes );
+
+		return $error;
+	}
+
+
+	/**
+	 * Returns the configuration attribute definitions of the provider to generate a list of available fields and
+	 * rules for the value of each field in the administration interface.
+	 *
+	 * @return array List of attribute definitions implementing \Aimeos\MW\Common\Critera\Attribute\Iface
+	 */
+	public function getConfigBE() : array
+	{
+		return array_merge( $this->getProvider()->getConfigBE(), $this->getConfigItems( $this->beConfig ) );
+	}
+
+
+	/**
+	 * Checks if the service provider should be available.
+	 *
+	 * @param \Aimeos\MShop\Order\Item\Base\Iface $basket Basket object
+	 * @return bool True if payment provider can be used, false if not
+	 */
+	public function isAvailable( \Aimeos\MShop\Order\Item\Base\Iface $basket ) : bool
+	{
+		$value = $basket->getPrice()->getValue();
+
+		if( $value === '0.00' && (bool) $this->getConfigValue( 'free.show' ) === false ) {
+			return false;
+		}
+
+		return $this->getProvider()->isAvailable( $basket );
+	}
+}
---
+++ lib/mshoplib/tests/MShop/Service/Provider/Decorator/Free.php
@@ -0,0 +1,120 @@
+<?php
+
+/**
+ * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
+ * @copyright Aimeos (aimeos.org), 2020
+ */
+
+
+namespace Aimeos\MShop\Service\Provider\Decorator;
+
+
+class DownloadTest extends \PHPUnit\Framework\TestCase
+{
+	private $object;
+	private $context;
+	private $servItem;
+	private $mockProvider;
+
+
+	protected function setUp() : void
+	{
+		$this->context = \TestHelperMShop::getContext();
+
+		$servManager = \Aimeos\MShop\Service\Manager\Factory::create( $this->context );
+		$this->servItem = $servManager->createItem();
+
+		$this->mockProvider = $this->getMockBuilder( \Aimeos\MShop\Service\Provider\Decorator\Example::class )
+			->disableOriginalConstructor()->getMock();
+
+		$this->object = new \Aimeos\MShop\Service\Provider\Decorator\Free( $this->mockProvider, $this->context, $this->servItem );
+	}
+
+
+	protected function tearDown() : void
+	{
+		\Aimeos\MShop\Order\Manager\Factory::injectManager( '\Aimeos\MShop\Order\Manager\StandardMock', null );
+	}
+
+
+	public function testGetConfigBE()
+	{
+		$this->mockProvider->expects( $this->once() )->method( 'getConfigBE' )->will( $this->returnValue( [] ) );
+
+		$result = $this->object->getConfigBE();
+
+		$this->assertArrayHasKey( 'free.show', $result );
+	}
+
+
+	public function testCheckConfigBEOK()
+	{
+		$this->mockProvider->expects( $this->once() )
+			->method( 'checkConfigBE' )
+			->will( $this->returnValue( [] ) );
+
+		$attributes = array( 'free.show' => '1' );
+		$result = $this->object->checkConfigBE( $attributes );
+
+		$this->assertEquals( 1, count( $result ) );
+		$this->assertNull( $result['free.show'] );
+	}
+
+
+	public function testCheckConfigBEFailure()
+	{
+		$this->mockProvider->expects( $this->once() )
+			->method( 'checkConfigBE' )
+			->will( $this->returnValue( [] ) );
+
+		$attributes = array( 'free.show' => [] );
+		$result = $this->object->checkConfigBE( $attributes );
+
+		$this->assertEquals( 1, count( $result ) );
+		$this->assertIsString( $result['free.show'] );
+	}
+
+
+	public function testIsAvailable()
+	{
+		$this->servItem->setConfig( array( 'free.show' => '1' ) );
+		$basket = \Aimeos\MShop::create( $this->context, 'order/base' )->off();
+
+		$this->mockProvider->expects( $this->once() )->method( 'isAvailable' )->will( $this->returnValue( true ) );
+
+		$this->assertTrue( $this->object->isAvailable( $basket ) );
+	}
+
+
+	public function testIsNotAvailable()
+	{
+		$this->servItem->setConfig( array( 'free.show' => '1' ) );
+		$basket = \Aimeos\MShop::create( $this->context, 'order/base' )->off()->getPrice()->setValue( '0.01' );
+
+		$this->mockProvider->expects( $this->once() )->method( 'isAvailable' )->will( $this->returnValue( true ) );
+
+		$this->assertFalse( $this->object->isAvailable( $basket ) );
+	}
+
+
+	public function testIsAvailableHidden()
+	{
+		$this->servItem->setConfig( array( 'free.show' => '0' ) );
+		$basket = \Aimeos\MShop::create( $this->context, 'order/base' )->off()->getPrice()->setValue( '0.01' );
+
+		$this->mockProvider->expects( $this->once() )->method( 'isAvailable' )->will( $this->returnValue( true ) );
+
+		$this->assertTrue( $this->object->isAvailable( $basket ) );
+	}
+
+
+	public function testIsNotAvailableHidden()
+	{
+		$this->servItem->setConfig( array( 'free.show' => '0' ) );
+		$basket = \Aimeos\MShop::create( $this->context, 'order/base' )->off();
+
+		$this->mockProvider->expects( $this->once() )->method( 'isAvailable' )->will( $this->returnValue( true ) );
+
+		$this->assertFalse( $this->object->isAvailable( $basket ) );
+	}
+}
