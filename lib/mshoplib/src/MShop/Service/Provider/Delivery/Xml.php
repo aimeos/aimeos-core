da326b8da19e5d8c56a9ade66f4d803154eeb793
Aimeos <aimeos@aimeos.org>
2020-07-11 16:56:01 +0200

    Fixed custom order XML template config

--- lib/mshoplib/src/MShop/Service/Provider/Delivery/Xml.php
+++ lib/mshoplib/src/MShop/Service/Provider/Delivery/Xml.php
@@ -204,7 +204,7 @@ class Xml
 	protected function createXml( array $orderItems, array $baseItems ) : string
 	{
 		$view = $this->getContext()->getView();
-		$template = $this->getConfigValue( 'template', 'service/provider/delivery/xml-body-standard' );
+		$template = $this->getConfigValue( 'xml.template', 'service/provider/delivery/xml-body-standard' );
 
 		return $view->assign( ['orderItems' => $orderItems, 'baseItems' => $baseItems] )->render( $template );
 	}
