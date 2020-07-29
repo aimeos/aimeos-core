02e97343a5729a9bc68e8f6706d179ef043826f7
Aimeos <aimeos@aimeos.org>
2020-07-29 18:33:17 +0200

    Make customer status and groups private in fromArray() and toArray()

--- lib/mshoplib/src/MShop/Customer/Item/Standard.php
+++ lib/mshoplib/src/MShop/Customer/Item/Standard.php
@@ -278,8 +278,8 @@ class Standard extends Base implements Iface
 				case 'customer.label': $item = $item->setLabel( $value ); break;
 				case 'customer.code': $item = $item->setCode( $value ); break;
 				case 'customer.birthday': $item = $item->setBirthday( $value ); break;
-				case 'customer.status': $item = $item->setStatus( $value ); break;
-				case 'customer.groups': $item = $item->setGroups( $value ); break;
+				case 'customer.status': !$private ?: $item = $item->setStatus( (int) $value ); break;
+				case 'customer.groups': !$private ?: $item = $item->setGroups( (array) $value ); break;
 				case 'customer.password': !$private ?: $item = $item->setPassword( $value ); break;
 				case 'customer.dateverified': !$private ?: $item = $item->setDateVerified( $value ); break;
 				default: continue 2;
@@ -305,11 +305,11 @@ class Standard extends Base implements Iface
 		$list['customer.label'] = $this->getLabel();
 		$list['customer.code'] = $this->getCode();
 		$list['customer.birthday'] = $this->getBirthday();
-		$list['customer.status'] = $this->getStatus();
-		$list['customer.groups'] = $this->getGroups();
 
 		if( $private === true )
 		{
+			$list['customer.status'] = $this->getStatus();
+			$list['customer.groups'] = $this->getGroups();
 			$list['customer.password'] = $this->getPassword();
 			$list['customer.dateverified'] = $this->getDateVerified();
 		}
