fd8d49cbc95e5b12e5a645d46d91b2e75072bfb2
Marcus Förster <m.foerster@xerc.de>
2020-08-03 13:40:03 +0200

    [FIX] TSL 4 licenses

--- lib/mshoplib/tests/MShop/Order/Manager/Base/Service/Attribute/StandardTest.php
+++ lib/mshoplib/tests/MShop/Order/Manager/Base/Service/Attribute/StandardTest.php
@@ -4,7 +4,7 @@ namespace Aimeos\MShop\Order\Manager\Base\Service\Attribute;
 
 
 /**
- * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
+ * @license LGPLv3, https://opensource.org/licenses/LGPL-3.0
  * @copyright Metaways Infosystems GmbH, 2011
  * @copyright Aimeos (aimeos.org), 2015-2020
  */
