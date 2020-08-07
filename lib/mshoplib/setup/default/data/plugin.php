66bd13f2d556f31ba7b9e741c043968a4f8d4768
Aimeos <aimeos@aimeos.org>
2020-08-07 08:46:24 +0200

    Improves plugin order

--- lib/mshoplib/setup/default/data/plugin.php
+++ lib/mshoplib/setup/default/data/plugin.php
@@ -7,18 +7,81 @@
  */
 
 return [
-	'plugin' => [
-		['type' => 'order', 'label' => 'Updates delivery/payment options on basket change', 'provider' => 'ServicesUpdate', 'config' => [], 'position' => 0, 'status' => 1],
-		['type' => 'order', 'label' => 'Adds addresses/delivery/payment to basket', 'provider' => 'Autofill', 'config' => ['address' => 1, 'useorder' => 1, 'orderaddress' => 1, 'orderservice' => 1, 'delivery' => 1, 'payment' => 0], 'position' => 1, 'status' => 1],
-		['type' => 'order', 'label' => 'Checks for required addresses (billing/delivery)', 'provider' => 'AddressesAvailable', 'config' => ['payment' => 1, 'delivery' => ''], 'position' => 2, 'status' => 1],
-		['type' => 'order', 'label' => 'Checks for required services (delivery/payment)', 'provider' => 'ServicesAvailable', 'config' => ['payment' => 1, 'delivery' => 1], 'position' => 3, 'status' => 1],
-		['type' => 'order', 'label' => 'Checks for deleted products', 'provider' => 'ProductGone', 'config' => [], 'position' => 4, 'status' => 1],
-		['type' => 'order', 'label' => 'Checks for changed product prices', 'provider' => 'ProductPrice', 'config' => [], 'position' => 5, 'status' => 1],
-		['type' => 'order', 'label' => 'Checks for necessary basket limits', 'provider' => 'BasketLimits', 'config' => ['min-products' => 1, 'max-products' => 100, 'min-value' => ['EUR' => '1.00'], 'max-value' => ['EUR' => '10000.00']], 'position' => 6, 'status' => 0],
-		['type' => 'order', 'label' => 'Limits maximum amount of products', 'provider' => 'ProductLimit', 'config' => ['single-number-max' => 10, 'total-number-max' => 100, 'single-value-max' => ['EUR' => '1000.00'], 'total-value-max' => ['EUR' => '10000.00']], 'position' => 7, 'status' => 0],
-		['type' => 'order', 'label' => 'Free shipping above threshold', 'provider' => 'Shipping', 'config' => ['threshold' => ['EUR' => '1.00']], 'position' => 8, 'status' => 0],
-		['type' => 'order', 'label' => 'Country and state tax rates', 'provider' => 'Taxrate', 'config' => ['country-taxrates' => ['US' => '5.00', 'AT' => '20.00'], 'state-taxrates' => ['CA' =>'6.25']], 'position' => 9, 'status' => 0],
-		['type' => 'order', 'label' => 'Coupon update', 'provider' => 'Coupon', 'config' => [], 'position' => 100, 'status' => 1],
-		['type' => 'order', 'label' => 'Checks for products out of stock', 'provider' => 'ProductStock', 'config' => [], 'position' => 101, 'status' => 1],
-	]
+	'plugin' => [[
+		'type' => 'order', 'label' => 'Limits maximum amount of products',
+		'provider' => 'ProductLimit', 'position' => 10, 'status' => 0,
+		'config' => [
+			'single-number-max' => 10,
+			'total-number-max' => 100,
+			'single-value-max' => ['EUR' => '1000.00'],
+			'total-value-max' => ['EUR' => '10000.00']
+		]
+	], [
+		'type' => 'order', 'label' => 'Checks for deleted products',
+		'provider' => 'ProductGone', 'position' => 20, 'status' => 1,
+		'config' => []
+	], [
+		'type' => 'order', 'label' => 'Checks for products out of stock',
+		'provider' => 'ProductStock', 'position' => 30, 'status' => 1,
+		'config' => []
+	], [
+		'type' => 'order', 'label' => 'Checks for changed product prices',
+		'provider' => 'ProductPrice', 'position' => 40, 'status' => 1,
+		'config' => []
+	], [
+		'type' => 'order', 'label' => 'Adds addresses/delivery/payment to basket',
+		'provider' => 'Autofill', 'position' => 50, 'status' => 1,
+		'config' => [
+			'address' => 1,
+			'useorder' => 1,
+			'orderaddress' => 1,
+			'orderservice' => 1,
+			'delivery' => 1,
+			'payment' => 0
+		]
+	], [
+		'type' => 'order', 'label' => 'Updates delivery/payment options on basket change',
+		'provider' => 'ServicesUpdate', 'position' => 60, 'status' => 1,
+		'config' => []
+	], [
+		'type' => 'order', 'label' => 'Free shipping above threshold',
+		'provider' => 'Shipping', 'position' => 70, 'status' => 0,
+		'config' => [
+			'threshold' => ['EUR' => '1.00']
+		]
+	], [
+		'type' => 'order', 'label' => 'Checks for necessary basket limits',
+		'provider' => 'BasketLimits', 'position' => 80, 'status' => 0,
+		'config' => [
+			'min-products' => 1,
+			'max-products' => 100,
+			'min-value' => ['EUR' => '1.00'],
+			'max-value' => ['EUR' => '10000.00']
+		]
+	], [
+		'type' => 'order', 'label' => 'Country and state tax rates',
+		'provider' => 'Taxrate', 'position' => 90, 'status' => 0,
+		'config' => [
+			'country-taxrates' => ['US' => '5.00', 'AT' => '20.00'],
+			'state-taxrates' => ['CA' =>'6.25']
+		]
+	], [
+		'type' => 'order', 'label' => 'Coupon update',
+		'provider' => 'Coupon', 'position' => 100, 'status' => 1,
+		'config' => []
+	], [
+		'type' => 'order', 'label' => 'Checks for required addresses (billing/delivery)',
+		'provider' => 'AddressesAvailable', 'position' => 110, 'status' => 1,
+		'config' => [
+			'payment' => 1,
+			'delivery' => ''
+		]
+	], [
+		'type' => 'order', 'label' => 'Checks for required services (delivery/payment)',
+		'provider' => 'ServicesAvailable', 'position' => 120, 'status' => 1,
+		'config' => [
+			'payment' => 1,
+			'delivery' => 1
+		]
+	]]
 ];
