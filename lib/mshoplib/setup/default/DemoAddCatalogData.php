3f57e7832230fac182df395f2ad1ec4fe5fd873d
Aimeos <aimeos@aimeos.org>
2020-10-16 09:42:20 +0200

    Added ability to import sub-categories from demo data

--- lib/mshoplib/setup/default/DemoAddCatalogData.php
+++ lib/mshoplib/setup/default/DemoAddCatalogData.php
@@ -68,6 +68,10 @@ class DemoAddCatalogData extends \Aimeos\MW\Setup\Task\MShopAddDataAbstract
 		}
 		catch( \Exception $e ) {; } // If no root node was already inserted into the database
 
+		$search = $manager->createSearch();
+		$search->setConditions( $search->compare( '=~', 'catalog.code', 'demo-' ) );
+		$manager->deleteItems( $manager->searchItems( $search )->getId()->toArray() );
+
 
 		if( $value === '1' )
 		{
@@ -80,13 +84,8 @@ class DemoAddCatalogData extends \Aimeos\MW\Setup\Task\MShopAddDataAbstract
 
 			if( $item === null )
 			{
-				$item = $manager->createItem();
-				$item->setCode( $data['code'] );
-				$item->setLabel( $data['label'] );
-				$item->setConfig( $data['config'] );
-				$item->setStatus( $data['status'] );
-
-				$manager->insertItem( $item );
+				$item = $manager->createItem()->fromArray( $data );
+				$item = $manager->insertItem( $item );
 			}
 
 
@@ -102,11 +101,51 @@ class DemoAddCatalogData extends \Aimeos\MW\Setup\Task\MShopAddDataAbstract
 				$this->addTexts( $item->getId(), $data['text'], 'catalog' );
 			}
 
+			if( isset( $data['catalog'] ) ) {
+				$this->addCatalog( $item->getId(), $data['catalog'], 'catalog' );
+			}
+
 			$this->status( 'added' );
 		}
 		else
 		{
 			$this->status( 'removed' );
+		}
+	}
+
+
+	/**
+	 * Adds the catalog items including referenced items
+	 *
+	 * @param string $id Unique ID of the parent category
+	 * @param array $data List of category data
+	 * @param string $domain Parent domain name (catalog)
+	 */
+	protected function addCatalog( string $id, array $data, string $domain )
+	{
+		$context = $this->getContext();
+		$manager = \Aimeos\MShop::create( $context, $domain );
+
+		foreach( $data as $entry )
+		{
+			$item = $manager->createItem()->fromArray( $entry );
+			$item = $manager->insertItem( $item, $id );
+
+			if( isset( $entry['media'] ) ) {
+				$this->addMedia( $item->getId(), $entry['media'], $domain );
+			}
+
+			if( isset( $entry['product'] ) ) {
+				$this->addProducts( $item->getId(), $entry['product'], $domain );
+			}
+
+			if( isset( $entry['text'] ) ) {
+				$this->addTexts( $item->getId(), $entry['text'], $domain );
+			}
+
+			if( isset( $data['catalog'] ) ) {
+				$this->addCatalog( $item->getId(), $data['catalog'], $domain );
+			}
 		}
 	}
 }
