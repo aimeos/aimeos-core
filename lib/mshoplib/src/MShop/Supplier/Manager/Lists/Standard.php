ff274d0b2119c912b8f94bd0392031782608081c
A.O <35069360+oulfr@users.noreply.github.com>
2021-04-17 18:53:51 +0200

    Delete duplicated entries (#218)
    Remove duplicate search config field

--- lib/mshoplib/src/MShop/Supplier/Manager/Lists/Standard.php
+++ lib/mshoplib/src/MShop/Supplier/Manager/Lists/Standard.php
@@ -134,14 +134,6 @@ class Standard
 			'internaltype' => \Aimeos\MW\DB\Statement\Base::PARAM_STR,
 			'public' => false,
 		),
-		'supplier.lists.key' => array(
-			'code' => 'supplier.lists.key',
-			'internalcode' => 'msupli."key"',
-			'label' => 'List key',
-			'type' => 'string',
-			'internaltype' => \Aimeos\MW\DB\Statement\Base::PARAM_STR,
-			'public' => false,
-		),
 	);
 
 
