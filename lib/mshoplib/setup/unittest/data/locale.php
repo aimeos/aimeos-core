996e4f6f1b7aa3532b3ebfa834ba2c4750654185
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2021-05-09 10:36:07 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- lib/mshoplib/setup/unittest/data/locale.php
+++ lib/mshoplib/setup/unittest/data/locale.php
@@ -11,27 +11,27 @@ return [
 		'unittest' => [
 			'code' => 'unittest', 'label' => 'Unit test site', 'status' => 1,
 			'logo' => [1 => 'path/to/site-logo.png'], 'supplierid' => '1234',
-			'config' => [ "timezone" => "Europe/Berlin", "emailfrom" => "no-reply@example.com", "emailreply" => "test@example.com" ]
+			'config' => ["timezone" => "Europe/Berlin", "emailfrom" => "no-reply@example.com", "emailreply" => "test@example.com"]
 		]
 	],
 
 	'locale/currency' => [
-		'EUR' => [ 'id' => 'EUR', 'label' => 'Euro', 'status' => 1 ],
-		'CHF' => [ 'id' => 'CHF', 'label' => 'Swiss franc', 'status' => 0 ],
-		'USD' => [ 'id' => 'USD', 'label' => 'US dollar', 'status' => 1 ],
-		'XAF' => [ 'id' => 'XAF', 'label' => 'CFA Franc BEAC', 'status' => 0 ],
-		'XOF' => [ 'id' => 'XOF', 'label' => 'CFA Franc BCEAO', 'status' => 0 ],
+		'EUR' => ['id' => 'EUR', 'label' => 'Euro', 'status' => 1],
+		'CHF' => ['id' => 'CHF', 'label' => 'Swiss franc', 'status' => 0],
+		'USD' => ['id' => 'USD', 'label' => 'US dollar', 'status' => 1],
+		'XAF' => ['id' => 'XAF', 'label' => 'CFA Franc BEAC', 'status' => 0],
+		'XOF' => ['id' => 'XOF', 'label' => 'CFA Franc BCEAO', 'status' => 0],
 	],
 
 	'locale/language' => [
-		'de' => [ 'id' => 'de', 'label' => 'German', 'status' => 1 ],
-		'en' => [ 'id' => 'en', 'label' => 'English', 'status' => 1 ],
-		'es' => [ 'id' => 'es', 'label' => 'Spanish', 'status' => 1 ],
-		'it' => [ 'id' => 'it', 'label' => 'Italian', 'status' => 0 ],
+		'de' => ['id' => 'de', 'label' => 'German', 'status' => 1],
+		'en' => ['id' => 'en', 'label' => 'English', 'status' => 1],
+		'es' => ['id' => 'es', 'label' => 'Spanish', 'status' => 1],
+		'it' => ['id' => 'it', 'label' => 'Italian', 'status' => 0],
 	],
 
 	'locale' => [
-		[ 'siteid' => 'unittest', 'langid' => 'de', 'currencyid' => 'EUR', 'pos' => 0, 'status' => 0 ],
-		[ 'siteid' => 'unittest', 'langid' => 'en', 'currencyid' => 'EUR', 'pos' => 1, 'status' => 1 ],
+		['siteid' => 'unittest', 'langid' => 'de', 'currencyid' => 'EUR', 'pos' => 0, 'status' => 0],
+		['siteid' => 'unittest', 'langid' => 'en', 'currencyid' => 'EUR', 'pos' => 1, 'status' => 1],
 	],
 ];
