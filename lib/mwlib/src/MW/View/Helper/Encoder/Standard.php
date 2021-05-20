a4c5df0acd27745022d6efd4296f537576c68413
Aimeos <aimeos@aimeos.org>
2021-05-20 17:21:02 +0200

    Fixed escaping of backticks

--- lib/mwlib/src/MW/View/Helper/Encoder/Standard.php
+++ lib/mwlib/src/MW/View/Helper/Encoder/Standard.php
@@ -56,7 +56,7 @@ class Standard
 			return $value;
 		}
 
-		return str_replace( ['"', '\'', '`'], ['&quot;', '&apos;', '&#96;'], (string) $value );
+		return str_replace( ['"', '\'', '`'], ['&quot;', '&apos;', '``'], (string) $value );
 	}
 
 
