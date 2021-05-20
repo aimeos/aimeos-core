ab20a2cfeffc73ddcb8f90f864e0e5230aba5572
Aimeos <aimeos@aimeos.org>
2021-05-20 12:59:02 +0200

    Encode backticks as HTML entities

--- lib/mwlib/tests/MW/View/Helper/Encoder/StandardTest.php
+++ lib/mwlib/tests/MW/View/Helper/Encoder/StandardTest.php
@@ -55,7 +55,7 @@ class StandardTest extends \PHPUnit\Framework\TestCase
 	{
 		$enc = $this->object->transform();
 
-		$this->assertEquals( '<a>&quot;attribute&apos;</a>', $enc->attr( '<a>"attribute\'</a>' ) );
+		$this->assertEquals( '<a>&quot;attri´bute&apos;</a>', $enc->attr( '<a>"attri&#96;bute\'</a>' ) );
 	}
 
 
