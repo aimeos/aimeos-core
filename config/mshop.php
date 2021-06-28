66553968214c920e65725c3b824105de1b8da1ae
Aimeos <aimeos@aimeos.org>
2021-06-28 08:40:13 +0200

    Removed unused configuration

--- config/mshop.php
+++ config/mshop.php
@@ -1,19 +1,4 @@
 <?php
 
 return [
-	'index' => [
-		'manager' => [
-//			'name' => 'Elastic',
-		]
-	],
-	'product' => [
-		'manager' => [
-//			'name' => 'Elastic',
-		]
-	],
-	'stock' => [
-		'manager' => [
-//			'name' => 'Elastic',
-		]
-	]
 ];
