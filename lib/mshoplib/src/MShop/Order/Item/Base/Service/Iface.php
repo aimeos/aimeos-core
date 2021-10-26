82b50c17bbcb1f6f2ac1da61dd209a72eedc9aad
Aimeos <aimeos@aimeos.org>
2021-10-26 20:42:24 +0200

    Fixed interface of service getAttributeItems()

--- lib/mshoplib/src/MShop/Order/Item/Base/Service/Iface.php
+++ lib/mshoplib/src/MShop/Order/Item/Base/Service/Iface.php
@@ -165,7 +165,7 @@ interface Iface
 	 * @param string|null $type Filters returned attributes by the given type or null for no filtering
 	 * @return \Aimeos\Map List of attribute items implementing \Aimeos\MShop\Order\Item\Base\Service\Attribute\Iface
 	 */
-	public function getAttributeItems( ?string $type ) : \Aimeos\Map;
+	public function getAttributeItems( string $type = null ) : \Aimeos\Map;
 
 	/**
 	 * Adds or replaces the attribute item in the list of service attributes.
