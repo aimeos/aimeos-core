04acb0c5c2efeb59c20e7080a9a1c76f32659eae
Aimeos <aimeos@aimeos.org>
2021-11-02 09:03:56 +0100

    Readded changing mshop_index_text to InnoDB due to DBAL bug

---
+++ lib/mshoplib/setup/IndexMigrateTextInnodb.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * @license LGPLv3, https://opensource.org/licenses/LGPL-3.0
+ * @copyright Aimeos (aimeos.org), 2020-2021
+ */
+
+
+namespace Aimeos\Upscheme\Task;
+
+
+class IndexMigrateTextInnodb extends Base
+{
+	public function before() : array
+	{
+		return ['TablesMigrateSiteid'];
+	}
+
+
+	public function up()
+	{
+		$db = $this->db( 'db-product' );
+
+		if( !$db->hasTable( 'mshop_index_text' ) ) {
+			return;
+		}
+
+		$this->info( 'Migrate mshop_index_text table engine to InnoDB', 'v' );
+
+		$db->for( 'mysql', 'ALTER TABLE mshop_index_text ENGINE=InnoDB' );
+	}
+}
