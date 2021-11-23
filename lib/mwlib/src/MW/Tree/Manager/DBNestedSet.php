c38691e1b0d2ebdda0cea13eb3ac127bf275264d
Aimeos <aimeos@aimeos.org>
2021-11-23 15:40:32 +0100

    Use search function closures to replace placeholders

--- lib/mwlib/src/MW/Tree/Manager/DBNestedSet.php
+++ lib/mwlib/src/MW/Tree/Manager/DBNestedSet.php
@@ -221,8 +221,9 @@ class DBNestedSet extends \Aimeos\MW\Tree\Manager\Base
 		$search = $condition ?: $this->createSearch();
 
 		$types = $this->getSearchTypes( $this->searchConfig );
+		$funcs = $this->getSearchFunctions( $this->searchConfig );
 		$translations = $this->getSearchTranslations( $this->searchConfig );
-		$conditions = $search->getConditionSource( $types, $translations );
+		$conditions = $search->getConditionSource( $types, $translations, [], $funcs );
 
 
 		$conn = $this->dbm->acquire( $this->dbname );
@@ -557,9 +558,10 @@ class DBNestedSet extends \Aimeos\MW\Tree\Manager\Base
 		}
 
 		$types = $this->getSearchTypes( $this->searchConfig );
+		$funcs = $this->getSearchFunctions( $this->searchConfig );
 		$translations = $this->getSearchTranslations( $this->searchConfig );
-		$conditions = $search->getConditionSource( $types, $translations );
-		$sortations = $search->getSortationSource( $types, $translations );
+		$conditions = $search->getConditionSource( $types, $translations, [], $funcs );
+		$sortations = $search->getSortationSource( $types, $translations, [], $funcs );
 
 		$sql = str_replace(
 			[':cond', ':order', ':size', ':start'],
