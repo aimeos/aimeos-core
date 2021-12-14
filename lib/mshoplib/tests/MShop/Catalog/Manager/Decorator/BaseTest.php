272ddd01e80cb9825a067f1a67f2b87efbea6d0a
Aimeos <aimeos@aimeos.org>
2021-12-14 14:09:01 +0100

    Fixed tests by using getContext() method

--- lib/mshoplib/tests/MShop/Catalog/Manager/Decorator/BaseTest.php
+++ lib/mshoplib/tests/MShop/Catalog/Manager/Decorator/BaseTest.php
@@ -18,7 +18,7 @@ class BaseTest extends \PHPUnit\Framework\TestCase
 
 	protected function setUp() : void
 	{
-		$this->context = \TestHelperMShop::context();
+		$this->context = \TestHelperMShop::getContext();
 
 		$this->stub = $this->getMockBuilder( \Aimeos\MShop\Catalog\Manager\Standard::class )
 			->disableOriginalConstructor()
