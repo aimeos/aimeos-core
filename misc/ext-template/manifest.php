f33d27e96d94a0a087e7926a5ce793b863ed47f4
Aimeos <aimeos@aimeos.org>
2022-03-10 10:19:30 +0100

    Updated extension template

--- misc/ext-template/manifest.php
+++ misc/ext-template/manifest.php
@@ -15,52 +15,43 @@ return [
 		'ai-controller-frontend',
 	],
 	'include' => [
-		'lib/custom/src',
-		'client/html/src',
-		'client/jsonapi/src',
-		'controller/common/src',
-		'controller/frontend/src',
-		'controller/jobs/src',
-		'admin/jsonadm/src',
-		'admin/jqadm/src',
+		'src',
 	],
 	'i18n' => [
-		'admin' => 'admin/i18n',
-		'admin/jsonadm' => 'admin/jsonadm/i18n',
-		'client' => 'client/i18n',
-		'client/code' => 'client/i18n/code',
-		'controller/common' => 'controller/common/i18n',
-		'controller/frontend' => 'controller/frontend/i18n',
-		'controller/jobs' => 'controller/jobs/i18n',
-		'mshop' => 'lib/custom/i18n',
+		'admin' => 'i18n',
+		'client' => 'i18n',
+		'client/code' => 'i18n',
+		'controller/common' => 'i18n',
+		'controller/frontend' => 'i18n',
+		'controller/jobs' => 'i18n',
+		'mshop' => 'i18n',
 	],
 	'setup' => [
-		'lib/custom/setup',
+		'setup',
 	],
 	'template' => [
 		'admin/jqadm/templates' => [
-			'admin/jqadm/templates',
+			'templates/admin/jqadm',
 		],
 		'admin/jsonadm/templates' => [
-			'admin/jsonadm/templates',
-		],
-		'client/html/templates' => [
-			'client/html/templates',
+			'templates/admin/jsonadm',
 		],
 		'client/jsonapi/templates' => [
-			'client/jsonapi/templates',
+			'templates/client/jsonapi',
+		],
+		'client/html/templates' => [
+			'templates/client/html',
 		],
 		'controller/jobs/templates' => [
-			'controller/jobs/templates',
-			'client/html/templates',
+			'templates/controller/jobs',
 		],
 	],
 	'custom' => [
 		'admin/jqadm' => [
-			'admin/jqadm/manifest.jsb2',
+			'manifest.jsb2',
 		],
 		'controller/jobs' => [
-			'controller/jobs/src',
+			'src',
 		],
 	],
 ];
