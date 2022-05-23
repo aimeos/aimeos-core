175607e2b7b4ee2df6b32e48ee1ae08c7be199eb
Aimeos <aimeos@aimeos.org>
2022-05-23 12:09:17 +0200

    Fixed TestHelper class for client/html tests

--- misc/ext-template/tests/TestHelper.php
+++ misc/ext-template/tests/TestHelper.php
@@ -41,6 +41,55 @@ class TestHelper
 	}
 
 
+	public static function getHtmlTemplatePaths()
+	{
+		return self::getAimeos()->getTemplatePaths( 'client/html/templates' );
+	}
+
+
+	public static function view( $site = 'unittest', \Aimeos\Base\Config\Iface $config = null )
+	{
+		if( $config === null ) {
+			$config = self::context( $site )->config();
+		}
+
+		$view = new \Aimeos\Base\View\Standard( self::getHtmlTemplatePaths() );
+
+		$trans = new \Aimeos\Base\Translation\None( 'de_DE' );
+		$helper = new \Aimeos\Base\View\Helper\Translate\Standard( $view, $trans );
+		$view->addHelper( 'translate', $helper );
+
+		$helper = new \Aimeos\Base\View\Helper\Url\Standard( $view, 'http://baseurl' );
+		$view->addHelper( 'url', $helper );
+
+		$helper = new \Aimeos\Base\View\Helper\Session\Standard( $view, self::context( $site )->session() );
+		$view->addHelper( 'session', $helper );
+
+		$helper = new \Aimeos\Base\View\Helper\Number\Standard( $view, '.', '' );
+		$view->addHelper( 'number', $helper );
+
+		$helper = new \Aimeos\Base\View\Helper\Date\Standard( $view, 'Y-m-d' );
+		$view->addHelper( 'date', $helper );
+
+		$paths = ['version', 'client', 'common', 'resource/fs/baseurl', 'resource/fs-media/baseurl', 'resource/fs-theme/baseurl'];
+		$config = new \Aimeos\Base\Config\Decorator\Protect( $config, $paths );
+		$helper = new \Aimeos\Base\View\Helper\Config\Standard( $view, $config );
+		$view->addHelper( 'config', $helper );
+
+		$helper = new \Aimeos\Base\View\Helper\Csrf\Standard( $view, '_csrf_token', '_csrf_value' );
+		$view->addHelper( 'csrf', $helper );
+
+		$psr17Factory = new \Nyholm\Psr7\Factory\Psr17Factory();
+		$helper = new \Aimeos\Base\View\Helper\Request\Standard( $view, $psr17Factory->createServerRequest( 'POST', 'https://aimeos.org' ) );
+		$view->addHelper( 'request', $helper );
+
+		$helper = new \Aimeos\Base\View\Helper\Response\Standard( $view, $psr17Factory->createResponse() );
+		$view->addHelper( 'response', $helper );
+
+		return $view;
+	}
+
+
 	/**
 	 * @param string $site
 	 */
@@ -52,24 +101,36 @@ class TestHelper
 
 		$paths = $aimeos->getConfigPaths();
 		$paths[] = __DIR__ . DIRECTORY_SEPARATOR . 'config';
+		$file = __DIR__ . DIRECTORY_SEPARATOR . 'confdoc.ser';
+		$local = array( 'resource' => array( 'fs' => array( 'adapter' => 'Standard', 'basedir' => __DIR__ . '/tmp' ) ) );
 
-		$conf = new \Aimeos\MW\Config\PHPArray( array(), $paths );
+		$conf = new \Aimeos\Base\Config\PHPArray( $local, $paths );
+		$conf = new \Aimeos\Base\Config\Decorator\Memory( $conf );
+		$conf = new \Aimeos\Base\Config\Decorator\Documentor( $conf, $file );
 		$ctx->setConfig( $conf );
 
 
-		$dbm = new \Aimeos\MW\DB\Manager\DBAL( $conf );
-		$ctx->setDatabaseManager( $dbm );
-
-
-		$logger = new \Aimeos\Base\Logger\File( $site . '.log', \Aimeos\Base\Logger\Base::DEBUG );
+		$logger = new \Aimeos\Base\Logger\File( $site . '.log', \Aimeos\Base\Logger\Iface::DEBUG );
 		$ctx->setLogger( $logger );
 
 
-		$cache = new \Aimeos\MW\Cache\None();
+		$dbm = \Aimeos\Base\DB\Factory::create( $conf, 'PDO' );
+		$ctx->setDatabaseManager( $dbm );
+
+
+		$fs = new \Aimeos\Base\Filesystem\Manager\Standard( $conf->get( 'resource' ) );
+		$ctx->setFilesystemManager( $fs );
+
+
+		$mq = new \Aimeos\Base\MQueue\Manager\Standard( $conf );
+		$ctx->setMessageQueueManager( $mq );
+
+
+		$cache = new \Aimeos\Base\Cache\None();
 		$ctx->setCache( $cache );
 
 
-		$i18n = new \Aimeos\MW\Translation\None( 'de' );
+		$i18n = new \Aimeos\Base\Translation\None( 'de' );
 		$ctx->setI18n( array( 'de' => $i18n ) );
 
 
@@ -78,12 +139,10 @@ class TestHelper
 
 
 		$localeManager = \Aimeos\MShop\Locale\Manager\Factory::create( $ctx );
-		$localeItem = $localeManager->bootstrap( $site, '', '', false );
+		$locale = $localeManager->bootstrap( $site, '', '', false );
+		$ctx->setLocale( $locale );
 
-		$ctx->setLocale( $localeItem );
 
-		$ctx->setEditor( '<extname>:lib/custom' );
-
-		return $ctx;
+		return $ctx->setEditor( '<extname>' );
 	}
 }
