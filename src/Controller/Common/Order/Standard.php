8033027399989fbe92c867761dbf067e1ba7d047
Aimeos <aimeos@aimeos.org>
2023-12-21 16:06:43 +0100

    Fixed updating selection stock level in market places

--- src/Controller/Common/Order/Standard.php
+++ src/Controller/Common/Order/Standard.php
@@ -232,7 +232,7 @@ class Standard
 	{
 		$stockManager = \Aimeos\MShop::create( $this->context, 'stock' );
 
-		$search = $stockManager->filter()->slice( 0, count( $prodIds ) )
+		$search = $stockManager->filter()->slice( 0, 0x7fffffff )
 			->add( ['stock.productid' => $prodIds, 'stock.type' => $stockType] );
 
 		return $stockManager->search( $search );
@@ -437,8 +437,8 @@ class Standard
 		$productItem = $productManager->get( $prodId, ['product'] );
 		$prodIds = $productItem->getRefItems( 'product', 'default', 'default' )->getId()->push( $productItem->getId() );
 
-		$stockItems = $this->getStockItems( $prodIds, $stocktype )->col( null, 'stock.productid' );
-		$selStockItem = $stockItems->pull( $prodId ) ?: $stockManager->create();
+		$stockItems = $this->getStockItems( $prodIds, $stocktype );
+		$selStockItem = $stockItems->col( null, 'stock.productid' )->pull( $prodId ) ?: $stockManager->create();
 
 		$sum = $stockItems->getStockLevel()->reduce( function( $result, $value ) {
 			return $result !== null && $value !== null ? $result + $value : null;
