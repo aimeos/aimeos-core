04a54d35923caf7dde4157fd3c0fda4add508ef9
Aimeos <aimeos@aimeos.org>
2024-05-13 11:25:38 +0200

    Removed en/EUR from demo locales

--- setup/default/data/locale.php
+++ setup/default/data/locale.php
@@ -2,7 +2,6 @@
 
 /**
  * @license LGPLv3, https://opensource.org/licenses/LGPL-3.0
- * @copyright Metaways Infosystems GmbH, 2012
  * @copyright Aimeos (aimeos.org), 2015-2024
  */
 
@@ -13,8 +12,7 @@ return [
 	],
 
 	'locale' => [
-		['site' => 'default', 'locale.languageid' => 'en', 'locale.currencyid' => 'EUR', 'locale.position' => 0],
-		['site' => 'default', 'locale.languageid' => 'en', 'locale.currencyid' => 'USD', 'locale.position' => 1],
-		['site' => 'default', 'locale.languageid' => 'de', 'locale.currencyid' => 'EUR', 'locale.position' => 2],
+		['site' => 'default', 'locale.languageid' => 'en', 'locale.currencyid' => 'USD', 'locale.position' => 0],
+		['site' => 'default', 'locale.languageid' => 'de', 'locale.currencyid' => 'EUR', 'locale.position' => 1],
 	],
 ];
