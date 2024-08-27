8006034e51c87727e1492921139d4dc0af866209
Aimeos <aimeos@aimeos.org>
2024-08-27 13:54:42 +0200

    Added method documentation for utility class methods

--- src/Utils.php
+++ src/Utils.php
@@ -14,6 +14,16 @@ namespace Aimeos;
  */
 class Utils
 {
+	/**
+	 * Creates a new object instance
+	 *
+	 * @param string $class Name of the class
+	 * @param array $args Constructor arguments
+	 * @param string|null $iface Name of the interface the object must implement
+	 * @return object New object instance
+	 * @throws \LogicException If the class isn't found or doesn't implement the interface
+	 * @todo 2025.01 Allow list of interfaces to check for common and specific interfaces
+	 */
 	public static function create( string $class, array $args, string $iface = null ) : object
 	{
 		if( class_exists( $class ) === false ) {
@@ -30,6 +40,15 @@ class Utils
 	}
 
 
+	/**
+	 * Checks if the object implements the given interface
+	 *
+	 * @param object $object Object to check
+	 * @param string $iface Name of the interface the object must implement
+	 * @return object Same object as passed in
+	 * @throws \LogicException If the object doesn't implement the interface
+	 * @todo 2025.01 Allow list of interfaces to check for common and specific interfaces
+	 */
 	public static function implements( object $object, string $iface ) : object
 	{
 		if( !( $object instanceof $iface ) ) {
