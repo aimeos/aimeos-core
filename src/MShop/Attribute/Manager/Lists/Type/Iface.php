7d953f8ffa83a92709045479789d00ae96764de4
Aimeos <aimeos@aimeos.org>
2024-08-30 11:27:19 +0200

    Fixed indentations

--- src/MShop/Attribute/Manager/Lists/Type/Iface.php
+++ src/MShop/Attribute/Manager/Lists/Type/Iface.php
@@ -18,6 +18,6 @@ namespace Aimeos\MShop\Attribute\Manager\Lists\Type;
  * @subpackage Attribute
  */
 interface Iface
-    extends \Aimeos\MShop\Common\Manager\Type\Iface
+	extends \Aimeos\MShop\Common\Manager\Type\Iface
 {
 }
