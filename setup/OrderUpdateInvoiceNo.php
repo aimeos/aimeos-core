6b2fa29e7299a213ff7d011c4cb9269e84d4fec7
Aimeos <aimeos@aimeos.org>
2024-12-21 20:03:05 +0100

    Added setup task for updating order invoice number

---
+++ setup/OrderUpdateInvoiceNo.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * @license LGPLv3, https://opensource.org/licenses/LGPL-3.0
+ * @copyright Aimeos (aimeos.org), 2023-2024
+ */
+
+
+namespace Aimeos\Upscheme\Task;
+
+
+class OrderUpdateInvoiceNo extends Base
+{
+	public function after() : array
+	{
+		return ['Locale', 'Order'];
+	}
+
+
+	public function up()
+	{
+		$db = $this->db( 'db-order' );
+		$db2 = $this->db( 'db-locale' );
+
+		if( !$db->hasColumn( 'mshop_order', 'invoiceno' ) || !$db2->hasColumn( 'mshop_locale_site', 'invoiceno' ) ) {
+			return;
+		}
+
+		$this->info( 'Set invoice ID in order table if empty', 'vv' );
+
+		$db->stmt()->update( 'mshop_order' )
+			->set( $db->qi( 'invoiceno' ), $db->qi( 'id' ) )
+			->where( $db->qi( 'invoiceno' ) . ' = \'\'' )
+			->executeStatement();
+
+		$result = $db->stmt()
+			->select( 'siteid', 'MAX(' . $db->qi( 'invoiceno' ) . ') AS maxnum' )
+			->from( 'mshop_order' )
+			->groupBy( 'siteid' )
+			->executeQuery();
+
+		while( $row = $result->fetchAssociative() )
+		{
+			$db2->stmt()->update( 'mshop_locale_site' )
+				->set( 'invoiceno', '?' )
+				->where( 'siteid = ?' )->andWhere( 'invoiceno = ?' )
+				->setParameters( [$row['maxnum'], $row['siteid'], 1] )
+				->executeStatement();
+		}
+	}
+}
