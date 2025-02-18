9384ea3395d59b0c8f0a8d8d48835112d3635673
Aimeos <aimeos@aimeos.org>
2025-02-18 15:40:09 +0100

    Improved ListRef item trait and fixed returning previously added list items

--- src/MShop/Common/Item/ListsRef/Traits.php
+++ src/MShop/Common/Item/ListsRef/Traits.php
@@ -21,7 +21,6 @@ trait Traits
 {
 	private array $listItems = [];
 	private array $listRmItems = [];
-	private array $listRmMap = [];
 	private array $listMap = [];
 	private int $listMax = 0;
 
@@ -98,12 +97,14 @@ trait Traits
 			if( $refItem instanceof \Aimeos\MShop\Common\Item\Domain\Iface ) {
 				$refItem->setDomain( $this->getResourceType() );
 			}
+
+			$listItem->setRefItem( $refItem );
 		}
 
 		$id = $listItem->getId() ?: '_' . $this->getId() . '_' . $domain . '_' . $listItem->getType() . '_' . $listItem->getRefId();
 
 		unset( $this->listItems[$domain][$id] ); // append at the end
-		$this->listItems[$domain][$id] = $listItem->setDomain( $domain )->setRefItem( $refItem );
+		$this->listItems[$domain][$id] = $listItem->setDomain( $domain );
 
 		if( isset( $this->listMap[$domain] ) )
 		{
@@ -128,7 +129,7 @@ trait Traits
 		if( isset( $this->listItems[$domain] )
 			&& ( $key = array_search( $listItem, $this->listItems[$domain], true ) ) !== false
 		) {
-			$this->listRmItems[] = $this->listRmMap[$domain][] = $listItem->setRefItem( $refItem );
+			$this->listRmItems[] = $listItem->setRefItem( $refItem );
 
 			unset( $this->listMap[$domain][$listItem->getType()][$listItem->getRefId()] );
 			unset( $this->listItems[$domain][$key] );
@@ -180,7 +181,7 @@ trait Traits
 	public function getListItemsDeleted( ?string $domain = null ) : \Aimeos\Map
 	{
 		if( $domain !== null ) {
-			return map( $this->listRmMap[$domain] ?? [] );
+			return map( $this->listRmItems )->filter( fn( $item ) => $item->getDomain() === $domain );
 		}
 
 		return map( $this->listRmItems );
@@ -243,12 +244,13 @@ trait Traits
 		$fcn = static::macro( 'listFilter' );
 
 		$iface = \Aimeos\MShop\Common\Item\TypeRef\Iface::class;
-		$listTypes = ( is_array( $listtype ) ? $listtype : array( $listtype ) );
-		$types = ( is_array( $type ) ? $type : array( $type ) );
+		$listTypes = is_array( $listtype ) ? $listtype : [$listtype];
+		$domains = is_array( $domain ) ? $domain : [$domain];
+		$types = is_array( $type ) ? $type : [$type];
 
 		foreach( $this->listItems as $dname => $list )
 		{
-			if( is_array( $domain ) && !in_array( $dname, $domain ) || is_string( $domain ) && $dname !== $domain ) {
+			if( $domain && !in_array( $dname, $domains ) ) {
 				continue;
 			}
 
