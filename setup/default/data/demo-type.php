2c964f1164dc7a129de64b16c0f8aaab5649f531
Aimeos <aimeos@aimeos.org>
2025-02-20 14:47:08 +0100

    Fixed demo type data

--- setup/default/data/demo-type.php
+++ setup/default/data/demo-type.php
@@ -6,12 +6,10 @@
  */
 
 return [
-	'attribute/type' => [
-		array( 'domain' => 'product', 'code' => 'color', 'label' => 'Color', 'status' => 1 ),
-		array( 'domain' => 'product', 'code' => 'size', 'label' => 'Size', 'status' => 1 ),
-		array( 'domain' => 'product', 'code' => 'width', 'label' => 'Width', 'status' => 1 ),
-		array( 'domain' => 'product', 'code' => 'length', 'label' => 'Length', 'status' => 1 ),
-		array( 'domain' => 'product', 'code' => 'print', 'label' => 'Print', 'status' => 1 ),
-		array( 'domain' => 'product', 'code' => 'sticker', 'label' => 'Sticker', 'status' => 1 ),
-	],
+	['domain' => 'product', 'code' => 'color', 'label' => 'Color', 'status' => 1],
+	['domain' => 'product', 'code' => 'size', 'label' => 'Size', 'status' => 1],
+	['domain' => 'product', 'code' => 'width', 'label' => 'Width', 'status' => 1],
+	['domain' => 'product', 'code' => 'length', 'label' => 'Length', 'status' => 1],
+	['domain' => 'product', 'code' => 'print', 'label' => 'Print', 'status' => 1],
+	['domain' => 'product', 'code' => 'sticker', 'label' => 'Sticker', 'status' => 1],
 ];
