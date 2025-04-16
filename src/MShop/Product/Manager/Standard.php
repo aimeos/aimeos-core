21efcb64e7514fc279814b2c2ef571cf0149608f
Aimeos <aimeos@aimeos.org>
2025-04-16 10:07:23 +0200

    Fixed saving custom product creation date

--- src/MShop/Product/Manager/Standard.php
+++ src/MShop/Product/Manager/Standard.php
@@ -382,6 +382,24 @@ class Standard
 
 
 	/**
+	 * Binds additional values to the statement before execution.
+	 *
+	 * @param \Aimeos\MShop\Common\Item\Iface $item Item object
+	 * @param \Aimeos\Base\DB\Statement\Iface $stmt Database statement object
+	 * @param int $idx Current bind index
+	 * @return \Aimeos\Base\DB\Statement\Iface Database statement object with bound values
+	 */
+	protected function bind( \Aimeos\MShop\Common\Item\Iface $item, \Aimeos\Base\DB\Statement\Iface $stmt, int &$idx ) : \Aimeos\Base\DB\Statement\Iface
+	{
+		if( $item->getId() !== null ) {
+			$stmt->bind( $idx++, $item->get( 'product.ctime' ) );
+		}
+
+		return $stmt;
+	}
+
+
+	/**
 	 * Returns the stock items for the given product codes
 	 *
 	 * @param array $entries List of product records
